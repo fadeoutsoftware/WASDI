402ab531e88bfacc172656ca7fbb9ce6ca4756a8
Salvatore <s.adamo@fadeout.it>
2017-05-23 15:30:14 +0200

    sftp user management

---
+++ wasdiwebserver/src/main/java/it/fadeout/sftp/SFTPManager.java
@@ -0,0 +1,107 @@
+package it.fadeout.sftp;
+
+import java.util.UUID;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import cern.colt.Arrays;
+import it.fadeout.mercurius.business.Message;
+import it.fadeout.mercurius.client.MercuriusAPI;
+
+/**
+ * class for sftp account management
+ * @author doy
+ *
+ */
+public class SFTPManager {
+	
+	String m_sAddress;
+	
+	public SFTPManager(String m_sAddress) {
+		this.m_sAddress = m_sAddress;
+	}
+
+	private String sendCommand(String sCommand) {
+		try {
+			WsClient oClient = new WsClient(m_sAddress, sCommand);
+			if (oClient.isOk()) return oClient.getData();		
+			System.out.println("ERROR: " + oClient.getData());		
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}				
+		return null;
+	}
+	
+	/**
+	 * @param sUser
+	 * @return true if sUser can connect to sftp server
+	 */
+	public boolean checkUser(String sUser) {
+		return sendCommand("exists_account " + sUser) != null;
+	}
+
+	/**
+	 * create a new account for sftp service
+	 * @param sUser
+	 * @param sPassword
+	 * @return 
+	 */
+	public boolean createAccount(String sUser, String sPassword) {			
+		return sendCommand("create_account " + sUser + " " + sPassword) != null;
+	}
+
+	/**
+	 * update a user password in sftp service
+	 * @param sUser
+	 * @param sPassword
+	 * @return 
+	 */
+	public boolean updatePassword(String sUser, String sPassword) {			
+		return sendCommand("update_password " + sUser + " " + sPassword) != null;
+	}
+
+	/**
+	 * remove an account from sftp service
+	 * @param sUser
+	 * @return 
+	 */
+	public boolean removeAccount(String sUser) {			
+		return sendCommand("remove_account " + sUser) != null;
+	}
+
+	/**
+	 * list the files in the sftp server user home 
+	 * @param sUser
+	 * @return
+	 */
+	public String[] list(String sUser) {
+		String s = sendCommand("list " + sUser);
+		return s.split("__WASDI_SEPARATOR__");
+	}
+
+	
+	public static void main(String[] args) {
+		
+		String wsAddress = "ws://10.172.47.35:6703"; 
+		SFTPManager oManager = new SFTPManager(wsAddress);
+		String sAccount = "prova";
+		
+//		oManager.createAccount(sAccount, sAccount);		
+//		System.out.println(Arrays.toString(oManager.list(sAccount)));
+		oManager.removeAccount(sAccount);		
+		
+		
+//		String sPassword = UUID.randomUUID().toString().split("-")[0];
+//		System.out.println(sPassword);
+//		oManager.removeAccount(sAccount);
+//		if (!oManager.createAccount(sAccount, sPassword)) System.out.println("NOOOO");
+//		//send email with new password
+//		MercuriusAPI oAPI = new MercuriusAPI("http://***REMOVED***/it.fadeout.mercurius.webapi");			
+//		Message oMessage = new Message();
+//		oMessage.setTilte("Wasdi sftp account");
+//		oMessage.setSender("adminwasdi@acrotec.it");
+//		oMessage.setMessage("USER: " + sAccount + " - PASSWORD: " + sPassword);
+//		System.out.println(oAPI.sendMailDirect("alessandro.burastero@gmail.com", oMessage));
+	}
+}
---
+++ wasdiwebserver/src/main/java/it/fadeout/sftp/WsClient.java
@@ -0,0 +1,72 @@
+package it.fadeout.sftp;
+
+import java.net.URI;
+import java.util.concurrent.Semaphore;
+
+import javax.websocket.ClientEndpoint;
+import javax.websocket.CloseReason;
+import javax.websocket.ContainerProvider;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.WebSocketContainer;
+
+@ClientEndpoint
+public class WsClient extends Semaphore {
+	
+	String m_sMessage;
+	boolean ok;
+	String data;
+	private Session m_sUserSession;
+
+	public WsClient(String sWsAddress, String sMessage) throws InterruptedException {
+		super(0, true);
+		m_sMessage = sMessage;
+		WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+		try {
+			container.connectToServer(this, new URI(sWsAddress));
+			acquire();
+		} catch (Exception e) {
+			e.printStackTrace();
+			release();
+		}		
+	}
+		
+   @OnOpen
+    public void onOpen(Session userSession) {
+        System.out.println("opening websocket...");
+        m_sUserSession = userSession;
+        userSession.getAsyncRemote().sendText(m_sMessage);	        
+   }
+   
+	@OnClose
+	public void onClose(Session userSession, CloseReason reason) {
+	    System.out.println("closing websocket");		    
+	}
+
+    /**
+     * Callback hook for Message Events. This method will be invoked when a client send a message.
+     *
+     * @param message The text message
+     */
+    @OnMessage
+    public void onMessage(String message) {
+    	
+    	System.out.println(message);
+    	
+    	ok = message.startsWith("OK;");
+    	String[] toks = message.split(",|;");
+		data = toks.length>1 ? toks[1] : "";
+    	release();
+    }
+
+	public boolean isOk() {
+		return ok;
+	}
+
+	public String getData() {
+		return data;
+	}
+    
+}
