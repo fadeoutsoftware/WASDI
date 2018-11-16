82c71bed01a52592239bf382fdb492ebae6e4e50
Cristiano Nattero <cristiano.nattero@gmail.com>
2018-11-16 12:08:05 +0100

    New REST API: WPSlist
    Since WPS providers could change, they are stored in the DB, and a list of viewmodels is returned upon request
    to /rest/processing/WPSlist

---
+++ wrappersnap/wasdishared/src/main/java/wasdi/shared/viewmodels/WpsViewModel.java
@@ -0,0 +1,22 @@
+/*
+ * WPS providers entity
+ * 
+ * Created by Cristiano Nattero on 2018.11.16
+ * 
+ * Fadeout software
+ * 
+ */
+
+package wasdi.shared.viewmodels;
+
+public class WpsViewModel {
+	private String address;
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String wPS) {
+		address = wPS;
+	}
+}
