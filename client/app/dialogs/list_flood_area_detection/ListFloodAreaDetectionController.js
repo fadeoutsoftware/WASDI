c8a71df8839baf469dcd273fd7d0bd63ad2c8438
Alessio Corrado <a.corrado@fadeout.it>
2018-11-26 14:20:02 +0100

    the mock dialog ListFloodAreaDetection is finished

--- client/app/dialogs/list_flood_area_detection/ListFloodAreaDetectionController.js
+++ client/app/dialogs/list_flood_area_detection/ListFloodAreaDetectionController.js
@@ -10,13 +10,27 @@ var ListFloodAreaDetectionController = (function() {
         this.m_oScope = $scope;
         this.m_oScope.m_oController = this;
         this.m_oExtras = oExtras;
-
+        this.m_aoProducts = this.m_oExtras.products;
+        this.m_oSelectedReferenceProduct = null;
+        this.m_oSelectedPostEventImageProduct = null;
+        if(utilsIsObjectNullOrUndefined(this.m_aoProducts) === false)
+        {
+            this.m_oSelectedReferenceProduct = this.m_aoProducts[0];
+            this.m_oSelectedPostEventImageProduct = this.m_aoProducts[0];
+        }
         $scope.close = function(result) {
             oClose(result, 300); // close, but give 500ms for bootstrap to animate
         };
 
     };
 
+    ListFloodAreaDetectionController.prototype.redirectToWebSite = function(){
+        this.m_oWindow.open('http://www.mydewetra.org', '_blank');
+    };
+
+    ListFloodAreaDetectionController.prototype.runListFloodAreaDetection = function(){
+        //TODO SEND REQUEST
+    };
 
 
     ListFloodAreaDetectionController.$inject = [
