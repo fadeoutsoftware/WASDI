14264e32b349f1a6da5aa731f58849e71fa5f468
a.corrado <a.corrado@fadeout.it>
2019-01-29 16:59:34 +0100

    Added delete workflow confirm (#70 github)

--- client/app/services/ConstantsService.js
+++ client/app/services/ConstantsService.js
@@ -10,11 +10,11 @@ service('ConstantsService', [function () {
 
     // WASDI SERVER
     /**/
-        this.URL = 'http://178.22.66.96/wasdiwebserver/';
-        this.WEBSTOMPURL = 'http://178.22.66.96/rabbit/stomp';
-        this.WMSURL = "http://178.22.66.96/geoserver/ows?";
-        //this.WASDIGEOSERVERWPS = "http://178.22.66.96/geoserver/wps";
-         this.WASDIGEOSERVERWPS = "http://www.wasdi.net/geoserver/wps";
+        // this.URL = 'http://178.22.66.96/wasdiwebserver/';
+        // this.WEBSTOMPURL = 'http://178.22.66.96/rabbit/stomp';
+        // this.WMSURL = "http://178.22.66.96/geoserver/ows?";
+        // //this.WASDIGEOSERVERWPS = "http://178.22.66.96/geoserver/wps";
+        //  this.WASDIGEOSERVERWPS = "http://www.wasdi.net/geoserver/wps";
     //
 
     /**/
@@ -44,9 +44,9 @@ service('ConstantsService', [function () {
     // this.WMSURL = "http://10.0.0.15:8080/geoserver/ows?";//wasdi/wms? OLD VERSION
 
     // LOCALHOST
-    // this.URL= 'http://127.0.0.1:8080/wasdiwebserver/';//
-    // this.WEBSTOMPURL = 'http://178.22.66.96/rabbit/stomp';
-    // this.WMSURL = "http://127.0.0.1:8080/geoserver/ows?";//wasdi/wms? OLD VERSION
+    this.URL= 'http://127.0.0.1:8080/wasdiwebserver/';//
+    this.WEBSTOMPURL = 'http://178.22.66.96/rabbit/stomp';
+    this.WMSURL = "http://127.0.0.1:8080/geoserver/ows?";//wasdi/wms? OLD VERSION
     
 
     this.WPSPROXY =  'https://cors-anywhere.herokuapp.com/';
