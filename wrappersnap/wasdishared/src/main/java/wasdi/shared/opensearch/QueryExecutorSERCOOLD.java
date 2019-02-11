3739a1b9b8f9dd98ad0c024ae0f7329392dc2747
Cristiano Nattero <c.nattero@fadeout.it>
2019-02-11 18:12:49 +0100

    Renamed class

---
+++ wrappersnap/wasdishared/src/main/java/wasdi/shared/opensearch/QueryExecutorSERCOOLD.java
@@ -0,0 +1,32 @@
+package wasdi.shared.opensearch;
+
+import org.apache.abdera.i18n.templates.Template;
+
+public class QueryExecutorSERCOOLD extends QueryExecutor {
+
+	public QueryExecutorSERCOOLD() {
+		System.out.println("QueryExecutorSERCOOLD");
+		m_sProvider = "SERCO";
+	}
+	
+	@Override
+	protected Template getTemplate() {
+		return new Template("{scheme}://{-append|.|host}early.onda-dias.eu{-opt|/|path}{-listjoin|/|path}{-prefix|/|page}{-opt|?|q}{-join|&|q,start,rows,orderby}");
+	}
+
+	@Override	
+	protected String[] getUrlPath() {
+		return new String[] {"catalogue","search"};
+	}
+
+	@Override
+	protected String getUrlSchema() {
+		return "http";
+	}
+
+	@Override
+	protected String getCountUrl(String sQuery) {
+		return "http://early.onda-dias.eu/catalogue/count?filter=" + sQuery;
+	}
+	
+}
