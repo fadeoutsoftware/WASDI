a11515aed4caf35534d3e4942f95e37351c28f21
a.corrado <a.corrado@fadeout.it>
2019-02-12 17:51:40 +0100

    Developed upload file dialog fixes + wasdilib upload file

--- client/app/dialogs/upload_file/UploadFileController.js
+++ client/app/dialogs/upload_file/UploadFileController.js
@@ -14,7 +14,7 @@ var UploadFileController = (function() {
         this.m_oProductService = oProductService;
         this.m_oFile = null;
         this.m_sWorkspaceId = this.m_oExtras.workflowId;
-
+        this.m_bIsUploading = false;
         var oController = this;
         $scope.close = function() {
             oClose("close", 500); // close, but give 500ms for bootstrap to animate
@@ -43,15 +43,34 @@ var UploadFileController = (function() {
         var oBody = new FormData();
         oBody.append('file', this.m_oFile[0]);
 
-        this.m_oProductService.uploadFile(this.m_sWorkspaceId,oBody)
+        this.m_bIsUploading = true;
+        var oController = this;
+        this.m_oProductService.uploadFile(this.m_sWorkspaceId,oBody,this.m_oFile[0].name)
             .success(function(data,status){
-
+                if(status !== 200)
+                {
+                    utilsVexDialogAlertTop("GURU MEDITATION<br>ERROR IN UPLOAD FILE");
+                }
+                else
+                {
+                    var oDialog = utilsVexDialogAlertBottomRightCorner("GURU MEDITATION<br>FILE UPLOADED");
+                    utilsVexCloseDialogAfter(5000,oDialog);
+                }
+                oController.cleanDragAndDrop();
+                oController.m_bIsUploading = false;
             })
             .error(function(data){
+                oController.m_bIsUploading = false;
+                oController.cleanDragAndDrop();
+                utilsVexDialogAlertTop("GURU MEDITATION<br>ERROR IN UPLOAD FILE");
 
             });
         
         return true;
+    }
+    UploadFileController.prototype.cleanDragAndDrop = function()
+    {
+        this.m_oFile[0] = null;
     }
     UploadFileController.$inject = [
         '$scope',
