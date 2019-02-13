38a2d2e87226fcb117c5d6e7917f96ef7e016c2b
a.corrado <a.corrado@fadeout.it>
2019-02-13 14:32:24 +0100

    Developed upload file dialog fixes + wasdilib upload file

--- libraries/jwasdilib/jwasdilib/src/main/java/wasdi/jwasdilib/App.java
+++ libraries/jwasdilib/jwasdilib/src/main/java/wasdi/jwasdilib/App.java
@@ -15,8 +15,7 @@ public class App
         WasdiLib oLib = new WasdiLib();
         
         oLib.init();
-        
-        
+        testUploadFile(oLib);
         //HashMap<String, String> asHeaders = new HashMap<>();
         //asHeaders.put("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
         //asHeaders.put("Host", "gpod.eo.esa.int");
@@ -36,18 +35,18 @@ public class App
         System.out.println("File Path " + sPath);
 */
         
-        oLib.openWorkspace("FirstWS");
-        
-        System.out.println(oLib.getParam("INPUTFILE"));
-        System.out.println(oLib.getParam("SOGLIA"));
-        System.out.println(oLib.getParam("PROVA"));
-        System.out.println(oLib.getParam("MANCA"));
-        
-        long lStartTime = System.currentTimeMillis();
-        String sPath = oLib.getFullProductPath("out.tif");
-        System.out.println("Donwload Time: " + (System.currentTimeMillis() - lStartTime) + " ms");
-        
-        System.out.println("File Path " + sPath);
+//        oLib.openWorkspace("FirstWS");
+//        
+//        System.out.println(oLib.getParam("INPUTFILE"));
+//        System.out.println(oLib.getParam("SOGLIA"));
+//        System.out.println(oLib.getParam("PROVA"));
+//        System.out.println(oLib.getParam("MANCA"));
+//        
+//        long lStartTime = System.currentTimeMillis();
+//        String sPath = oLib.getFullProductPath("out.tif");
+//        System.out.println("Donwload Time: " + (System.currentTimeMillis() - lStartTime) + " ms");
+//        
+//        System.out.println("File Path " + sPath);
 
         
         /*
@@ -67,5 +66,10 @@ public class App
         */
         System.out.println("JWasdiLib Test Done");
         
+    }
+    
+    public static void testUploadFile(WasdiLib oLib)
+    {
+    	oLib.uploadFile("S1B_IW_GRDH_1SDV_20180101T001334_20180101T001359_008970_010027_F7B8.zip");
     }
 }
