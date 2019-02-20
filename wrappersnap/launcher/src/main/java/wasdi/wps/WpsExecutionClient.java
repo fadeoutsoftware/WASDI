7b802ebb61e44485a0368242e695ceb6653dcbe2
Cristiano Nattero <c.nattero@fadeout.it>
2019-02-20 12:11:12 +0100

    WpsExecutionClient and corresponding Factory

---
+++ wrappersnap/launcher/src/main/java/wasdi/wps/WpsExecutionClient.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Cristiano Nattero on 2019-02-20
+ * 
+ * Fadeout software
+ *
+ */
+package wasdi.wps;
+
+/**
+ * @author c.nattero
+ *
+ */
+public class WpsExecutionClient {
+
+}
