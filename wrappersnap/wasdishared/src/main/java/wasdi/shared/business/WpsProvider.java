43e48a2d739d9d15b6793367a3a08822919e6a3a
Cristiano Nattero <cristiano.nattero@gmail.com>
2019-03-08 09:54:48 +0100

    Added fields

--- wrappersnap/wasdishared/src/main/java/wasdi/shared/business/WpsProvider.java
+++ wrappersnap/wasdishared/src/main/java/wasdi/shared/business/WpsProvider.java
@@ -10,8 +10,15 @@
 package wasdi.shared.business;
 
 public class WpsProvider {
-	//MAYBE add a name field, should it become necessary 
+ 
+	//WASDI address to call
 	private String address;
+	
+	//label for the provider
+	private String provider;
+	
+	//real provider to be called by our proxy
+	private String providerUrl;
 
 	public String getAddress() {
 		return address;
@@ -19,5 +26,21 @@ public class WpsProvider {
 
 	public void setAddress(String wPS) {
 		address = wPS;
+	}
+
+	public String getProvider() {
+		return provider;
+	}
+
+	public void setProvider(String provider) {
+		this.provider = provider;
+	}
+
+	public String getProviderUrl() {
+		return providerUrl;
+	}
+
+	public void setProviderUrl(String providerUrl) {
+		this.providerUrl = providerUrl;
 	}
 }
