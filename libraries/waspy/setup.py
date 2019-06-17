0f82a457b626744f18de82a8bcd4f5816e221c74
Cristiano Nattero <cristiano.nattero@gmail.com>
2019-06-17 17:40:05 +0200

    Setup for creating a pip package

--- libraries/waspy/setup.py
+++ libraries/waspy/setup.py
@@ -7,22 +7,23 @@ Created on 11 Jun 2018
 """
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+with open("README.md", "r") as oFh:
+    sLong_description = oFh.read()
+
 
 setuptools.setup(
     name="wasdi",
-    version="0.1.1",
+    version="0.1.2",
     author="FadeOut Software",
     author_email="info@fadeout.biz",
     description="The Wasdi Python library",
-    long_description=long_description,
+    long_description=sLong_description,
     long_description_content_type="text/markdown",
     url="http://www.wasdi.net",
     packages=setuptools.find_packages(),
-    classifiers=(
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ),
+    ],
 )
