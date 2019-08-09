eb87142be8d6d5110c7b8166cb874187a78f35ae
Cristiano Nattero <cristiano.nattero@gmail.com>
2019-08-09 11:53:21 +0200

    Improved log and formatting

--- wasdiwebserver/src/main/java/it/fadeout/rest/resources/WasdiResource.java
+++ wasdiwebserver/src/main/java/it/fadeout/rest/resources/WasdiResource.java
@@ -6,30 +6,30 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 
+import it.fadeout.Wasdi;
 import wasdi.shared.viewmodels.PrimitiveResult;
-
 
 @Path("wasdi")
 public class WasdiResource {
-	
+
 	@Context
 	ServletConfig m_oServletConfig;
-	
+
 	@GET
 	@Path("/hello")
-	@Produces({"application/xml", "application/json", "text/xml"})	
-	public PrimitiveResult hello()
-	{
+	@Produces({ "application/xml", "application/json", "text/xml" })
+	public PrimitiveResult hello() {
+		Wasdi.DebugLog("WasdiResource.hello");
 		PrimitiveResult oResult = new PrimitiveResult();
 		oResult.setStringValue("Hello Wasdi!!");
 		return oResult;
 	}
-	
+
 	@GET
 	@Path("/version")
-	@Produces({"application/xml", "application/json", "text/xml"})	
-	public PrimitiveResult serverVersion()
-	{
+	@Produces({ "application/xml", "application/json", "text/xml" })
+	public PrimitiveResult serverVersion() {
+		Wasdi.DebugLog("WasdiResource.serverVersion");
 		PrimitiveResult oResult = new PrimitiveResult();
 		oResult.setStringValue(m_oServletConfig.getInitParameter("ServerVersion"));
 		return oResult;
