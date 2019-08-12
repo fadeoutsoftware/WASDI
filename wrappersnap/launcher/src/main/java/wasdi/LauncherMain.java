8504b251fa342ea1095b965a41266be544db495d
Cristiano Nattero <cristiano.nattero@gmail.com>
2019-08-12 10:31:50 +0200

    Removed unused import

--- wrappersnap/launcher/src/main/java/wasdi/LauncherMain.java
+++ wrappersnap/launcher/src/main/java/wasdi/LauncherMain.java
@@ -26,7 +26,6 @@ import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
 import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 import org.apache.log4j.xml.DOMConfigurator;
