f133a244fcbfaf2bb31c6a1663c94f32734b7b66
Cristiano Nattero <cristiano.nattero@gmail.com>
2019-12-20 13:17:30 +0100

    Created QueryExecutorCREODIAS

---
+++ wrappersnap/wasdishared/src/main/java/wasdi/shared/opensearch/QueryExecutorCREODIAS.java
@@ -0,0 +1,51 @@
+/**
+ * Created by Cristiano Nattero on 2019-12-20
+ * 
+ * Fadeout software
+ *
+ */
+package wasdi.shared.opensearch;
+
+import org.apache.abdera.i18n.templates.Template;
+
+/**
+ * @author c.nattero
+ *
+ */
+public class QueryExecutorCREODIAS extends QueryExecutor {
+
+	/**
+	 * 
+	 */
+	public QueryExecutorCREODIAS() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see wasdi.shared.opensearch.QueryExecutor#getUrlPath()
+	 */
+	@Override
+	protected String[] getUrlPath() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see wasdi.shared.opensearch.QueryExecutor#getTemplate()
+	 */
+	@Override
+	protected Template getTemplate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see wasdi.shared.opensearch.QueryExecutor#getCountUrl(java.lang.String)
+	 */
+	@Override
+	protected String getCountUrl(String sQuery) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
