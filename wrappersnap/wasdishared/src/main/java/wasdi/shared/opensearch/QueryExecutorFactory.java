c30e66062c4e9e9f62be5953c866df85f765a9e7
Cristiano Nattero <cristiano.nattero@gmail.com>
2019-12-23 18:42:21 +0100

    Safer handling of returned vars

--- wrappersnap/wasdishared/src/main/java/wasdi/shared/opensearch/QueryExecutorFactory.java
+++ wrappersnap/wasdishared/src/main/java/wasdi/shared/opensearch/QueryExecutorFactory.java
@@ -25,21 +25,21 @@ public class QueryExecutorFactory {
 	static {
 		Utils.debugLog("QueryExecutorFactory");
 		final Map<String, Supplier<QueryExecutor>> aoMap = new HashMap<>();
-		
+
 		aoMap.put("ONDA", QueryExecutorONDA::new);
 		aoMap.put("SENTINEL", QueryExecutorSENTINEL::new);
 		aoMap.put("MATERA", QueryExecutorMATERA::new);
 		aoMap.put("PROBAV", QueryExecutorPROBAV::new);
 		aoMap.put("FEDEO", QueryExecutorFEDEO::new);
 		aoMap.put("CREODIAS", QueryExecutorCREODIAS::new);
-		
+
 		s_aoExecutors = Collections.unmodifiableMap(aoMap);
 		Utils.debugLog("QueryExecutorFactory.static constructor, s_aoExecutors content:");
 		for (String sKey : s_aoExecutors.keySet()) {
 			Utils.debugLog("QueryExecutorFactory.s_aoExecutors key: " + sKey);
 		}
 	}
-	
+
 	private QueryExecutor supply(String sProvider) {
 		Utils.debugLog("QueryExecutorFactory.QueryExecutor( "+sProvider+" )");
 		QueryExecutor oExecutor = null;
@@ -53,24 +53,34 @@ public class QueryExecutorFactory {
 		}
 		return oExecutor;	
 	}
-	
+
 	public QueryExecutor getExecutor(
 			String sProvider,
 			AuthenticationCredentials oCredentials,
 			String sDownloadProtocol, String sGetMetadata) {
-		Utils.debugLog("QueryExecutorFactory.getExecutor( "+sProvider+" )...");
+		Utils.debugLog("QueryExecutorFactory.getExecutor( " +
+				sProvider + ", <credentials>, " +
+				sDownloadProtocol + ", " + sGetMetadata + " )..."
+				);
 		QueryExecutor oExecutor = null;
-		
+
 		try {
 			oExecutor = supply(sProvider);
-			oExecutor.setCredentials(oCredentials);
-			oExecutor.setMustCollectMetadata(Utils.doesThisStringMeansTrue(sGetMetadata));
-			oExecutor.setDownloadProtocol(sDownloadProtocol);
-			
-		} catch (Exception e) {
-			e.printStackTrace();
+		} catch (Exception oE) {
+			Utils.debugLog("QueryExecutorFactory.getExecutor: " + oE );
 		}
-		
+		if( null != oExecutor) {
+			try {
+				oExecutor.setCredentials(oCredentials);
+				oExecutor.setMustCollectMetadata(Utils.doesThisStringMeansTrue(sGetMetadata));
+				oExecutor.setDownloadProtocol(sDownloadProtocol);
+			} catch (Exception oE1) {
+				Utils.debugLog("QueryExecutorFactory.getExecutor: " + oE1 );
+			}
+		} else {
+			throw new NullPointerException("QueryExecutorFactory.getExecutor: could not get a non-null QueryExecutor" );
+		}
+
 		return oExecutor;
 	}
 
