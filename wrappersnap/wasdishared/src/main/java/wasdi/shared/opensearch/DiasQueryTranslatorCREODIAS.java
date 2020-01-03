5aa1318422300fa179e80ed3b2e8519c16421757
Cristiano Nattero <cristiano.nattero@gmail.com>
2020-01-03 11:55:43 +0100

    Query and Response translators added to git

---
+++ wrappersnap/wasdishared/src/main/java/wasdi/shared/opensearch/DiasQueryTranslatorCREODIAS.java
@@ -0,0 +1,25 @@
+/**
+ * Created by Cristiano Nattero on 2019-12-23
+ * 
+ * Fadeout software
+ *
+ */
+package wasdi.shared.opensearch;
+
+/**
+ * @author c.nattero
+ *
+ */
+public class DiasQueryTranslatorCREODIAS extends DiasQueryTranslator {
+
+	/* (non-Javadoc)
+	 * @see wasdi.shared.opensearch.DiasQueryTranslator#translate(java.lang.String)
+	 */
+	@Override
+	public String translate(String sQuery) {
+		// TODO translate and return the url string
+		//already added prefix: https://finder.creodias.eu/resto/api/collections/
+		return "Sentinel1/search.json?startDate=2019-12-01T00:00:00Z&completionDate=2019-12-10T23:59:59Z&geometry=POLYGON((7.397874989401342+45.00475144371268,10.373746303074263+44.94785607558927,10.389830621260842+43.612039503172866,7.703504034412235+43.809704932512176,7.397874989401342+45.00475144371268))";
+	}
+
+}
