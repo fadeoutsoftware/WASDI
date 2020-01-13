ecf7ffc00ce61317796227fb74e151669a67ca4d
Cristiano Nattero <cristiano.nattero@gmail.com>
2020-01-13 16:47:06 +0100

    Added code to obtain keyCloak Token

--- wrappersnap/launcher/src/main/java/wasdi/filebuffer/CREODIASProviderAdapter.java
+++ wrappersnap/launcher/src/main/java/wasdi/filebuffer/CREODIASProviderAdapter.java
@@ -8,7 +8,11 @@ package wasdi.filebuffer;
 
 import org.apache.log4j.Logger;
 
+import com.google.java.contract.Ensures;
+
 import wasdi.shared.business.ProcessWorkspace;
+import wasdi.shared.opensearch.DiasResponseTranslatorCREODIAS;
+import wasdi.shared.utils.Utils;
 
 /**
  * @author c.nattero
@@ -54,9 +58,21 @@ public class CREODIASProviderAdapter extends ProviderAdapter {
 	 * @see wasdi.filebuffer.ProviderAdapter#GetFileName(java.lang.String)
 	 */
 	@Override
+	@Ensures("sResult!=null")
 	public String GetFileName(String sFileURL) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		if (Utils.isNullOrEmpty(sFileURL)) {
+			m_oLogger.debug("CREODIASProviderAdapter.GetFileName: sFileURL is null or Empty");
+			m_oLogger.fatal("CREODIASProviderAdapter.GetFileName: sFileURL is null or Empty");
+			return "";
+		}
+		
+		String sResult = "";
+		if(sFileURL.contains( DiasResponseTranslatorCREODIAS.SLINK_SEPARATOR_CREODIAS ) ) {
+			int iStart = sFileURL.indexOf(DiasResponseTranslatorCREODIAS.SLINK_SEPARATOR_CREODIAS);
+			sResult = sFileURL.substring(iStart + DiasResponseTranslatorCREODIAS.SLINK_SEPARATOR_CREODIAS.length());
+		}
+		
+		return sResult;
 	}
 
 }
