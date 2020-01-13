64fadc28e52b3b30f2df03321d4929be836ab8b0
Cristiano Nattero <cristiano.nattero@gmail.com>
2020-01-13 16:51:44 +0100

    Added code to obtain keyCloak token

--- wrappersnap/launcher/src/main/java/wasdi/filebuffer/CREODIASProviderAdapter.java
+++ wrappersnap/launcher/src/main/java/wasdi/filebuffer/CREODIASProviderAdapter.java
@@ -6,13 +6,18 @@
  */
 package wasdi.filebuffer;
 
-import org.apache.log4j.Logger;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-import com.google.java.contract.Ensures;
+import org.apache.commons.net.io.Util;
+import org.apache.log4j.Logger;
 
 import wasdi.shared.business.ProcessWorkspace;
 import wasdi.shared.opensearch.DiasResponseTranslatorCREODIAS;
 import wasdi.shared.utils.Utils;
+import wasdi.shared.viewmodels.QueryResultViewModel;
 
 /**
  * @author c.nattero
@@ -23,6 +28,7 @@ public class CREODIASProviderAdapter extends ProviderAdapter {
 	/**
 	 * 
 	 */
+	//TODO uncapitalize method
 	public CREODIASProviderAdapter() {
 		// TODO Auto-generated constructor stub
 	}
@@ -58,21 +64,59 @@ public class CREODIASProviderAdapter extends ProviderAdapter {
 	 * @see wasdi.filebuffer.ProviderAdapter#GetFileName(java.lang.String)
 	 */
 	@Override
-	@Ensures("sResult!=null")
 	public String GetFileName(String sFileURL) throws Exception {
 		if (Utils.isNullOrEmpty(sFileURL)) {
-			m_oLogger.debug("CREODIASProviderAdapter.GetFileName: sFileURL is null or Empty");
 			m_oLogger.fatal("CREODIASProviderAdapter.GetFileName: sFileURL is null or Empty");
 			return "";
 		}
 		
+		
 		String sResult = "";
-		if(sFileURL.contains( DiasResponseTranslatorCREODIAS.SLINK_SEPARATOR_CREODIAS ) ) {
-			int iStart = sFileURL.indexOf(DiasResponseTranslatorCREODIAS.SLINK_SEPARATOR_CREODIAS);
-			sResult = sFileURL.substring(iStart + DiasResponseTranslatorCREODIAS.SLINK_SEPARATOR_CREODIAS.length());
+		try {
+		sResult = sFileURL.split(DiasResponseTranslatorCREODIAS.SLINK_SEPARATOR_CREODIAS)[DiasResponseTranslatorCREODIAS.IPOSITIONOF_FILENAME];
+		} catch (Exception oE) {
+			this.m_oLogger.error(oE);
 		}
 		
 		return sResult;
+	}
+	
+	private String obtainKeycloakToken(QueryResultViewModel oResult) {
+		try {
+			URL oURL = new URL("https://auth.creodias.eu/auth/realms/dias/protocol/openid-connect/token");
+
+			HttpURLConnection oConnection = (HttpURLConnection) oURL.openConnection();
+			oConnection.setRequestMethod("POST");
+			oConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+			oConnection.setDoOutput(true);
+			oConnection.getOutputStream().write("client_id=CLOUDFERRO_PUBLIC&password=<PASSWORDHERE>&username=<USERHERE>&grant_type=password".getBytes());
+			int iStatus = oConnection.getResponseCode();
+			System.out.println("Response status: " + iStatus);
+			if( iStatus == 200) {
+				InputStream oInputStream = oConnection.getInputStream();
+				ByteArrayOutputStream oBytearrayOutputStream = new ByteArrayOutputStream();
+				if(null!=oInputStream) {
+					Util.copyStream(oInputStream, oBytearrayOutputStream);
+					String sResult = oBytearrayOutputStream.toString();
+					System.out.println(sResult);
+					return sResult;
+				}
+			} else {
+				ByteArrayOutputStream oBytearrayOutputStream = new ByteArrayOutputStream();
+				InputStream oErrorStream = oConnection.getErrorStream();
+				Util.copyStream(oErrorStream, oBytearrayOutputStream);
+				String sMessage = oBytearrayOutputStream.toString();
+				System.out.println(sMessage);
+
+			}
+
+
+		} catch (Exception oE) {
+			// TODO Auto-generated catch block
+			oE.printStackTrace();
+		}
+
+		return null;
 	}
 
 }
