319c494eb25cf362e891c417309a90abbefc3462
Cristiano Nattero <cristiano.nattero@gmail.com>
2020-02-03 18:37:18 +0100

    Query translator created for SOBLOO

---
+++ wrappersnap/wasdishared/src/main/java/wasdi/shared/opensearch/DiasQueryTranslatorSOBLOO.java
@@ -0,0 +1,24 @@
+/**
+ * Created by Cristiano Nattero on 2020-02-03
+ * 
+ * Fadeout software
+ *
+ */
+package wasdi.shared.opensearch;
+
+/**
+ * @author c.nattero
+ *
+ */
+public class DiasQueryTranslatorSOBLOO extends DiasQueryTranslator {
+
+	/* (non-Javadoc)
+	 * @see wasdi.shared.opensearch.DiasQueryTranslator#translate(java.lang.String)
+	 */
+	@Override
+	protected String translate(String sQuery) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
