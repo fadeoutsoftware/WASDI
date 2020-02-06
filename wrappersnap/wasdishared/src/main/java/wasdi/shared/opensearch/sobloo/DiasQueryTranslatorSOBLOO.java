634a167049b6a1337f676b91dbecffa9272f562b
Cristiano Nattero <cristiano.nattero@gmail.com>
2020-02-06 17:07:51 +0100

    Identify query portion and pass it to parser

--- wrappersnap/wasdishared/src/main/java/wasdi/shared/opensearch/sobloo/DiasQueryTranslatorSOBLOO.java
+++ wrappersnap/wasdishared/src/main/java/wasdi/shared/opensearch/sobloo/DiasQueryTranslatorSOBLOO.java
@@ -11,6 +11,9 @@ import org.json.JSONObject;
 import com.google.common.base.Preconditions;
 
 import wasdi.shared.opensearch.DiasQueryTranslator;
+import wasdi.shared.opensearch.QueryTranslationParser;
+import wasdi.shared.utils.Utils;
+import wasdi.shared.utils.WasdiFileUtils;
 
 /**
  * @author c.nattero
@@ -18,7 +21,6 @@ import wasdi.shared.opensearch.DiasQueryTranslator;
  */
 public class DiasQueryTranslatorSOBLOO extends DiasQueryTranslator {
 	
-	private JSONObject m_oJSONConf;
 
 	//TODO one class per mission, w/ 2 maps: WASDI to SOBLOO keys, and WASDI to SOBLOO values 
 	
@@ -26,8 +28,35 @@ public class DiasQueryTranslatorSOBLOO extends DiasQueryTranslator {
 	 * @see wasdi.shared.opensearch.DiasQueryTranslator#translate(java.lang.String)
 	 */
 	@Override
-	protected String translate(String sQuery) {
-		return "f=acquisition.missionName:eq:Sentinel-1A";
+	protected String translate(String sQueryFromClient) {
+		Preconditions.checkNotNull(sQueryFromClient, "DiasQueryTranslatorSOBLOO.translate: query is null");
+		
+		String sResult = null;
+		try {
+			JSONObject oAppConf = WasdiFileUtils.loadJsonFromFile(m_sAppConfigPath);
+			JSONObject oParseConf = WasdiFileUtils.loadJsonFromFile(m_sParserConfigPath);
+			String sQuery = this.prepareQuery(sQueryFromClient);
+			//TODO parse footprint
+			//TODO parse time frame
+
+			for (Object oObject : oAppConf.optJSONArray("missions")) {
+				JSONObject oJson = (JSONObject) oObject;
+				String sValue = oJson.optString("indexvalue", null);
+				if(sQuery.contains(sValue)) {
+					//todo isolate relevant portion of query
+					int iStart = sQuery.indexOf(sValue);
+					int iEnd = sQuery.indexOf(')', iStart);
+					String sQueryPart = sQuery.substring(iStart, iEnd);
+					QueryTranslationParser oParser = new QueryTranslationParser(oParseConf.optJSONObject(sValue), oJson);
+					sResult += oParser.parse(sQueryPart);
+				}
+			}
+			
+		} catch (Exception oE) {
+			Utils.debugLog("DiasQueryTranslatorSOBLOO.translate( " + sQueryFromClient + " ): " + oE);
+		}
+
+		return sResult;
 	}
 
 }
