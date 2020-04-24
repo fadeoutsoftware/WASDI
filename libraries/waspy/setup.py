c51f1a020c58f266e48cbb86a9a1de95914d4ff9
pcampanella <p.campanella@fadeout.it>
2020-04-24 10:27:47 +0200

    upated lib version for executeProcessor

--- libraries/waspy/setup.py
+++ libraries/waspy/setup.py
@@ -15,7 +15,7 @@ with io.open("README.md", "r", encoding="utf8") as oFh:
 
 setuptools.setup(
     name="wasdi",
-    version="0.3.6",
+    version="0.4.1",
     author="FadeOut Software",
     author_email="info@fadeout.biz",
     description="The Wasdi Python library",
