e6a7eb3ce66f084cb20aeda7cbb8488438a7c7a7
Cristiano Nattero <cristiano.nattero@gmail.com>
2020-04-27 12:20:50 +0200

    Stubs for Query and Response translators

---
+++ wrappersnap/wasdishared/src/main/java/wasdi/shared/opensearch/eodc/DiasQueryTranslatorEODC.java
@@ -0,0 +1,44 @@
+/**
+ * Created by Cristiano Nattero on 2020-04-27
+ * 
+ * Fadeout software
+ *
+ */
+package wasdi.shared.opensearch.eodc;
+
+import wasdi.shared.opensearch.DiasQueryTranslator;
+
+/**
+ * @author c.nattero
+ *
+ */
+public class DiasQueryTranslatorEODC extends DiasQueryTranslator {
+
+	/* (non-Javadoc)
+	 * @see wasdi.shared.opensearch.DiasQueryTranslator#translate(java.lang.String)
+	 */
+	@Override
+	protected String translate(String sQueryFromClient) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see wasdi.shared.opensearch.DiasQueryTranslator#parseTimeFrame(java.lang.String)
+	 */
+	@Override
+	protected String parseTimeFrame(String sQuery) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see wasdi.shared.opensearch.DiasQueryTranslator#parseFootPrint(java.lang.String)
+	 */
+	@Override
+	protected String parseFootPrint(String sQuery) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ wrappersnap/wasdishared/src/main/java/wasdi/shared/opensearch/eodc/DiasResponseTranslatorEODC.java
@@ -0,0 +1,29 @@
+/**
+ * Created by Cristiano Nattero on 2020-04-27
+ * 
+ * Fadeout software
+ *
+ */
+package wasdi.shared.opensearch.eodc;
+
+import java.util.List;
+
+import wasdi.shared.opensearch.DiasResponseTranslator;
+import wasdi.shared.viewmodels.QueryResultViewModel;
+
+/**
+ * @author c.nattero
+ *
+ */
+public class DiasResponseTranslatorEODC extends DiasResponseTranslator {
+
+	/* (non-Javadoc)
+	 * @see wasdi.shared.opensearch.DiasResponseTranslator#translateBatch(java.lang.String, boolean, java.lang.String)
+	 */
+	@Override
+	public List<QueryResultViewModel> translateBatch(String sJson, boolean bFullViewModel, String sDownloadProtocol) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
