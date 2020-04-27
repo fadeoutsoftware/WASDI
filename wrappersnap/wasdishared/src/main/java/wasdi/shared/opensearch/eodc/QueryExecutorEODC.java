7b70c06e0a6758b5fd9499a307bd6441bdea459b
Cristiano Nattero <cristiano.nattero@gmail.com>
2020-04-27 12:18:40 +0200

    Created QueryExecutorEODC

---
+++ wrappersnap/wasdishared/src/main/java/wasdi/shared/opensearch/eodc/QueryExecutorEODC.java
@@ -0,0 +1,46 @@
+/**
+ * Created by Cristiano Nattero on 2020-04-27
+ * 
+ * Fadeout software
+ *
+ */
+package wasdi.shared.opensearch.eodc;
+
+import org.apache.abdera.i18n.templates.Template;
+
+import wasdi.shared.opensearch.QueryExecutor;
+
+/**
+ * @author c.nattero
+ *
+ */
+public class QueryExecutorEODC extends QueryExecutor {
+
+	/* (non-Javadoc)
+	 * @see wasdi.shared.opensearch.QueryExecutor#getUrlPath()
+	 */
+	@Override
+	protected String[] getUrlPath() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see wasdi.shared.opensearch.QueryExecutor#getTemplate()
+	 */
+	@Override
+	protected Template getTemplate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see wasdi.shared.opensearch.QueryExecutor#getCountUrl(java.lang.String)
+	 */
+	@Override
+	protected String getCountUrl(String sQuery) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
