719fffea8f522192661cfd129d2de5c1527d36f5
pcampanella <p.campanella@fadeout.it>
2020-04-27 17:12:28 +0200

    First implementation of EODC Translator

--- wrappersnap/wasdishared/src/main/java/wasdi/shared/opensearch/eodc/DiasResponseTranslatorEODC.java
+++ wrappersnap/wasdishared/src/main/java/wasdi/shared/opensearch/eodc/DiasResponseTranslatorEODC.java
@@ -6,9 +6,18 @@
  */
 package wasdi.shared.opensearch.eodc;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import wasdi.shared.opensearch.DiasResponseTranslator;
+import wasdi.shared.utils.Utils;
 import wasdi.shared.viewmodels.QueryResultViewModel;
 
 /**
@@ -16,14 +25,83 @@ import wasdi.shared.viewmodels.QueryResultViewModel;
  *
  */
 public class DiasResponseTranslatorEODC extends DiasResponseTranslator {
+	
+	static ObjectMapper s_oMapper = new ObjectMapper();
 
 	/* (non-Javadoc)
 	 * @see wasdi.shared.opensearch.DiasResponseTranslator#translateBatch(java.lang.String, boolean, java.lang.String)
 	 */
 	@Override
 	public List<QueryResultViewModel> translateBatch(String sJson, boolean bFullViewModel, String sDownloadProtocol) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		
+		ArrayList<QueryResultViewModel> aoResults = new ArrayList<QueryResultViewModel>();
+		
+		try {
+			// Convert the string in a Json
+			Map<String, Object> aoJsonResults = s_oMapper.readValue(sJson, new TypeReference<Map<String, Object>>() {});
+			
+			// Get the childs
+			Map<String, Object> aoChilds = (Map<String, Object>) aoJsonResults.get("csw:GetRecordsResponse");
+			
+			// Access the SearchResults child
+			Map<String, Object> aoSearchResults = (Map<String, Object>) aoChilds.get("csw:SearchResults");
+			
+			// Access the list of records
+			ArrayList<Map<String, Object>> aoRecords = (ArrayList<Map<String, Object>>) aoSearchResults.get("csw:Record");
+			
+			// For each record
+			for (Map<String, Object> oRecord : aoRecords) {
+				
+				try {
+					ArrayList<Map<String, Object>> aoReferences = (ArrayList<Map<String, Object>>) oRecord.get("dct:references");
+					
+					String sLink = "";
+					
+					for (Map<String, Object> oReference : aoReferences) {
+						
+						String sScheme = (String) oReference.get("@scheme");
+						if (sScheme.equals("offlineAccess")) {
+							sLink = (String) oReference.get("#text");
+						}
+					}
+					
+					Map<String, Object> oBoundingBox = (Map<String,Object>) oRecord.get("ows:BoundingBox");
+					
+					String sLowerCorner = (String) oBoundingBox.get("ows:LowerCorner");
+					String sUpperCorner = (String) oBoundingBox.get("ows:UpperCorner");
+					
+					String [] asLatLonLower = sLowerCorner.split(" ");
+					String [] asLatLonUpper = sUpperCorner.split(" ");
+					
+					String sFootPrint = "MULTIPOLYGON (((" + asLatLonLower[1] + " " + asLatLonLower [0] + "," + asLatLonLower[1] + " " + asLatLonUpper[0] + "," + asLatLonUpper[1] + " " + asLatLonUpper[0] + "," + asLatLonUpper[1] + " " + asLatLonLower [0] + "," + asLatLonLower[1] + " " + asLatLonLower [0] + ")))";
+					
+					// Create WASDI View Model
+					QueryResultViewModel oResViewModel = new QueryResultViewModel();
+					
+					oResViewModel.setTitle( (String) oRecord.get("dc:title"));
+					oResViewModel.setProvider("EODC");
+					oResViewModel.setSummary("");
+					oResViewModel.setLink("file://" + sLink);
+					oResViewModel.setFootprint(sFootPrint);
+					oResViewModel.getProperties().put("format", (String) oRecord.get("dc:format"));
+					oResViewModel.getProperties().put("creationDate", (String) oRecord.get("dc:date"));					
+					
+					aoResults.add(oResViewModel);
+				}
+				catch (Exception oInnerEx) {
+					Utils.debugLog("Exception:" + oInnerEx.toString());
+				}
 
+			}
+			
+			Utils.debugLog("Conversion done");
+			
+		} 
+		catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		
+		return aoResults;
+	}
 }
