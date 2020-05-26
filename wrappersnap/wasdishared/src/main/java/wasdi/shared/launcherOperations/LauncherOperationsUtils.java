fc16a0e9170d5a46f77cfb834d6fd77f22b76bf4
Cristiano Nattero <cristiano.nattero@gmail.com>
2020-05-26 09:33:00 +0200

    [WIP] Utils - check if an operation can spawn children

---
+++ wrappersnap/wasdishared/src/main/java/wasdi/shared/launcherOperations/LauncherOperationsUtils.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Cristiano Nattero on 2020-05-25
+ * 
+ * Fadeout software
+ *
+ */
+package wasdi.shared.launcherOperations;
+
+/**
+ * @author c.nattero
+ *
+ */
+public class LauncherOperationsUtils {
+	public boolean canOperationSpawnChildren(String sOperation) {
+		//todo implement
+		return false;
+	}
+}
