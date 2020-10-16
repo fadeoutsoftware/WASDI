81e53bc23be1cbdccc7dcd01cdb1fcf98d69b145
pcampanella <p.campanella@fadeout.it>
2020-10-16 19:06:42 +0200

    unused imports

--- wasdiwebserver/src/main/java/it/fadeout/Wasdi.java
+++ wasdiwebserver/src/main/java/it/fadeout/Wasdi.java
@@ -11,7 +11,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.lang.reflect.Field;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
