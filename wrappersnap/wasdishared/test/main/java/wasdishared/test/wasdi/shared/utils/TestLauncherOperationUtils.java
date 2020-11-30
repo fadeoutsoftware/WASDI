f73c8a0cc0eaa552a45887a855cb5e4492ffbc03
Marco <m.menapace@fadeout.it>
2020-11-30 11:19:36 +0100

    fixed minor bug on cast
    long operation between int values now cast to 2 long values

---
+++ wrappersnap/wasdishared/test/main/java/wasdishared/test/wasdi/shared/utils/TestLauncherOperationUtils.java
@@ -0,0 +1,31 @@
+/**
+ * Created by Marco Menapace on 2020-11-26
+ * 
+ * Fadeout software
+ *
+ */
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.params.Test;
+
+import wasdi.shared.launcherOperations.LauncherOperationsUtils;
+
+package wasdishared.test.wasdi.shared.utils;
+
+
+public class TestLauncherOperationUtils {
+	
+	@Test
+	public void callTests(String sDate, Long lEpoch) {
+		testValid();
+		testError();
+	}
+	
+	final void testValid() {
+		assertTrue(LauncherOperationsUtils.isValidResourceOperation("INGEST"));
+	}
+	final void testError() {
+		// Assert false
+		assertTrue(!LauncherOperationsUtils.isValidResourceOperation("vndoicdsniovn"));
+	}
+
+}
