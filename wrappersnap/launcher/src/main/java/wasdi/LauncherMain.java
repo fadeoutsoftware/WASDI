d9ab00d17d36792450e1d2c26bb7ec6faf005d75
Cristiano Nattero <c.nattero@fadeout.it>
2020-12-09 16:23:08 +0100

    Get missing BBOX from product view model

--- wrappersnap/launcher/src/main/java/wasdi/LauncherMain.java
+++ wrappersnap/launcher/src/main/java/wasdi/LauncherMain.java
@@ -118,6 +118,7 @@ import wasdi.shared.utils.EndMessageProvider;
 import wasdi.shared.utils.FtpClient;
 import wasdi.shared.utils.SerializationUtils;
 import wasdi.shared.utils.Utils;
+import wasdi.shared.viewmodels.GeorefProductViewModel;
 import wasdi.shared.viewmodels.MetadataViewModel;
 import wasdi.shared.viewmodels.ProductViewModel;
 import wasdi.shared.viewmodels.PublishBandResultViewModel;
@@ -2825,10 +2826,10 @@ public class LauncherMain implements ProcessWorkspaceUpdateSubscriber {
 
 	/**
 	 * Converts a product in a ViewModel, add it to the workspace and send it to the
-	 * rabbit queue The method is Safe: controls if the products already exists and
+	 * rabbit queue. The method is Safe: it controls if the products already exists and
 	 * if it is already added to the workspace
 	 * 
-	 * @param oProductViewModel               View Model... if null, read it from the product in sFileName
+	 * @param oProductViewModel	View Model... if null, read it from the product in sFileName
 	 * @param sFullPathFileName File Name
 	 * @param sWorkspace        Workspace
 	 * @param sExchange         Queue Id
@@ -2851,7 +2852,15 @@ public class LauncherMain implements ProcessWorkspaceUpdateSubscriber {
 
 		WasdiProductReader oReadProduct = new WasdiProductReader(oFile);
 
-		// Get the Boundig Box
+		// Get the Bounding Box
+		if(Utils.isNullOrEmpty(sBBox)) {
+			try {
+				GeorefProductViewModel oGeorefProductViewModel = (GeorefProductViewModel) oProductViewModel;
+				sBBox = oGeorefProductViewModel.getBbox();
+			}catch (Exception oE) {
+				s_oLogger.warn("LauncherMain.AddProductToDbAndSendToRabbit: could not extract BBox from GeorefProductViewModel due to: " + oE);
+			}
+		}
 		if (Utils.isNullOrEmpty(sBBox)) {
 			s_oLogger.debug("LauncherMain.AddProductToDbAndSendToRabbit: bbox not set. Try to auto get it ");
 			sBBox = oReadProduct.getProductBoundingBox();
