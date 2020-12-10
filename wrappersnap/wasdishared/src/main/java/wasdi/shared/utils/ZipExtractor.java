3ed3ececbdd8de653bdac17d713d7ae10a9922de
Marco <m.menapace@fadeout.it>
2020-12-10 14:25:45 +0100

    New utils for unzipping
    The new utils unzip files and checks the size of each single file, the number of files, and the total size. All these checks are done t avoid attacks like:
    - Zip slips
    - Zip Bomb
    Checks documentation for references to vulnerabilities

---
+++ wrappersnap/wasdishared/src/main/java/wasdi/shared/utils/ZipExtractor.java
@@ -0,0 +1,253 @@
+/*
+ * Copyright (c) Fadeout Software 2020. Marco Menapace
+ *
+ */
+
+package wasdi.shared.utils;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.security.SecureRandom;
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
+import java.util.Comparator;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+/**
+ * Utility class for zip extraction operation with some security considerations.
+ *
+ * @see <a href="http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2018-1263">CVE-2018-1263</a>
+ * @see <a href="http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2018-16131">CVE-2018-16131</a>
+ */
+public class ZipExtractor {
+    // These parameters must be instantiated using configurations values
+    static final int BUFFER = 512;
+    long m_lToobigtotal = 1024L * 1024L * 1024L; // Max size of unzipped total Data, 1 GB
+    long m_lToobigsingle = 1024L * 1024L * 512L; // Max size of unzipped total Data, 512 MB
+    int m_lToomany = 1024; // Maximum number of files that can be extracted
+
+    /**
+     * Safer version of the unzip method.
+     * This method take in account the total dimension extracted and the dimension for each file.
+     * If the extraction fails a clean method should be invoked, removing all the files extracted till
+     * that point.
+     * Try-with-resources approach for the ZipInputStream.
+     * The extraction creates a temporary directory which is programmatically deleted if
+     * - errors occurs (I.E. a file is bigger than the limit imposed)
+     * - the unzip is completed without errors
+     *
+     * @param filename the filename of the Zip file to be extracted
+     * @param path     The intended path in which the file should be extracted
+     * @throws java.io.IOException Throws IO exception in case the zip file is not founded
+     */
+    public String unzip(String filename, String path) throws java.io.IOException {
+        FileInputStream fis = new FileInputStream(filename);
+        ZipEntry entry;
+        int entries = 0;
+        long total = 0;
+        long single = 0;
+        int iRandom = Math.abs(new SecureRandom().nextInt());
+        String sTemp = "tmp-" + iRandom + "/";
+        String sTempPath = path + sTemp;
+
+        if (new File(sTempPath).mkdir()) System.out.println("Temp directory created");
+        else throw new IOException("Can't create temporary dir " + sTempPath);
+
+        try (ZipInputStream zis = new ZipInputStream(new BufferedInputStream(fis))) {
+            while ((entry = zis.getNextEntry()) != null) {
+                System.out.println("Extracting: " + entry);
+                int count;
+                byte[] data = new byte[BUFFER];
+                // Write the files to the disk, but ensure that the filename is valid,
+                // and that the file is not insanely big
+                String name = validateFilename(sTempPath + entry.getName(), sTempPath); // throws exception in case
+                // Random used to mitigate attacks
+                //File oTempDir = new File(path + "/tmp-" +iRandom+"/");
+                if (entry.isDirectory()) {
+                    System.out.println("Creating directory " + name);
+                    if (new File(name).mkdir()) System.out.println("Directory created");
+                    entries++; // count also a directory creation as an entry
+                    continue;
+                }
+                FileOutputStream fos = new FileOutputStream(name);
+                BufferedOutputStream dest = new BufferedOutputStream(fos, BUFFER);
+                while (total + BUFFER <= m_lToobigtotal &&
+                        single + BUFFER <= m_lToobigsingle &&
+                        (count = zis.read(data, 0, BUFFER)) != -1) {
+                    dest.write(data, 0, count);
+                    total += count;
+                    single += count;
+                }
+                dest.flush();
+                dest.close();
+                zis.closeEntry();
+                entries++;
+                if (single + BUFFER > m_lToobigsingle) {
+                    cleanTempDir(sTempPath, sTemp);
+                    throw new IllegalStateException("File being unzipped is too big. The limit is " + humanReadableByteCountSI(m_lToobigsingle));
+                }
+                if (total + BUFFER > m_lToobigtotal) {
+                    cleanTempDir(sTempPath, sTemp);
+                    throw new IllegalStateException("File extraction interrupted because total dimension is over extraction limits. The limit is " + humanReadableByteCountSI(m_lToobigtotal));
+                }
+                if (entries > m_lToomany) {
+                    cleanTempDir(sTempPath, sTemp);
+                    throw new IllegalStateException("Too many files to unzip.");
+                }
+                single = 0; // resets single file byte-counter
+            }
+            /// IF everything went well cp temp content to original folder (overwrite it's fine) and delete temp dir
+            cleanTempDir(sTempPath, sTemp);
+        }
+        return sTempPath;
+    }
+
+    /**
+     * Instantiates a ZipExtractor with default parameters
+     */
+    public ZipExtractor() {
+    }
+
+    /**
+     * Instantiates a ZipExtractor passing 3 critical parameters (in bytes)
+     *
+     * @param toobigtotal     the total maximum size allowed for extraction
+     * @param m_lToobigsingle the maximum single size for each file
+     * @param m_lToomany      the maximum number of files allowed to be extracted
+     */
+    public ZipExtractor(long toobigtotal, long m_lToobigsingle, int m_lToomany) {
+        this.m_lToobigtotal = toobigtotal;
+        this.m_lToobigsingle = m_lToobigsingle;
+        this.m_lToomany = m_lToomany;
+    }
+
+    /**
+     * Checks that the the output dir is coherent with the current dir.
+     * This methods filters out directory traversal attempts.
+     *
+     * @param filename    the file name of the zip file to be extracted
+     * @param intendedDir the intended directory where the extraction must be done
+     * @return the canonical path of the file
+     * @throws java.io.IOException in case of the file is outside target extraction directory
+     */
+    private static String validateFilename(String filename, String intendedDir)
+            throws java.io.IOException {
+        File f = new File(filename);
+        String canonicalPath = f.getCanonicalPath();
+
+        File iD = new File(intendedDir);
+        String canonicalID = iD.getCanonicalPath();
+
+        if (canonicalPath.startsWith(canonicalID)) {
+            return canonicalPath;
+        } else {
+            throw new IllegalStateException("File is outside extraction target directory.");
+        }
+    }
+
+    /**
+     * Util methods to obtain a human readable byte count(GB,MB,KB) from
+     * a byte count.
+     *
+     * @param bytes the number of the bytes that should be considered
+     * @return String with the human readable string (e.g. kB, MB, GB)
+     */
+    public static String humanReadableByteCountSI(long bytes) {
+        if (-1000 < bytes && bytes < 1000) {
+            return bytes + " B";
+        }
+        CharacterIterator ci = new StringCharacterIterator("kMGTPE");
+        while (bytes <= -999_950 || bytes >= 999_950) {
+            bytes /= 1000;
+            ci.next();
+        }
+        return String.format("%.1f %cB", bytes / 1000.0, ci.current());
+    }
+
+    /**
+     * After a successful extraction this method is invoked in order to move the
+     * temporary folder content and cleanup the directory.
+     * @param tempPath the temp path where to clean up
+     * @param sTemp the folder name of the temp path [temp-{random-generated-id}]
+     * @return True if the operation is done without errors nor exceptions. False instead
+     */
+    private boolean cleanTempDir(String tempPath, String sTemp) {
+        File dir = new File(tempPath); // point one dir
+
+        try {
+            Files.walk(dir.toPath())
+                    .sorted(Comparator.naturalOrder()). // this make the dir before other files
+                    map(Path::toFile).
+                    forEach(f -> {
+                        try {
+                            File dest = new File(f.getCanonicalPath().replace(sTemp, "")); // removes the tmp-part from the destination files
+                            if (dest.isDirectory()) return; // checks the existence of the dir
+                            /*System.out.println("moving " +
+                                    f.getCanonicalFile().toPath() +
+                                    " to " +
+                                    dest.getCanonicalFile().toPath());*/
+
+                            Files.copy(f.getCanonicalFile().toPath(), dest.getCanonicalFile().toPath(), StandardCopyOption.REPLACE_EXISTING);
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        try {
+            deleteDirectory(dir.toPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+
+    }
+
+    /**
+     * Util method that recursively delete the path passed as input
+     * On unix systems is equivalent to "rm -rf [path]"
+     * @param toBeDeleted path to be deleted
+     * @throws IOException
+     */
+    protected void deleteDirectory(Path toBeDeleted) throws IOException {
+        Files.walk(toBeDeleted)
+                .sorted(Comparator.reverseOrder())
+                .map(Path::toFile)
+                .forEach(File::delete);
+    }
+
+    /**
+     * Setter of the parameter that checks the total size of the extracted files
+     *
+     * @param TOOBIGTOTAL size, in bytes, of the limit
+     */
+    public void setTOOBIGTOTAL(long TOOBIGTOTAL) {
+        m_lToobigtotal = TOOBIGTOTAL;
+    }
+
+    /**
+     * Setter of the parameter that checks the size of each single file
+     *
+     * @param TOOBIGSINGLE size, in bytes, of the limit
+     */
+    public void setTOOBIGSINGLE(long TOOBIGSINGLE) {
+        m_lToobigsingle = TOOBIGSINGLE;
+    }
+
+    /**
+     * Setter of the parameter that checks the number of files being extracted
+     *
+     * @param TOOMANY count of the files that must not be exceed
+     */
+    public void setTOOMANY(int TOOMANY) {
+        m_lToomany = TOOMANY;
+    }
+}
---
+++ wrappersnap/wasdishared/src/test/java/wasdi/shared/utils/TestZipExtractor.java
@@ -0,0 +1,173 @@
+/*
+ * Copyright (c) Fadeout Software 2020. Marco Menapace
+ *
+ */
+package wasdi.shared.utils;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import wasdi.shared.utils.ZipExtractor;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+/**
+ * Test class for safe extraction using it.fadeout.utils.zipextractor.ZipExtractor
+ * Checks different condition. Before every single test the directory of the extraction is
+ * cleaned. Tests also several settings of zip extractor itself.
+ */
+public class TestZipExtractor {
+
+    private ZipExtractor zipExtractor;
+    // final string pointing the path for testing
+    private final static String sExtractionPath = "./src/test/java/extractionFolder/";
+    private final static String sExtractionFileName = "test.zip";
+
+    /**
+     * This methods cleans the out directory before and after each test.
+     */
+    @BeforeEach
+    @AfterEach
+    private void deleteExtractedFolder() {
+
+        zipExtractor = new ZipExtractor();
+        zipExtractor.setTOOBIGTOTAL(1024L * 1024L * 1024L * 1L); // Max size of unzipped total Data, 1 GB
+        zipExtractor.setTOOBIGSINGLE(1024L * 1024L * 512L); // Max size of single file, 512 MB
+        zipExtractor.setTOOMANY(30); // Max count of unzipped files
+
+        // Directory clean
+        // for each file
+        for (File f : new File(sExtractionPath).listFiles()) {
+            try {
+                if (!f.getCanonicalFile().toString().contains(sExtractionFileName)) { // exclude test.zip
+                    if (f.isDirectory()) zipExtractor.deleteDirectory(f.toPath());
+                    else {
+                        if (!f.delete()) {
+                            System.err.println("Before Test extraction - can't delete file " + f.getCanonicalFile());
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    /**
+     * Test that the extraction is done by checking that the files list contained
+     * in the original zip file are on the file system.
+     */
+    @Test
+    void testExtractionDone() {
+        // first unzip
+        String sTempDir = "";
+        try {
+
+            sTempDir = zipExtractor.unzip(sExtractionPath + sExtractionFileName, sExtractionPath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // then check files in temp dir
+        File here = new File(sTempDir);
+        ArrayList<String> path = new ArrayList<>();
+        try {
+            Files.walk(here.toPath().getParent()) // get the parent of the temp dir
+                    .forEach(s -> path.add(s.toString()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String[] fileListInTestZip = getFileListInTestZip();
+        // this checks is done on temp dir but at this point the temp dir is deleted
+        for (String s : fileListInTestZip) {
+            Assertions.assertTrue(path.contains(sExtractionPath + s));
+        }
+
+
+    }
+
+    /**
+     * Checks that extraction fails for a single file being too big
+     */
+    @Test
+    void testExtractionSingleFileTooBig() {
+        zipExtractor.setTOOBIGSINGLE(1024 * 20); // aka 20 KB
+        // with the current test must be checked that Big L script shouldn't be extracted
+        Exception exception = Assertions.assertThrows(IllegalStateException.class, () -> {
+            zipExtractor.unzip(sExtractionPath + sExtractionFileName, sExtractionPath);
+        });
+        // first unzip
+        Assertions.assertTrue(exception.getMessage().contains("File being unzipped is too big"));
+
+    }
+
+    /**
+     * Checks that extraction fails for total size exceeding the imposed limits
+     */
+    @Test
+    void testExtractionAllFilesTooBig() {
+        zipExtractor.setTOOBIGTOTAL(1); // aka 1 Byte
+        // with the current test must be checked that Big L script shouldn't be extracted
+        Exception exception = Assertions.assertThrows(IllegalStateException.class, () -> {
+            zipExtractor.unzip(sExtractionPath + sExtractionFileName, sExtractionPath);
+        });
+        // first unzip
+        Assertions.assertTrue(exception.getMessage().contains("File extraction interrupted because total dimension is over extraction limits"));
+
+    }
+
+    /**
+     * Checks that extraction fails for total number of files exceed the imposed limit
+     */
+    @Test
+    void testExtractionTooMany() {
+        zipExtractor.setTOOMANY(5); // aka 5 files
+        // with the current test must be checked that Big L script shouldn't be extracted
+        Exception exception = Assertions.assertThrows(IllegalStateException.class, () -> {
+            zipExtractor.unzip(sExtractionPath + sExtractionFileName, sExtractionPath);
+        });
+        // first unzip
+        Assertions.assertTrue(exception.getMessage().contains("Too many files to unzip"));
+
+    }
+
+
+    /**
+     * Utils methods used that returns a String array containing all the
+     * entries inside the zip file.
+     *
+     * @return String array with the entries of the zip file used in these test
+     */
+    static String[] getFileListInTestZip() {
+        try {
+            FileInputStream fis = new FileInputStream(sExtractionPath + sExtractionFileName);
+            ZipInputStream zis = new ZipInputStream(new BufferedInputStream(fis));
+            ArrayList<String> returnArray = new ArrayList<>();
+            ZipEntry ze = zis.getNextEntry();
+            while (ze != null) {
+                //if (!ze.getName().endsWith(".zip")) {
+                if (ze.getName().endsWith("/")) {
+                    returnArray.add(ze.getName().substring(0, ze.getName().length() - 1));
+                } else {
+                    returnArray.add(ze.getName());
+                }
+                //}
+                ze = zis.getNextEntry();
+            }
+            return returnArray.toArray(new String[0]);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+}
