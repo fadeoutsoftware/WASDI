bb38a1a873f9ae2efeb54e79a7c7439671833eda
Marco Menapace <m.menapace@fadeout.it>
2021-01-22 09:40:19 +0100

    workflow manager fixed

--- client/app/dialogs/workflow_manager/WorkFlowManagerController.js
+++ client/app/dialogs/workflow_manager/WorkFlowManagerController.js
@@ -56,10 +56,10 @@ var WorkFlowManagerController = (function() {
     {
         var oController = this;
         this.m_bIsLoadingWorkflows = true;
-        this.m_oSnapOperationService.getWorkflowsByUser().success(function (data) {
-            if(utilsIsObjectNullOrUndefined(data) == false)
+        this.m_oSnapOperationService.getWorkflowsByUser().then(function (data) {
+            if(utilsIsObjectNullOrUndefined(data.data) == false)
             {
-                oController.m_aoWorkflows = data;
+                oController.m_aoWorkflows = data.data;
             }
             else
             {
@@ -72,7 +72,7 @@ var WorkFlowManagerController = (function() {
                 oController.m_sSelectedWorkflowTab = 'WorkFlowTab2';
             }
             oController.m_bIsLoadingWorkflows = false;
-        }).error(function (error) {
+        },function (error) {
             utilsVexDialogAlertTop("GURU MEDITATION<br>ERROR GETTING WORKFLOW LIST");
             oController.m_bIsLoadingWorkflows = false;
         });
@@ -196,8 +196,8 @@ var WorkFlowManagerController = (function() {
             return false;
         }
         var oController = this;
-        this.m_oSnapOperationService.executeGraphFromWorkflowId(sWorkspaceId,oObjectWorkFlow).success(function (data) {
-            if(utilsIsObjectNullOrUndefined(data) === false && data.boolValue === true )
+        this.m_oSnapOperationService.executeGraphFromWorkflowId(sWorkspaceId,oObjectWorkFlow).then(function (data) {
+            if(utilsIsObjectNullOrUndefined(data.data) === false && data.boolValue === true )
             {
                 oController.cleanAllExecuteWorkflowFields();
             }
@@ -207,7 +207,7 @@ var WorkFlowManagerController = (function() {
             }
             oController.closeDialogWithDelay("",500);
 
-        }).error(function (error) {
+        },function (error) {
 
             oController.cleanAllExecuteWorkflowFields();
             utilsVexDialogAlertTop("GURU MEDITATION<br>ERROR IN EXECUTE WORKFLOW");
@@ -231,9 +231,9 @@ var WorkFlowManagerController = (function() {
             return false;
         }
         var oController = this;
-        this.m_oSnapOperationService.deleteWorkflow(oWorkflow.workflowId).success(function (data)
+        this.m_oSnapOperationService.deleteWorkflow(oWorkflow.workflowId).then(function (data)
         {
-            if(utilsIsObjectNullOrUndefined(data) == false)
+            if(utilsIsObjectNullOrUndefined(data.data) == false)
             {
                 oController.getWorkflowsByUser();
             }
@@ -242,7 +242,7 @@ var WorkFlowManagerController = (function() {
                 //TODO ERROR
                 utilsVexDialogAlertTop("GURU MEDITATION<br>ERROR IN DELETE WORKFLOW");
             }
-        }).error(function (error) {
+        },function (error) {
             utilsVexDialogAlertTop("GURU MEDITATION<br>ERROR IN DELETE WORKFLOW");
         });
 
@@ -295,8 +295,8 @@ var WorkFlowManagerController = (function() {
         }
         this.isUploadingWorkflow=true;
         var oController = this;
-        this.m_oSnapOperationService.uploadGraph(this.m_sWorkspaceId,sName,sDescription,oBody,bIsPublic).success(function (data) {
-            if(utilsIsObjectNullOrUndefined(data) == false)
+        this.m_oSnapOperationService.uploadGraph(this.m_sWorkspaceId,sName,sDescription,oBody,bIsPublic).then(function (data) {
+            if(utilsIsObjectNullOrUndefined(data.data) == false)
             {
                 //Reload list o workFlows
                 oController.getWorkflowsByUser();
@@ -311,7 +311,7 @@ var WorkFlowManagerController = (function() {
             }
 
             oController.isUploadingWorkflow = false;
-        }).error(function (error) {
+        },function (error) {
             utilsVexDialogAlertTop("GURU MEDITATION<br>ERROR IN UPLOAD WORKFLOW PROCESS");
             oController.cleanAllUploadWorkflowFields();
             oController.isUploadingWorkflow = false;
