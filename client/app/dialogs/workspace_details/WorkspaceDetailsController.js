1ae53e1dc8c0297f1f2e9e433f3e13675ee8232f
Marco Menapace <m.menapace@fadeout.it>
2021-02-09 15:32:35 +0100

    finally extras working in modal view

--- client/app/dialogs/workspace_details/WorkspaceDetailsController.js
+++ client/app/dialogs/workspace_details/WorkspaceDetailsController.js
@@ -1,11 +1,11 @@
- /**
+/**
  * Created by m.menapace on 9/02/2021.
  */
 
 
-var WorkspaceDetailsController = (function() {
+var WorkspaceDetailsController = (function () {
 
-    function WorkspaceDetailsController($scope) {
+    function WorkspaceDetailsController($scope, oExtras) {
 
         /**
          * Angular Scope
@@ -15,9 +15,29 @@ var WorkspaceDetailsController = (function() {
          * Reference to this controller
          */
         this.m_oScope.m_oController = this;
-    }
-     WorkspaceDetailsController.$inject = [
+        /**
+         * import the extras
+         */
+        this.m_oExtras = oExtras;
+        /**
+         * workspace id
+         */
+        this.m_workspaceId = this.m_oExtras.WorkSpaceId;
+        // the workspace id passed through extras to the modal
+        // then, if ok, call the other methods from angular starting from here
+        // get WS viewmodel, date and co [...]
+
+
+    } // end constructor
+
+    /*WorkspaceDetailsController.prototype.printSomething = function (){
+        console.log("helo ! ");
+        console.log(this.m_workspaceId);
+    }*/
+
+    WorkspaceDetailsController.$inject = [
         '$scope',
+        'extras'
     ];
     return WorkspaceDetailsController;
 })();
