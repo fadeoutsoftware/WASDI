8ba7786e77ac009c0c2a1832e7939e5610c89216
pcampanella <p.campanella@fadeout.it>
2021-02-11 18:25:50 +0100

    adding launcher processes logs and payloads.

---
+++ wrappersnap/wasdishared/src/main/java/wasdi/shared/payload/OperationPayload.java
@@ -0,0 +1,14 @@
+package wasdi.shared.payload;
+
+public class OperationPayload {
+	protected String operation;
+	
+	public String getOperation() {
+		return operation;
+	}
+	public void setOperation(String operation) {
+		this.operation = operation;
+	}
+	
+
+}
