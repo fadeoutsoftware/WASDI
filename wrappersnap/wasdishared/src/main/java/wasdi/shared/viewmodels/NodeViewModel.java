3051531cf54471c5f30c0e5ee88940f0a255e157
Marco Menapace <m.menapace@fadeout.it>
2021-02-11 13:11:15 +0100

    new view model for Nodes

---
+++ wrappersnap/wasdishared/src/main/java/wasdi/shared/viewmodels/NodeViewModel.java
@@ -0,0 +1,34 @@
+package wasdi.shared.viewmodels;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by m.menapace on 11/02/2021
+ */
+public class NodeViewModel {
+
+    private String nodeCode;
+    private String cloudProvider;
+    
+	public String getNodeCode() {
+		return nodeCode;
+	}
+	public void setNodeCode(String nodeCode) {
+		this.nodeCode = nodeCode;
+	}
+	public String getCloudProvider() {
+		return cloudProvider;
+	}
+	public void setCloudProvider(String cloudProvider) {
+		this.cloudProvider = cloudProvider;
+	}
+    
+    
+    
+	
+    
+    
+
+}
