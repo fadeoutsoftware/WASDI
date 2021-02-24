ba95638e8bd34606e7a4a811539abb53328dd04e
Cristiano Nattero <c.nattero@fadeout.it>
2021-02-24 12:15:02 +0100

    Added keycloak service + binding

--- wasdiwebserver/src/main/java/it/fadeout/WasdiBinder.java
+++ wasdiwebserver/src/main/java/it/fadeout/WasdiBinder.java
@@ -10,6 +10,8 @@ import javax.inject.Singleton;
 
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 
+import it.fadeout.rest.resources.AuthResource;
+import it.fadeout.services.KeycloakService;
 import it.fadeout.services.ConfigProvidersCatalog;
 import it.fadeout.services.ProvidersCatalog;
 import wasdi.shared.data.NodeRepository;
@@ -23,6 +25,8 @@ public class WasdiBinder extends AbstractBinder {
     @Override
     protected void configure() {
         bind(ConfigProvidersCatalog.class).to(ProvidersCatalog.class).in(Singleton.class);
+        
+        bind(KeycloakService.class).to(AuthResource.class).in(Singleton.class);
 
         // repositories binding
         bind(NodeRepository.class).to(NodeRepository.class);
---
+++ wasdiwebserver/src/main/java/it/fadeout/services/KeycloakService.java
@@ -0,0 +1,95 @@
+package it.fadeout.services;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletConfig;
+import javax.ws.rs.core.Context;
+
+import org.json.JSONObject;
+
+import it.fadeout.Wasdi;
+import wasdi.shared.utils.Utils;
+
+public class KeycloakService {
+
+	@Context
+	ServletConfig m_oServletConfig;
+
+	private static final String s_sACCESS_TOKEN = "access_token";
+
+	//just to hide the implicit one
+	private KeycloakService() {}
+
+	public String getKeycloakAdminCliToken() {
+		try {
+			String sAuthUrl = m_oServletConfig.getInitParameter("keycloak_server");
+			String sCliSecret = m_oServletConfig.getInitParameter("keycloak_CLI_Secret");
+			//URL
+			if(!sAuthUrl.endsWith("/")) {
+				sAuthUrl += "/";
+			}
+			sAuthUrl += "realms/master/protocol/openid-connect/token";
+			//payload
+			String sPayload = "client_id=admin-cli" +
+					"&grant_type=client_credentials" +
+					"&client_secret=" + sCliSecret;
+			//headers
+			Map<String, String> asHeaders = new HashMap<>();
+			asHeaders.put("Content-Type", "application/x-www-form-urlencoded");
+			//POST -> authenticate on keycloak 
+			String sAuthResult = Wasdi.httpPost(sAuthUrl, sPayload, asHeaders);
+			if(Utils.isNullOrEmpty(sAuthResult)) {
+				throw new RuntimeException("could not login into keycloak");
+			}
+			//read response and get token
+			JSONObject oJson = new JSONObject(sAuthResult);
+			if( null == oJson||
+					!oJson.has(s_sACCESS_TOKEN) ||							
+					Utils.isNullOrEmpty(oJson.optString(s_sACCESS_TOKEN, null))
+					) {
+				throw new NullPointerException("Missing access token");
+			}
+			String sKcTokenId = "";
+			sKcTokenId = oJson.optString(s_sACCESS_TOKEN, null);
+			if(Utils.isNullOrEmpty(sKcTokenId)) {
+				throw new NullPointerException("Token id null or empty");
+			}
+			Utils.debugLog("AuthResource.getKeycloakAdminCliToken: admin token obtained :-)");
+			return sKcTokenId;
+		} catch (Exception oE) {
+			Utils.debugLog("AuthResource.getKeycloakAdminCliToken: " + oE);
+		}
+		return null;
+	}
+
+	public String getUserDataFromKeycloak(String sKcTokenId, String sUserId) {
+		// build keycloak API URL
+		String sUrl = m_oServletConfig.getInitParameter("keycloak_server");
+		if(!sUrl.endsWith("/")) {
+			sUrl += "/";
+		}
+		sUrl += "admin/realms/wasdi/users?username=";
+		sUrl += sUserId;
+		Utils.debugLog("AuthResource.userRegistration: about to post to " + sUrl);
+		Map<String, String> asHeaders = new HashMap<>();
+		asHeaders.clear();
+		asHeaders.put("Authorization", "Bearer " + sKcTokenId);
+		return Wasdi.httpGet(sUrl, asHeaders);
+	}
+
+	public String keyCloakLogin(String sUser, String sPassword) {
+		//authenticate against keycloak
+		String sUrl = m_oServletConfig.getInitParameter("keycloak_auth");
+
+		String sPayload = "client_id=";
+		sPayload += m_oServletConfig.getInitParameter("keycloak_confidentialClient");
+		sPayload += "&grant_type=password&username=" + sUser;
+		sPayload += "&password=" + sPassword;
+		Map<String, String> asHeaders = new HashMap<>();
+		asHeaders.put("Content-Type", "application/x-www-form-urlencoded");
+		String sAuthResult = Wasdi.httpPost(sUrl, sPayload, asHeaders);
+
+		return sAuthResult;
+	}
+}
