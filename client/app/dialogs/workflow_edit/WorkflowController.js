46904f5de3c6c3f1076b99f05139a9dc84dea507
Marco <m.menapace@fadeout.it>
2021-05-05 14:03:57 +0200

    Languages fix, some formatting and begin of implementation of sharings UI

--- client/app/dialogs/workflow_edit/WorkflowController.js
+++ client/app/dialogs/workflow_edit/WorkflowController.js
@@ -11,10 +11,25 @@ var WorkflowController = (function() {
          * Reference to the controller
          */
         this.m_oScope.m_oController = this;
-
+        /**
+         * First tab visualized
+         * @type {string}
+         */
         this.m_sSelectedTab = "Base";
-
+        /**
+         * Extras injected from modal invoker
+         */
         this.m_oExtras = oExtras;
+
+        /**
+         * Object with the infos about the current workflow
+         */
+        this.m_oWorkflow = this.m_oExtras.workflow;
+        /**
+         * Field to add sharing
+         * @type {string}
+         */
+        this.m_sUserEmail ="";
     }
     WorkflowController.$inject = [
         '$scope',
