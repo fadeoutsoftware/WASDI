240f6d25ee070196db112d0ddb8522c4128284a6
Cristiano Nattero <c.nattero@fadeout.it>
2021-06-09 17:06:00 +0200

    Print status

---
+++ libraries/matlabwasdilib/wPrintStatus.m
@@ -0,0 +1,1 @@
+function wPrintStatus(Wasdi)% Prints the status% Syntax:% printStatus(Wasdi)% % INPUT%    Wasdi: Wasdi object created after the wasdilib call%  if exist("Wasdi") < 1     disp('Wasdi variable does not exist')    return   end     Wasdi.printStatus();   end
