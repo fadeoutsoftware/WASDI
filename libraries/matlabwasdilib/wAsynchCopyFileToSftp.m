d44875d9facd23b2386d2685f49222783e3b53f2
Marco Menapace <m.menapace@fadeout.it>
2021-06-10 16:56:15 +0200

    fixed comments up to wAsynchCopyFileToSftp.m

--- libraries/matlabwasdilib/wAsynchCopyFileToSftp.m
+++ libraries/matlabwasdilib/wAsynchCopyFileToSftp.m
@@ -1,12 +1,11 @@
 function sProcessId=wAsynchCopyFileToSftp(Wasdi, sFileName, sRelativePath)
-% Copy file to SFTP folder, asynchronous version
-% Syntax
-% wAsynchCopyFileToSftp(Wasdi, sFileName, sRelativePath)
-% 
-% INPUT
-%  Wasdi: Wasdi object created after the wasdilib call
-%	 sFileName: a string containing the file name
-%	 sRelativePath: a string containinng the relative path
+%Copy file to SFTP folder, asynchronous version
+%Syntax
+%wAsynchCopyFileToSftp(Wasdi, sFileName, sRelativePath)
+%
+%:param Wasdi: Wasdi object created after the wasdilib call
+%:param sFileName: a string containing the file name
+%:param sRelativePath: a string containinng the relative path
 %
 
   if exist("Wasdi") < 1 
