5db9244bded3434bf0ea962424dd2d975c38b48a
Cristiano Nattero <c.nattero@fadeout.it>
2021-07-08 17:31:16 +0200

    Added service methods for resetting password

--- wasdiwebserver/src/main/java/it/fadeout/services/KeycloakService.java
+++ wasdiwebserver/src/main/java/it/fadeout/services/KeycloakService.java
@@ -1,15 +1,18 @@
 package it.fadeout.services;
 
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.ServletConfig;
 import javax.ws.rs.core.Context;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import it.fadeout.Wasdi;
 import wasdi.shared.utils.Utils;
+import wasdi.shared.viewmodels.PrimitiveResult;
 
 public class KeycloakService implements AuthProviderService {
 
@@ -64,18 +67,18 @@ public class KeycloakService implements AuthProviderService {
 		return null;
 	}
 
-	public String getUserData(String sKcTokenId, String sUserId) {
+	public String getUserData(String sToken, String sUserId) {
 		// build keycloak API URL
 		String sUrl = m_oServletConfig.getInitParameter("keycloak_server");
 		if(!sUrl.endsWith("/")) {
 			sUrl += "/";
 		}
-		sUrl += "admin/realms/wasdi/users?username=";
+		sUrl += "admin/realms/wasdi/users?exact=true&username=";
 		sUrl += sUserId;
 		Utils.debugLog("KeycloakService.userRegistration: about to GET to " + sUrl);
 		Map<String, String> asHeaders = new HashMap<>();
 		asHeaders.clear();
-		asHeaders.put("Authorization", "Bearer " + sKcTokenId);
+		asHeaders.put("Authorization", "Bearer " + sToken);
 		return Wasdi.httpGet(sUrl, asHeaders);
 	}
 
@@ -95,4 +98,81 @@ public class KeycloakService implements AuthProviderService {
 
 		return sAuthResult;
 	}
+	
+	@Override
+	public String getUserDbId(String sUserId) {
+		if(Utils.isNullOrEmpty(sUserId)) {
+			Utils.debugLog("KeycloakService.getUserDbId: user id null or empty, aborting");
+			return null;
+		}
+		String sToken = getToken();
+		if(Utils.isNullOrEmpty(sToken)) {
+			Utils.debugLog("KeycloakService.getUserDbId: token null or empty, aborting");
+			return null;
+		}
+		String sResponse = getUserData(sToken, sUserId);
+		if(Utils.isNullOrEmpty(sResponse)) {
+			Utils.debugLog("KeycloakService.getUserDbId: response null or empty, aborting");
+			return null;
+		}
+		try {
+			JSONArray oResponseArray = new JSONArray(sResponse);
+			if(oResponseArray.length() != 1) {
+				Utils.debugLog("KeycloakService.getUserDbId: response array has length " + oResponseArray.length() + ", but lenght of exactly 1 was expected, aborting");
+				return null;
+			}
+			JSONObject oEntry = oResponseArray.getJSONObject(0);
+			return oEntry.optString("id", null);
+		} catch (Exception oE) {
+			Utils.debugLog("KeycloakService.getUserDbId: could not parse response due to " + oE + ", aborting");
+		}
+		return null;
+	}
+	
+	@Override
+	public PrimitiveResult requirePasswordUpdateViaEmail(String sUserId) {
+		PrimitiveResult oResult = new PrimitiveResult();
+		oResult.setBoolValue(false);
+		oResult.setIntValue(500);
+		if(Utils.isNullOrEmpty(sUserId)) {
+			Utils.debugLog("KeycloakService.requirePasswordUpdateViaEmail: user id null or empty, aborting");
+			return oResult;
+		}
+		String sUserDbId = getUserDbId(sUserId);
+		if(Utils.isNullOrEmpty(sUserDbId)) {
+			Utils.debugLog("KeycloakService.requirePasswordUpdateViaEmail: user DB id null or empty, aborting");
+			return oResult;
+		}
+		StringBuilder oUrlBuilder = new StringBuilder()
+				.append(m_oServletConfig.getInitParameter("keycloak_server"))
+				.append("admin/realms/wasdi/users/")
+				.append(sUserDbId)
+				.append("/execute-actions-email?redirect_uri=https://www.wasdi.net/&client_id=wasdi_api");
+		String sUrl = oUrlBuilder.toString();
+		//todo check if it is possible to invalidate current password (password expire?)
+		//alternatively: set a temporary password unknown to the user
+		//todo decide whether we want to interrupt existing sessions
+		String sPayload = "[\"UPDATE_PASSWORD\"]";
+		try {
+			String sToken = getToken();
+			Map<String, String> asHeaders = new HashMap<>();
+			asHeaders.put("Authorization", "Bearer " + sToken);
+			asHeaders.put("Content-Type", "application/json");
+			String sResponse = Wasdi.httpPut(sUrl, sPayload, asHeaders);
+			System.out.println(sResponse);
+			oResult.setIntValue(200);
+			return oResult;
+		} catch (Exception oE) {
+			Utils.debugLog("KeycloakService.requirePasswordUpdateViaEmail: could not establish connection due to: " + oE + ", aborting");
+		}
+		
+				
+		return oResult;
+	}
 }
+
+
+
+
+
+
