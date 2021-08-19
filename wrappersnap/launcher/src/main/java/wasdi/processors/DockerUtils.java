ac6b978aa2e6c8e3fa5deedfb74b8637d728092b
Marco Menapace <m.menapace@fadeout.it>
2021-08-19 10:12:06 +0200

    added log to check deployed version

--- wrappersnap/launcher/src/main/java/wasdi/processors/DockerUtils.java
+++ wrappersnap/launcher/src/main/java/wasdi/processors/DockerUtils.java
@@ -15,32 +15,32 @@ import wasdi.shared.utils.Utils;
  *
  */
 public class DockerUtils {
-	
+
 	/**
 	 * Wasdi Processor
 	 */
 	Processor m_oProcessor;
-	
+
 	/**
 	 * Folder of the processor
 	 */
 	String m_sProcessorFolder;
-	
+
 	/**
 	 * Wasdi Working Path
 	 */
 	String m_sWorkingRootPath;
-	
+
 	/**
 	 * Log file for docker operations
 	 */
 	String m_sDockerLogFile = "/usr/lib/wasdi/launcher/logs/dockers.log";
-	
+
 	/**
 	 * User that run the docker
 	 */
 	String m_sUser = "tomcat8";
-	
+
 	/**
 	 * Create a new instance
 	 * @param oProcessor Processor
@@ -53,27 +53,27 @@ public class DockerUtils {
 		m_sWorkingRootPath = sWorkingRootPath;
 		m_sUser = sTomcatUser;
 	}
-	
+
 	/**
 	 * Deploy a docker
 	 */
 	public boolean deploy() {
-		
+
 		try {
-			
+
 			// Generate Docker Name
 			String sProcessorName = m_oProcessor.getName();
-			
+
 			String sDockerName = "wasdi/"+sProcessorName+":"+m_oProcessor.getVersion();
-			
+
 			// Initialize Args
 			ArrayList<String> asArgs = new ArrayList<>();
-			
+
 			// Generate shell script file
 			String sBuildScriptFile = m_sProcessorFolder+"deploywasdidocker.sh";
-			
+
 			File oBuildScriptFile = new File(sBuildScriptFile);
-			
+
 			try (BufferedWriter oBuildScriptWriter = new BufferedWriter(new FileWriter(oBuildScriptFile))) {
 				// Fill the script file
 				if(oBuildScriptWriter != null ) {
@@ -83,7 +83,7 @@ public class DockerUtils {
 					oBuildScriptWriter.newLine();
 					oBuildScriptWriter.write("echo Deploy Docker Started >> " + m_sDockerLogFile);
 					oBuildScriptWriter.newLine();
-					oBuildScriptWriter.write("docker build -t" + sDockerName + " " + m_sProcessorFolder + " --build-arg USR_NAME=" + m_sUser+ " --build-arg USR_ID=$(id -u "+m_sUser+")" +  
+					oBuildScriptWriter.write("docker build -t" + sDockerName + " " + m_sProcessorFolder + " --build-arg USR_NAME=" + m_sUser+ " --build-arg USR_ID=$(id -u "+m_sUser+")" +
 							" --build-arg GRP_NAME=" + m_sUser+ " --build-arg GRP_ID=$(id -g "+m_sUser+")" +
 							" $1 >> " + m_sDockerLogFile + " 2>&1");
 					oBuildScriptWriter.newLine();
@@ -92,82 +92,84 @@ public class DockerUtils {
 					oBuildScriptWriter.close();
 				}
 			}
-			
+
 			// Wait a little bit to let the file be written
 			Thread.sleep(2000);
-						
+
 			// Make it executable
 			Runtime.getRuntime().exec("chmod u+x "+sBuildScriptFile);
-			
+
 			// And wait a little bit to make the chmod done
 			Thread.sleep(2000);
-			
+
 			// Run the script
 			WasdiProcessorEngine.shellExec(sBuildScriptFile, asArgs);
-			
+
 			LauncherMain.s_oLogger.debug("DockerUtils.deploy: created image " + sDockerName);
 		}
 		catch (Exception oEx) {
 			Utils.debugLog("DockerUtils.deploy: " + oEx.toString());
 			return false;
 		}
-		
+
 		return true;
 	}
-	
+
 	/**
 	 * Run the docker
 	 */
 	public boolean run() {
 		return run(m_oProcessor.getPort());
 	}
-	
+
 	/**
 	 * Run the docker at the specified port
 	 * @param iProcessorPort Port to use
 	 */
 	public boolean run(int iProcessorPort) {
-		
+
 		try {
 			// Get the docker name
 			String sDockerName = "wasdi/"+m_oProcessor.getName() +":" + m_oProcessor.getVersion();
 			String sCommand = "docker";
-			
+
 			// Initialize Args
 			ArrayList<String> asArgs = new ArrayList<>();
-			
+
 			// Processor Script File
-			String sRunFile = m_sProcessorFolder+"runwasdidocker.sh";			
+			String sRunFile = m_sProcessorFolder+"runwasdidocker.sh";
 			File oRunFile = new File(sRunFile);
-			
+
 			// Check if it is already done:
-			
+
 			if (!oRunFile.exists() || oRunFile.length() == 0L) {
-								
+
 				// Command
 				asArgs.add(sCommand);
-				
+
 				// Action
 				asArgs.add("run");
-				
+
 				// User
 				asArgs.add("-u$(id -u "+m_sUser+"):$(id -g "+m_sUser+")");
-				
+
 				// Working Path
 				asArgs.add("-v"+ m_sWorkingRootPath + ":/data/wasdi");
-				
+
 				// Processor folder
 				asArgs.add("--mount");
 				asArgs.add("type=bind,src="+m_sProcessorFolder+",dst=/wasdi");
-				
+
 				// Port
 				asArgs.add("-p127.0.0.1:"+iProcessorPort+":5000");
 
 				/* TODO this parameter is needed just by adwaiseo node
 					- Check if it's possibile to add to properties.config file
 				 */
+
+				LauncherMain.s_oLogger.debug("DockerUtils.run adding host mapping to the run arguments");
 				asArgs.add("--add-host=adwaiseo1.wasdi.net:172.17.0.1");
-				
+
 				// Docker name
 				asArgs.add(sDockerName);
 
@@ -176,13 +178,13 @@ public class DockerUtils {
 
 
 
-				
+
 				for (String sArg : asArgs) {
 					sCommandLine += sArg + " ";
 				}
-				
+
 				LauncherMain.s_oLogger.debug("DockerUtils.run CommandLine: " + sCommandLine);
-							
+
 				try (BufferedWriter oRunWriter = new BufferedWriter(new FileWriter(oRunFile))) {
 					if(null!= oRunWriter) {
 						LauncherMain.s_oLogger.debug("DockerUtils.run: Creating "+sRunFile+" file");
@@ -198,34 +200,34 @@ public class DockerUtils {
 						oRunWriter.close();
 					}
 				}
-								
+
 				Runtime.getRuntime().exec("chmod u+x "+sRunFile);
-				
+
 				asArgs.clear();
 			}
-			
+
 			try {
 				if (!oRunFile.canExecute()) {
 					Runtime.getRuntime().exec("chmod u+x "+sRunFile);
-				}				
+				}
 			}
 			catch (Exception oInnerEx) {
 				Runtime.getRuntime().exec("chmod u+x "+sRunFile);
 			}
-			
+
 			// Execute the command to start the docker
-			WasdiProcessorEngine.shellExec(sRunFile, asArgs, false);		
-						
+			WasdiProcessorEngine.shellExec(sRunFile, asArgs, false);
+
 			LauncherMain.s_oLogger.debug("DockerUtils.run " + sDockerName + " started");
 		}
 		catch (Exception oEx) {
 			Utils.debugLog("DockerUtils.run: " + oEx.toString());
 			return false;
 		}
-		
+
 		return true;
 	}
-	
+
 	/**
 	 * Delete using Processor member variable
 	 * @return
@@ -236,7 +238,7 @@ public class DockerUtils {
 		}
 		else return false;
 	}
-	
+
 	/**
 	 * Delete using processor name and default "1" version
 	 * @param sProcessorName
@@ -245,7 +247,7 @@ public class DockerUtils {
 	public boolean delete(String sProcessorName) {
 		return delete(sProcessorName, "1");
 	}
-	
+
 	/**
 	 * Delete using processor name and processor version
 	 * @param sProcessorName
@@ -253,17 +255,17 @@ public class DockerUtils {
 	 * @return
 	 */
 	public boolean delete(String sProcessorName, String sVersion) {
-		
+
 		try {
-			
+
 			// docker ps -a | awk '{ print $1,$2 }' | grep <imagename> | awk '{print $1 }' | xargs -I {} docker rm -f {}
 			// docker rmi -f <imagename>
-			
+
 			String sDockerName = "wasdi/"+sProcessorName+":"+sVersion;
-			
-			String sDeleteScriptFile = m_sProcessorFolder+"cleanwasdidocker.sh";			
+
+			String sDeleteScriptFile = m_sProcessorFolder+"cleanwasdidocker.sh";
 			File oDeleteScriptFile = new File(sDeleteScriptFile);
-			
+
 			if (!oDeleteScriptFile.exists() || oDeleteScriptFile.length() == 0L) {
 				try (BufferedWriter oDeleteScriptWriter = new BufferedWriter(new FileWriter(oDeleteScriptFile))) {
 					if(oDeleteScriptWriter != null) {
@@ -275,40 +277,40 @@ public class DockerUtils {
 						oDeleteScriptWriter.newLine();
 						oDeleteScriptWriter.flush();
 						oDeleteScriptWriter.close();
-					}					
-				}				
-				
+					}
+				}
+
 				Runtime.getRuntime().exec("chmod u+x "+sDeleteScriptFile);
 			}
 			else {
 				Runtime.getRuntime().exec("chmod u+x "+sDeleteScriptFile);
 			}
 
-			
+
 			Runtime.getRuntime().exec(sDeleteScriptFile);
 
 			// Wait for docker to finish
 			Thread.sleep(15000);
-			
+
 			// Delete this image
 			ArrayList<String> asArgs = new ArrayList<>();
 			// Remove the container image
 			asArgs.add("rmi");
 			asArgs.add("-f");
 			asArgs.add(sDockerName);
-			
+
 			String sCommand = "docker";
-			
+
 			WasdiProcessorEngine.shellExec(sCommand, asArgs, false);
-			
+
 			// Wait for docker to finish
-			Thread.sleep(15000);			
+			Thread.sleep(15000);
 		}
 		catch (Exception oEx) {
 			Utils.debugLog("DockerUtils.delete: " + oEx.toString());
 			return false;
 		}
-		
+
 		return true;
 	}
 }
