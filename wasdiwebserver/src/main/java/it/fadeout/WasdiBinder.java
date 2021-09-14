6a66c4109fcb3785f82903aa363762a3308db11e
Cristiano Nattero <c.nattero@fadeout.it>
2021-09-14 16:51:51 +0200

    Bind ProcessService

--- wasdiwebserver/src/main/java/it/fadeout/WasdiBinder.java
+++ wasdiwebserver/src/main/java/it/fadeout/WasdiBinder.java
@@ -12,9 +12,12 @@ import org.glassfish.hk2.utilities.binding.AbstractBinder;
 
 import it.fadeout.rest.resources.AuthResource;
 import it.fadeout.services.KeycloakService;
+import it.fadeout.services.ProcessService;
+import it.fadeout.services.ProcessServiceInterface;
 import it.fadeout.services.AuthProviderService;
 import it.fadeout.services.ConfigProvidersCatalog;
 import it.fadeout.services.ProvidersCatalog;
+import wasdi.shared.business.ProcessWorkspace;
 import wasdi.shared.data.NodeRepository;
 
 /**
@@ -23,13 +26,16 @@ import wasdi.shared.data.NodeRepository;
  *
  */
 public class WasdiBinder extends AbstractBinder {
-    @Override
-    protected void configure() {
-        bind(ConfigProvidersCatalog.class).to(ProvidersCatalog.class).in(Singleton.class);
-        
-        bind(KeycloakService.class).to(AuthProviderService.class).in(Singleton.class);
-
-        // repositories binding
-        bind(NodeRepository.class).to(NodeRepository.class);
-    }
+	@Override
+	protected void configure() {
+		//providers catalog
+		bind(ConfigProvidersCatalog.class).to(ProvidersCatalog.class).in(Singleton.class);
+		//authentication provider -> keycloak
+		bind(KeycloakService.class).to(AuthProviderService.class).in(Singleton.class);
+		// repositories binding
+		bind(NodeRepository.class).to(NodeRepository.class);
+		//process workspace
+		//bind(ProcessService.class).to(ProcessServiceInterface.class).in(Singleton.class);
+		bind(ProcessService.class).to(ProcessService.class).in(Singleton.class);
+	}
 }
