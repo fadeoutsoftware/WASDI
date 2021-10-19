c06817add10fb4301cfe7e18ce35820cf64ee72a
Marco Menapace <m.menapace@fadeout.it>
2021-10-19 18:01:41 +0200

    Completed feature, field "showExtension" on JSON now let the developer to show the file name in the product combo. Fallback to old behaviour for retro-compatibility.

--- client/app/lib/factories/ViewElementFactory.js
+++ client/app/lib/factories/ViewElementFactory.js
@@ -34,7 +34,7 @@ function ViewElementFactory() {
 
             oViewElement.asListValues = [];
 
-            for (; iValues < oControl.values.length; iValues ++) {
+            for (; iValues < oControl.values.length; iValues++) {
                 let oItem = {
                     name: oControl.values[iValues],
                     id: "" + iValues
@@ -51,380 +51,384 @@ function ViewElementFactory() {
             // Bounding Box from Map
             oViewElement = new SelectArea();
         }
-        else if (oControl.type === "date"){
+        else if (oControl.type === "date") {
             oViewElement = new DateTimePicker();
         }
-        else if (oControl.type === "productlist"){
+        else if (oControl.type === "productlist") {
             oViewElement = new ProductList();
         }
-        else if (oControl.type === "searcheoimage"){
+        else if (oControl.type === "searcheoimage") {
             oViewElement = new SearchEOImage();
         }
-        else if (oControl.type === "productscombo"){
-            oViewElement = new ProductsCombo();
-        }
-        else if(oControl.type === "boolean"){
-            oViewElement = new CheckBox();
-
-            if (utilsIsObjectNullOrUndefined(oControl.default) == false) {
-                oViewElement.m_bValue = oControl.default;
+        else if (oControl.type === "productscombo") {
+            if (oControl.showExtension != undefined) {
+                oViewElement = new ProductsCombo(oControl.showExtension);
+            } else {
+                oViewElement = new ProductsCombo(false); // back to default behaviour, in case showExtension is not specified
             }
-        }
-        else if(oControl.type === "slider") {
-            oViewElement = new Slider();
-
-            if(utilsIsObjectNullOrUndefined(oControl.min) == false) {
-                oViewElement.m_iMin = oControl.min;
             }
-            if(utilsIsObjectNullOrUndefined(oControl.max) == false) {
-                oViewElement.m_iMax = oControl.max;
+        else if (oControl.type === "boolean") {
+                oViewElement = new CheckBox();
+
+                if (utilsIsObjectNullOrUndefined(oControl.default) == false) {
+                    oViewElement.m_bValue = oControl.default;
+                }
             }
-            if (utilsIsObjectNullOrUndefined(oControl.default) == false) {
-                oViewElement.m_iValue = oControl.default;
+            else if (oControl.type === "slider") {
+                oViewElement = new Slider();
+
+                if (utilsIsObjectNullOrUndefined(oControl.min) == false) {
+                    oViewElement.m_iMin = oControl.min;
+                }
+                if (utilsIsObjectNullOrUndefined(oControl.max) == false) {
+                    oViewElement.m_iMax = oControl.max;
+                }
+                if (utilsIsObjectNullOrUndefined(oControl.default) == false) {
+                    oViewElement.m_iValue = oControl.default;
+                }
+
+            }
+            else if (oControl.type === "hidden") {
+                oViewElement = new Hidden();
+                oViewElement.m_oValue = oControl.default;
+            }
+            else {
+                oViewElement = new TextBox();
             }
 
-        }
-        else if (oControl.type === "hidden") {
-            oViewElement = new Hidden();
-            oViewElement.m_oValue = oControl.default;
-        }
-        else {
-            oViewElement = new TextBox();
-        }
+            oViewElement.type = oControl.type;
+            oViewElement.label = oControl.label;
+            oViewElement.paramName = oControl.param;
+            oViewElement.required = oControl.required
 
-        oViewElement.type = oControl.type;
-        oViewElement.label = oControl.label;
-        oViewElement.paramName = oControl.param;
-        oViewElement.required = oControl.required
-
-        return oViewElement;
-    }
-
-    this.getTabElements = function (oTab) {
-
-        let aoTabElements = [];
-
-        for (let iControl=0; iControl<oTab.controls.length; iControl ++) {
-            let oControl = oTab.controls[iControl];
-
-            let oViewElement = this.createViewElement(oControl);
-
-            aoTabElements.push(oViewElement);
+            return oViewElement;
         }
 
-        return aoTabElements;
-    }
-}
+        this.getTabElements = function (oTab) {
 
-/**
- * Search EO Image Control Class
- * @constructor
- */
-let SearchEOImage = function() {
-    this.oTableOfProducts = new ProductList();
-    this.oStartDate = new DateTimePicker();
-    this.oEndDate = new DateTimePicker();
-    this.oSelectArea = new SelectArea();
-    this.aoProviders = [];
-    this.aoProviders.push("ONDA");
-    this.aoMissionsFilters = [];
+            let aoTabElements = [];
 
+            for (let iControl = 0; iControl < oTab.controls.length; iControl++) {
+                let oControl = oTab.controls[iControl];
 
-    /*
-    let tst7 = oFactory.CreateViewElement("searcheoimage");
-    tst7.sLabel = "Sono una light search";
-    tst7.oStartDate.m_sDate =  moment().subtract(1, 'days').startOf('day');
-    tst7.oEndDate.m_sDate = moment();
-    tst7.oSelectArea.iHeight = 200;
-    tst7.oSelectArea.iWidth = 500;
-    tst7.aoProviders.push(providers.ONDA);
-    tst7.aoMissionsFilters.push({name:"sentinel-1" },{name:"sentinel-2" });
-    tst7.oTableOfProducts.isAvailableSelection = true;
-    tst7.oTableOfProducts.isSingleSelection = true;
-    */
+                let oViewElement = this.createViewElement(oControl);
 
-    /**
-     * This control does not really return a value
-     * @returns {string}
-     */
-    this.getValue = function () {
-        return "";
+                aoTabElements.push(oViewElement);
+            }
+
+            return aoTabElements;
+        }
     }
 
     /**
-     * This control does not really return a value
-     * @returns {string}
+     * Search EO Image Control Class
+     * @constructor
      */
-    this.getStringValue = function () {
-        return "";
-    }
-};
+    let SearchEOImage = function () {
+        this.oTableOfProducts = new ProductList();
+        this.oStartDate = new DateTimePicker();
+        this.oEndDate = new DateTimePicker();
+        this.oSelectArea = new SelectArea();
+        this.aoProviders = [];
+        this.aoProviders.push("ONDA");
+        this.aoMissionsFilters = [];
 
-/**
- * Product List Control Class
- * @constructor
- */
-let ProductList = function(){
-    this.aoProducts = [];
-    this.isAvailableSelection = false;
-    this.isSingleSelection = true;
-    this.oSingleSelectionLayer = {};
 
-    /**
-     * Return the selected product
-     * @returns {{}}
-     */
-    this.getValue = function () {
-        return this.oSingleSelectionLayer;
-    }
+        /*
+        let tst7 = oFactory.CreateViewElement("searcheoimage");
+        tst7.sLabel = "Sono una light search";
+        tst7.oStartDate.m_sDate =  moment().subtract(1, 'days').startOf('day');
+        tst7.oEndDate.m_sDate = moment();
+        tst7.oSelectArea.iHeight = 200;
+        tst7.oSelectArea.iWidth = 500;
+        tst7.aoProviders.push(providers.ONDA);
+        tst7.aoMissionsFilters.push({name:"sentinel-1" },{name:"sentinel-2" });
+        tst7.oTableOfProducts.isAvailableSelection = true;
+        tst7.oTableOfProducts.isSingleSelection = true;
+        */
 
-    /**
-     * Return the name of the selected product
-     * @returns {{}}
-     */
-    this.getStringValue = function () {
-        return this.oSingleSelectionLayer;
-    }
-
-};
-
-/**
- * Date Time Picker Control Class
- * @constructor
- */
-let DateTimePicker = function(){
-    this.m_sDate = null;
-
-    /**
-     * Returns the selected Date
-     * @returns {string|null} Date as a string in format YYYY-MM-DD
-     */
-    this.getValue = function () {
-        if (this.m_sDate) {
-            return this.m_sDate;
-        }
-        else {
+        /**
+         * This control does not really return a value
+         * @returns {string}
+         */
+        this.getValue = function () {
             return "";
         }
-    }
 
-    /**
-     * Returns the selected Date
-     * @returns {string|null} Date as a string in format YYYY-MM-DD
-     */
-    this.getStringValue = function () {
-        if (this.m_sDate) {
-            return this.m_sDate;
-        }
-        else {
+        /**
+         * This control does not really return a value
+         * @returns {string}
+         */
+        this.getStringValue = function () {
             return "";
         }
-    }
-
-};
-
-/**
- * Select Area (bbox) Control Class
- * @constructor
- */
-let SelectArea = function () {
-    this.oBoundingBox = {
-        northEast : "",
-        southWest : ""
     };
-    this.iWidth = "";
-    this.iHeight = "";
 
     /**
-     * Return the bbox as a JSON Obkect
-     * @returns {{southWest: {lat: "", lon:""}, northEast: {lat: "", lon:""}}|string}
+     * Product List Control Class
+     * @constructor
      */
-    this.getValue = function () {
-        try {
-            return this.oBoundingBox;
+    let ProductList = function () {
+        this.aoProducts = [];
+        this.isAvailableSelection = false;
+        this.isSingleSelection = true;
+        this.oSingleSelectionLayer = {};
+
+        /**
+         * Return the selected product
+         * @returns {{}}
+         */
+        this.getValue = function () {
+            return this.oSingleSelectionLayer;
         }
-        catch (e) {
-            return "";
+
+        /**
+         * Return the name of the selected product
+         * @returns {{}}
+         */
+        this.getStringValue = function () {
+            return this.oSingleSelectionLayer;
         }
-    }
+
+    };
 
     /**
-     * Return the bounding box as a string.
-     * @returns {string} BBox as string: LATN,LONW,LATS,LONE
+     * Date Time Picker Control Class
+     * @constructor
      */
-    this.getStringValue = function () {
-        try {
-            if (this.oBoundingBox) {
-                return "" + this.oBoundingBox.northEast.lat.toFixed(2) + "," + this.oBoundingBox.southWest.lng.toFixed(2)+"," + this.oBoundingBox.southWest.lat.toFixed(2)+","+ + this.oBoundingBox.northEast.lng.toFixed(2);
+    let DateTimePicker = function () {
+        this.m_sDate = null;
+
+        /**
+         * Returns the selected Date
+         * @returns {string|null} Date as a string in format YYYY-MM-DD
+         */
+        this.getValue = function () {
+            if (this.m_sDate) {
+                return this.m_sDate;
             }
             else {
                 return "";
             }
         }
-        catch (e) {
-            return "";
+
+        /**
+         * Returns the selected Date
+         * @returns {string|null} Date as a string in format YYYY-MM-DD
+         */
+        this.getStringValue = function () {
+            if (this.m_sDate) {
+                return this.m_sDate;
+            }
+            else {
+                return "";
+            }
         }
-    }
-};
 
-
-
-/**
- * Text Box Control Class
- * @constructor
- */
-let TextBox = function () {
-    this.m_sText = "";
+    };
 
     /**
-     * Get the value of the textbox
-     * @returns {string} String in the textbox
+     * Select Area (bbox) Control Class
+     * @constructor
      */
-    this.getValue = function () {
-        return this.m_sText;
-    }
+    let SelectArea = function () {
+        this.oBoundingBox = {
+            northEast: "",
+            southWest: ""
+        };
+        this.iWidth = "";
+        this.iHeight = "";
 
-    /**
-     * Get the value of the textbox
-     * @returns {string} String in the textbox
-     */
-    this.getStringValue = function () {
-        return this.m_sText;
-    }
-};
-
-/**
- * Hidden Control Class
- * @constructor
- */
-let Hidden = function () {
-    this.m_oValue = "";
-
-    /**
-     * Get the value of the control
-     * @returns {string} String in the control
-     */
-    this.getValue = function () {
-        return this.m_oValue;
-    }
-
-    /**
-     * Get the value of the control
-     * @returns {string} String in the control
-     */
-    this.getStringValue = function () {
-        return String(this.m_oValue);
-    }
-};
-
-/**
- * Check box Control Class
- * @constructor
- */
-let CheckBox = function () {
-    this.m_bValue = true;
-
-    /**
-     * Return the value of the checkbox
-     * @returns {boolean} True if selected, False if not
-     */
-    this.getValue = function () {
-        return this.m_bValue;
-    }
-
-    /**
-     * Return the value of the checkbox as a string:
-     * 1 = true 0 = false
-     * @returns {string}
-     */
-    this.getStringValue = function () {
-        if (this.m_bValue) {
-            return "1";
+        /**
+         * Return the bbox as a JSON Obkect
+         * @returns {{southWest: {lat: "", lon:""}, northEast: {lat: "", lon:""}}|string}
+         */
+        this.getValue = function () {
+            try {
+                return this.oBoundingBox;
+            }
+            catch (e) {
+                return "";
+            }
         }
-        else {
-            return "0";
+
+        /**
+         * Return the bounding box as a string.
+         * @returns {string} BBox as string: LATN,LONW,LATS,LONE
+         */
+        this.getStringValue = function () {
+            try {
+                if (this.oBoundingBox) {
+                    return "" + this.oBoundingBox.northEast.lat.toFixed(2) + "," + this.oBoundingBox.southWest.lng.toFixed(2) + "," + this.oBoundingBox.southWest.lat.toFixed(2) + "," + + this.oBoundingBox.northEast.lng.toFixed(2);
+                }
+                else {
+                    return "";
+                }
+            }
+            catch (e) {
+                return "";
+            }
         }
-    }
+    };
 
-};
 
-/**
- * Drop Down Control Class
- * @constructor
- */
-let DropDown = function () {
-    this.asListValues = [];
-    this.sSelectedValues = "";
-    this.oOnClickFunction = null;
-    this.bEnableSearchFilter = true;
-    this.sDropdownName = "";
 
     /**
-     * Get the selected value
-     * @returns {string}
+     * Text Box Control Class
+     * @constructor
      */
-    this.getValue = function () {
-        return this.sSelectedValues.name;
-    }
+    let TextBox = function () {
+        this.m_sText = "";
+
+        /**
+         * Get the value of the textbox
+         * @returns {string} String in the textbox
+         */
+        this.getValue = function () {
+            return this.m_sText;
+        }
+
+        /**
+         * Get the value of the textbox
+         * @returns {string} String in the textbox
+         */
+        this.getStringValue = function () {
+            return this.m_sText;
+        }
+    };
 
     /**
-     * Get the selected value
-     * @returns {string}
+     * Hidden Control Class
+     * @constructor
      */
-    this.getStringValue = function () {
-        return this.sSelectedValues.name;
-    }
-};
+    let Hidden = function () {
+        this.m_oValue = "";
 
+        /**
+         * Get the value of the control
+         * @returns {string} String in the control
+         */
+        this.getValue = function () {
+            return this.m_oValue;
+        }
 
-/**
- * Products Combo Control Class
- * @constructor
- */
-let ProductsCombo = function () {
-    this.asListValues = [];
-    this.sSelectedValues = "";
-    this.oOnClickFunction = null;
-    this.bEnableSearchFilter = true;
-    this.sDropdownName = "";
-    this.bShowExtension = true;
+        /**
+         * Get the value of the control
+         * @returns {string} String in the control
+         */
+        this.getStringValue = function () {
+            return String(this.m_oValue);
+        }
+    };
 
     /**
-     * Get the selected value
-     * @returns {string}
+     * Check box Control Class
+     * @constructor
      */
-    this.getValue = function () {
-        return this.sSelectedValues.name;
-    }
+    let CheckBox = function () {
+        this.m_bValue = true;
 
-    this.getStringValue = function () {
-        return this.sSelectedValues.name;
-    }
-};
+        /**
+         * Return the value of the checkbox
+         * @returns {boolean} True if selected, False if not
+         */
+        this.getValue = function () {
+            return this.m_bValue;
+        }
 
-/**
- * Slider for a numeric input
- * @constructor
- */
-let Slider = function () {
-    this.m_iMin = 0;
-    this.m_iMax = 10;
-    this.m_iValue = 5;
+        /**
+         * Return the value of the checkbox as a string:
+         * 1 = true 0 = false
+         * @returns {string}
+         */
+        this.getStringValue = function () {
+            if (this.m_bValue) {
+                return "1";
+            }
+            else {
+                return "0";
+            }
+        }
+
+    };
 
     /**
-     * Get the selected value
-     * @returns {number}
+     * Drop Down Control Class
+     * @constructor
      */
-    this.getValue = function () {
-        return this.m_iValue;
-    }
+    let DropDown = function () {
+        this.asListValues = [];
+        this.sSelectedValues = "";
+        this.oOnClickFunction = null;
+        this.bEnableSearchFilter = true;
+        this.sDropdownName = "";
+
+        /**
+         * Get the selected value
+         * @returns {string}
+         */
+        this.getValue = function () {
+            return this.sSelectedValues.name;
+        }
+
+        /**
+         * Get the selected value
+         * @returns {string}
+         */
+        this.getStringValue = function () {
+            return this.sSelectedValues.name;
+        }
+    };
+
 
     /**
-     * Get the selected value as a string
-     * @returns {number}
+     * Products Combo Control Class
+     * @constructor
      */
-    this.getStringValue = function () {
-        return String(this.m_iValue);
-    }
+    let ProductsCombo = function (bShowExt) {
+        this.asListValues = [];
+        this.sSelectedValues = "";
+        this.oOnClickFunction = null;
+        this.bEnableSearchFilter = true;
+        this.sDropdownName = "";
+        this.bShowExtension = bShowExt;
 
-}
+        /**
+         * Get the selected value
+         * @returns {string}
+         */
+        this.getValue = function () {
+            return this.sSelectedValues.name;
+        }
+
+        this.getStringValue = function () {
+            return this.sSelectedValues.name;
+        }
+    };
+
+    /**
+     * Slider for a numeric input
+     * @constructor
+     */
+    let Slider = function () {
+        this.m_iMin = 0;
+        this.m_iMax = 10;
+        this.m_iValue = 5;
+
+        /**
+         * Get the selected value
+         * @returns {number}
+         */
+        this.getValue = function () {
+            return this.m_iValue;
+        }
+
+        /**
+         * Get the selected value as a string
+         * @returns {number}
+         */
+        this.getStringValue = function () {
+            return String(this.m_iValue);
+        }
+
+    }
