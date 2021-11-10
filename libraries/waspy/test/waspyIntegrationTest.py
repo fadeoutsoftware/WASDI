bd800360d47c576875ed0ce51631a3db51163bc3
Cristiano Nattero <c.nattero@fadeout.it>
2021-11-10 17:21:33 +0100

    WIP importing unit tests

--- libraries/waspy/test/waspyIntegrationTest.py
+++ libraries/waspy/test/waspyIntegrationTest.py
@@ -9,6 +9,8 @@ from datetime import date, timedelta
 
 import wasdi
 
+unittest.TestLoader.sortTestMethodsUsing = None
+
 
 class WaspyIntegrationTests(unittest.TestCase):
     '''
@@ -17,6 +19,7 @@ class WaspyIntegrationTests(unittest.TestCase):
     workspace initialized a the beginning of each test.
     To test everything please create a non empty ws and set it in config.json for initialization
     '''
+
     # todo create unique workspace name each time
     # m_sWorkspaceName = ""
     # m_iWorkspaceNameLen = 64
@@ -30,7 +33,15 @@ class WaspyIntegrationTests(unittest.TestCase):
     def setUpClass(cls):
         cls.m_iWorkspaceNameLen = 512
         cls.m_sWorkspaceName = cls.randomString(cls.m_iWorkspaceNameLen)
-        cls.clearWorkspaces(cls)
+
+        wasdi.init("./config.json")
+        URL = "https://test.wasdi.net/wasdiwebserver/rest"
+        print("swapping Base url to " + URL)
+        wasdi.setBaseUrl(URL)
+        wasdi.setWorkspaceBaseUrl("https://test.wasdi.net/wasdiwebserver/rest")
+        wasdi.getWorkspaceBaseUrl()
+
+        # cls.clearWorkspaces(cls)
 
     @classmethod
     def randomString(cls, size=6, chars=string.ascii_uppercase + string.digits):
@@ -58,148 +69,155 @@ class WaspyIntegrationTests(unittest.TestCase):
         for sProduct in asProducts:
             wasdi.deleteProduct(sProduct)
 
-    def setUp(self):
-        # init library
-        wasdi.init("./config.json")
-        URL = "https://test.wasdi.net/wasdiwebserver/rest"
-        print("swapping Base url to " + URL)
-        wasdi.setBaseUrl(URL)
-        wasdi.setWorkspaceBaseUrl("https://test.wasdi.net/wasdiwebserver/rest")
+    # def test_01_createWorkspace():
+    # {
+    #     LOGGER.info("createWorkspace");
+    #
+    # String
+    # createdWorkspaceId = wasdi.createWorkspace(sWorkspaceName, sNodeCode);
+    #
+    # String
+    # foundWorkspaceId = wasdi.getWorkspaceIdByName(sWorkspaceName);
+    #
+    # assertEquals(createdWorkspaceId, foundWorkspaceId);
+    # }
 
-    # 
+
+
+
+
+    #
     # test begin here
     #
 
-    def test_createAndDeleteWorkspace(self):
-        '''
-        Creates and delete a workspace by using its Id !
-        returns the id itself, if deleted
-        '''
-        print(self.m_sWorkspaceName)
-        sWsIdAtCreation = wasdi.createWorkspace(self.m_sWorkspaceName)
-        self.assertIsNotNone(sWsIdAtCreation)
-        wsId = wasdi.getWorkspaceIdByName(self.m_sWorkspaceName)
-        self.assertEqual(wsId, wasdi.getActiveWorkspaceId())
-        asProducts = wasdi.getProductsByActiveWorkspace()
-        self.assertEqual(0, len(asProducts))
-        self.assertTrue(wasdi.deleteWorkspace(wsId))
-
-    def test_ListProduct(self):
-        self.clearProducts()
-        self.assertEqual(0, wasdi.getProductsByActiveWorkspace())
-
-
-    def test_PrintStatus(self):
-        wasdi.printStatus()
-
-    def test_hello(self):
-        self.assertTrue(wasdi.hello().__contains__("Hello Wasdi"))
-
-    def test_GetListOfWorkspace(self):
-        '''assume that, at least, the testing user has a workspace'''
-        self.assertTrue(len(wasdi.getWorkspaces()) > 0)
-
-    def test_getWorkspaceOwnerByName(self):
-        '''
-        Creates a workspace checks its owner and delete the workspace by using its Id !
-        '''
-        oOwner = wasdi.getUser();
-        self.assertIsNotNone(wasdi.createWorkspace(self.m_sWorkspaceName))
-        wsId = wasdi.getWorkspaceIdByName(self.m_sWorkspaceName)
-        self.assertEqual(wasdi.getWorkspaceOwnerByName(self.m_sWorkspaceName), oOwner)
-        wasdi.deleteWorkspace(wsId)
-
-    def test_getWorkspaceOwnerById(self):
-        '''
-        Creates a workspace checks its owner and delete the workspace by using its Id !
-        '''
-        oOwner = wasdi.getUser();
-        self.assertIsNotNone(wasdi.createWorkspace(self.m_sWorkspaceName))
-        wsId = wasdi.getWorkspaceIdByName(self.m_sWorkspaceName)
-        self.assertEqual(wasdi.getWorkspaceOwnerByWsId(wsId), oOwner)
-        wasdi.deleteWorkspace(wsId)
-
-    def test_openWorkspaceById(self):
-        '''
-        Creates a workspace and use it, the delete it
-        '''
-        oOwner = wasdi.getUser();
-        self.assertIsNotNone(wasdi.createWorkspace(self.m_sWorkspaceName))
-        wsId = wasdi.getWorkspaceIdByName(self.m_sWorkspaceName)
-        self.assertEqual(wasdi.openWorkspaceById(wsId), wsId)
-        wasdi.deleteWorkspace(wsId)
-
-    def test_openWorkspaceByName(self):
-        '''
-        Creates a workspace and use it, the delete it
-        '''
-        oOwner = wasdi.getUser();
-        self.assertIsNotNone(wasdi.createWorkspace(self.m_sWorkspaceName))
-        wsId = wasdi.getWorkspaceIdByName(self.m_sWorkspaceName)
-        self.assertEqual(wasdi.openWorkspace(self.m_sWorkspaceName), wsId)
-        wasdi.deleteWorkspace(wsId)
-
-    def test_getProductListByWorkspace(self):
-        self.assertTrue(len(wasdi.getProductsByActiveWorkspace()) > 0)
-
-    def test_fileExistOnWasdi(self):
-        '''Warning the setup for this test is not fully automated'''
-        on_wasdi = wasdi.fileExistsOnWasdi("S1B_IW_RAW__0SDV_20211018T170617_20211018T170649_029193_037BE3_D53A")
-        self.assertTrue(on_wasdi != False)
-
-    def test_waitProcess(self):
-        '''Warning the setup for this test is not fully automated'''
-        paramsDict = {"name": "WASDI"}
-        processID = wasdi.executeProcessor("hellowasdiworld", paramsDict)
-        if processID is None or processID == "":
-            sStatus = "somethingisbroken"
-        else:
-            sStatus = wasdi.waitProcess(processID)
-        self.assertTrue(sStatus in {"DONE", "STOPPED", "ERROR"})
-
-    def test_importProduct(self):
-        '''
-        This test checks the capabilities of WASDI to gather
-        images. This implies the search and the download of at least
-        one image on a 10 day timespan
-        '''
-
-        oFromDate = date.today() - timedelta(days=15)
-        sFromDate = oFromDate.strftime('%Y-%m-%d')
-
-        oToDate = date.today() - timedelta(days=5)
-        sToDate = oToDate.strftime('%Y-%m-%d')
-
-        eo_images = wasdi.searchEOImages("S2", sFromDate, sToDate,
-                                         None, None, None, None, None, None, None, None,
-                                         "LSA", "46.69,12.44,45.60,13.93")
-        self.assertIsNotNone(eo_images)
-        self.assertTrue(len(eo_images) > 0)
-        # In case assertion isn't violated
-        productName = eo_images[0].get("title")
-        if (len(eo_images) > 0):
-            wasdi.importProduct(eo_images[0], "LSA")
-            # The file should be available in the active workspace
-            # this is problematic -> it requires a bit of time to complete so need
-            self.assertTrue(wasdi.getProductsByActiveWorkspace().__contains__(productName))
-
-    def test_CheckProdcutExistOnWasdi(self):
-        aoProductList = wasdi.getProductsByActiveWorkspace()
-        self.assertTrue("S2B_MSIL1C_20211015T101029_N0301_R022_T32TQR_20211015T121549.zip" in aoProductList)
-
-    def test_WaitForProcessesEmptyList(self):
-        ''' Checks that, if the list passed to the methods is empty,
-         liveness conditions holds '''
-        wasdiProcIDList = []  # creates and empty list of procIDs
-        wasdi.waitProcesses(wasdiProcIDList)
-        self.assertTrue(True);  # find a better way to check timeout with assertions
-
-    def test_setProcessPayload(self):
-        '''Warning the setup for this test is not fully automated.
-        It requires the sharing of the hellowasdiworld application '''
-        pid = wasdi.executeProcessor("hellowasdiworld", "{\"name\": \"WASDI\"}")
-        self.assertTrue(wasdi.setProcessPayload(pid, "{ \"name\" : \"TestPayload\"}") != "")
-        sStatus = wasdi.waitProcess(pid)
+    # def test_createAndDeleteWorkspace(self):
+    #     '''
+    #     Creates and delete a workspace by using its Id !
+    #     returns the id itself, if deleted
+    #     '''
+    #     sWsIdAtCreation = wasdi.createWorkspace(self.m_sWorkspaceName)
+    #     self.assertIsNotNone(sWsIdAtCreation)
+    #     wsId = wasdi.getWorkspaceIdByName(self.m_sWorkspaceName)
+    #     self.assertEqual(wsId, wasdi.getActiveWorkspaceId())
+    #     asProducts = wasdi.getProductsByActiveWorkspace()
+    #     self.assertEqual(0, len(asProducts))
+    #     self.assertTrue(wasdi.deleteWorkspace(wsId))
+    #
+    # def test_ListProduct(self):
+    #     self.clearProducts()
+    #     self.assertEqual(0, wasdi.getProductsByActiveWorkspace())
+    #
+    # def test_PrintStatus(self):
+    #     wasdi.printStatus()
+    #
+    # def test_hello(self):
+    #     self.assertTrue(wasdi.hello().__contains__("Hello Wasdi"))
+    #
+    # def test_GetListOfWorkspace(self):
+    #     '''assume that, at least, the testing user has a workspace'''
+    #     self.assertTrue(len(wasdi.getWorkspaces()) > 0)
+    #
+    # def test_getWorkspaceOwnerByName(self):
+    #     '''
+    #     Creates a workspace checks its owner and delete the workspace by using its Id !
+    #     '''
+    #     oOwner = wasdi.getUser();
+    #     self.assertIsNotNone(wasdi.createWorkspace(self.m_sWorkspaceName))
+    #     wsId = wasdi.getWorkspaceIdByName(self.m_sWorkspaceName)
+    #     self.assertEqual(wasdi.getWorkspaceOwnerByName(self.m_sWorkspaceName), oOwner)
+    #     wasdi.deleteWorkspace(wsId)
+    #
+    # def test_getWorkspaceOwnerById(self):
+    #     '''
+    #     Creates a workspace checks its owner and delete the workspace by using its Id !
+    #     '''
+    #     oOwner = wasdi.getUser();
+    #     self.assertIsNotNone(wasdi.createWorkspace(self.m_sWorkspaceName))
+    #     wsId = wasdi.getWorkspaceIdByName(self.m_sWorkspaceName)
+    #     self.assertEqual(wasdi.getWorkspaceOwnerByWsId(wsId), oOwner)
+    #     wasdi.deleteWorkspace(wsId)
+    #
+    # def test_openWorkspaceById(self):
+    #     '''
+    #     Creates a workspace and use it, the delete it
+    #     '''
+    #     oOwner = wasdi.getUser();
+    #     self.assertIsNotNone(wasdi.createWorkspace(self.m_sWorkspaceName))
+    #     wsId = wasdi.getWorkspaceIdByName(self.m_sWorkspaceName)
+    #     self.assertEqual(wasdi.openWorkspaceById(wsId), wsId)
+    #     wasdi.deleteWorkspace(wsId)
+    #
+    # def test_openWorkspaceByName(self):
+    #     '''
+    #     Creates a workspace and use it, the delete it
+    #     '''
+    #     oOwner = wasdi.getUser();
+    #     self.assertIsNotNone(wasdi.createWorkspace(self.m_sWorkspaceName))
+    #     wsId = wasdi.getWorkspaceIdByName(self.m_sWorkspaceName)
+    #     self.assertEqual(wasdi.openWorkspace(self.m_sWorkspaceName), wsId)
+    #     wasdi.deleteWorkspace(wsId)
+    #
+    # def test_getProductListByWorkspace(self):
+    #     self.assertTrue(len(wasdi.getProductsByActiveWorkspace()) > 0)
+    #
+    # def test_fileExistOnWasdi(self):
+    #     '''Warning the setup for this test is not fully automated'''
+    #     on_wasdi = wasdi.fileExistsOnWasdi("S1B_IW_RAW__0SDV_20211018T170617_20211018T170649_029193_037BE3_D53A")
+    #     self.assertTrue(on_wasdi != False)
+    #
+    # def test_waitProcess(self):
+    #     '''Warning the setup for this test is not fully automated'''
+    #     paramsDict = {"name": "WASDI"}
+    #     processID = wasdi.executeProcessor("hellowasdiworld", paramsDict)
+    #     if processID is None or processID == "":
+    #         sStatus = "somethingisbroken"
+    #     else:
+    #         sStatus = wasdi.waitProcess(processID)
+    #     self.assertTrue(sStatus in {"DONE", "STOPPED", "ERROR"})
+    #
+    # def test_importProduct(self):
+    #     '''
+    #     This test checks the capabilities of WASDI to gather
+    #     images. This implies the search and the download of at least
+    #     one image on a 10 day timespan
+    #     '''
+    #
+    #     oFromDate = date.today() - timedelta(days=15)
+    #     sFromDate = oFromDate.strftime('%Y-%m-%d')
+    #
+    #     oToDate = date.today() - timedelta(days=5)
+    #     sToDate = oToDate.strftime('%Y-%m-%d')
+    #
+    #     eo_images = wasdi.searchEOImages("S2", sFromDate, sToDate,
+    #                                      None, None, None, None, None, None, None, None,
+    #                                      "LSA", "46.69,12.44,45.60,13.93")
+    #     self.assertIsNotNone(eo_images)
+    #     self.assertTrue(len(eo_images) > 0)
+    #     # In case assertion isn't violated
+    #     productName = eo_images[0].get("title")
+    #     if (len(eo_images) > 0):
+    #         wasdi.importProduct(eo_images[0], "LSA")
+    #         # The file should be available in the active workspace
+    #         # this is problematic -> it requires a bit of time to complete so need
+    #         self.assertTrue(wasdi.getProductsByActiveWorkspace().__contains__(productName))
+    #
+    # def test_CheckProdcutExistOnWasdi(self):
+    #     aoProductList = wasdi.getProductsByActiveWorkspace()
+    #     self.assertTrue("S2B_MSIL1C_20211015T101029_N0301_R022_T32TQR_20211015T121549.zip" in aoProductList)
+    #
+    # def test_WaitForProcessesEmptyList(self):
+    #     ''' Checks that, if the list passed to the methods is empty,
+    #      liveness conditions holds '''
+    #     wasdiProcIDList = []  # creates and empty list of procIDs
+    #     wasdi.waitProcesses(wasdiProcIDList)
+    #     self.assertTrue(True);  # find a better way to check timeout with assertions
+    #
+    # def test_setProcessPayload(self):
+    #     '''Warning the setup for this test is not fully automated.
+    #     It requires the sharing of the hellowasdiworld application '''
+    #     pid = wasdi.executeProcessor("hellowasdiworld", "{\"name\": \"WASDI\"}")
+    #     self.assertTrue(wasdi.setProcessPayload(pid, "{ \"name\" : \"TestPayload\"}") != "")
+    #     sStatus = wasdi.waitProcess(pid)
 
     '''def test_addFileToWasdi(self):
         wasdi.addFileToWASDI("C:\\Users\\m.menapace.FADEOUT\\Documents\\Fadeout\\WASDI\\libraries\\waspy\\test\\256x256.tif")
