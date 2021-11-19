bb761376e25fe700ad81676a8ff7936c8e186f15
ppetrescu-wasdi <91553442+ppetrescu-wasdi@users.noreply.github.com>
2021-11-19 10:17:00 +0100

    #263: User/Application parameters library client and server side - revisiting the Detail and List ViewModel

---
+++ wrappersnap/wasdishared/src/main/java/wasdi/shared/viewmodels/processors/ProcessorParametersTemplateDetailViewModel.java
@@ -0,0 +1,29 @@
+package wasdi.shared.viewmodels.processors;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * ProcessorParametersTemplate Detail View Model
+ * 
+ * Represents a ProcessorParametersTemplate
+ * 
+ * @author PetruPetrescu
+ *
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProcessorParametersTemplateDetailViewModel {
+
+	private String templateId;
+	private String userId;
+	private String processorId;
+	private String name;
+	private String description;
+	private String jsonParameters;
+
+}
