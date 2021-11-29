6ee2755a3c118bc4b2444d1e93dc3477cbaeb8a1
pcampanella <p.campanella@fadeout.it>
2021-11-29 19:32:32 +0100

    removed python 2.7

--- wrappersnap/wasdishared/src/main/java/wasdi/shared/business/ProcessorTypes.java
+++ wrappersnap/wasdishared/src/main/java/wasdi/shared/business/ProcessorTypes.java
@@ -10,7 +10,7 @@ public class ProcessorTypes {
 	private ProcessorTypes() {
 		// / private constructor to hide the public implicit one 
 	}
-	public static String UBUNTU_PYTHON27_SNAP = "ubuntu_python_snap";
+	//public static String UBUNTU_PYTHON27_SNAP = "ubuntu_python_snap";
 	public static String UBUNTU_PYTHON37_SNAP = "ubuntu_python37_snap";
 	public static String IDL = "ubuntu_idl372";
 	public static String OCTAVE = "octave";
@@ -23,7 +23,6 @@ public class ProcessorTypes {
 	 */
 	public static String getTemplateFolder(String sProcessorType) {
 		if (sProcessorType.equals(IDL)) return "idl";
-		else if (sProcessorType.equals(UBUNTU_PYTHON27_SNAP)) return "python27";
 		else if (sProcessorType.equals(UBUNTU_PYTHON37_SNAP)) return "python37";
 		else if (sProcessorType.equals(OCTAVE)) return "octave";
 		else if (sProcessorType.equals(CONDA)) return "conda";
