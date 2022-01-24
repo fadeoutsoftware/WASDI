a89cb99c816cb6f61343c64800bbbbb633b5b382
Marco Menapace <m.menapace@fadeout.it>
2022-01-24 16:45:37 +0100

    tested setup, login and helloworld ok !

--- libraries/jswasdilib/index.js
+++ libraries/jswasdilib/index.js
@@ -1,9 +1,15 @@
 import fetch from 'node-fetch';
-'use strict';
-class Wasdi {
-    constructor(){}
 
-    helloWasdiWorld(){
+'use strict';
+
+class Wasdi {
+    constructor() {
+    }
+
+    /**
+     * Test method to check wasdi instance, with a tiny bit of developer's traditions
+     */
+    helloWasdiWorld() {
 
         var requestOptions = {
             method: 'GET',
@@ -15,9 +21,41 @@ class Wasdi {
             .then(result => console.log(result))
             .catch(error => console.log('error', error));
     }
+
+    /**
+     * Api call for the login to Wasdi services
+     * @param sUserName The username, corresponding to the e-mail used during registration
+     * @param sPassword The selected password
+     */
+    login(sUserName, sPassword) {
+        /*var myHeaders = new fetch.Headers();
+        myHeaders.append("Content-Type", "application/x-www-form-urlencoded");*/
+
+        var urlencoded = new URLSearchParams();
+        urlencoded.append("client_id", "wasdi_client");
+        urlencoded.append("grant_type", "password");
+        urlencoded.append("username", sUserName);
+        urlencoded.append("password", sPassword);
+
+        var requestOptions = {
+            method: 'POST',
+            headers: {"Content-Type": "application/x-www-form-urlencoded"},
+            body: urlencoded,
+            redirect: 'follow'
+        };
+
+        fetch("https://www.wasdi.net/auth/realms/wasdi/protocol/openid-connect/token", requestOptions)
+            .then(response => response.text())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
+    }
 }
 
 var wasdiInstance = new Wasdi();
 wasdiInstance.helloWasdiWorld();
 wasdiInstance.helloWasdiWorld();
+// Template - removed private credentials
+// wasdiInstance.login(username, password);
+
+
 
