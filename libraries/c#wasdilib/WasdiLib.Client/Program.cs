ed56f6831d24fc5b2a0adb9de892a8603d46e01e
ppetrescu-wasdi <petru.petrescu@wasdi.cloud>
2022-01-31 15:59:27 +0100

    C# Wasdi Lib - adding the lib app

---
+++ libraries/c#wasdilib/WasdiLib.Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WasdiLib.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program.Main()");
+            WasdiLib wasdi = new WasdiLib();
+            Console.WriteLine(wasdi.HelloWasdi());
+        }
+    }
+
+}
