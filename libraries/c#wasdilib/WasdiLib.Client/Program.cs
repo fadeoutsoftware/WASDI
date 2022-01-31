c374f488bb2f131e7d9a452ee0fded303b453415
ppetrescu-wasdi <petru.petrescu@wasdi.cloud>
2022-01-31 17:04:52 +0100

    C# Wasdi Lib - adding the client console-app

---
+++ libraries/c#wasdilib/WasdiLib.Client/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WasdiLib.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program.Main()");
+
+            WasdiLib wasdi = new WasdiLib();
+            Console.WriteLine(wasdi.HelloWasdi());
+        }
+    }
+
+}
