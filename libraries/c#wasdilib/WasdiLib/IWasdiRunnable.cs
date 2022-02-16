2f6b651bfa8b09a69d94372e41dc002062a1404e
ppetrescu-wasdi <petru.petrescu@wasdi.cloud>
2022-02-16 19:19:02 +0100

    C# Wasdi Lib - adding the IWasdiRunnable interface

---
+++ libraries/c#wasdilib/WasdiLib/IWasdiRunnable.cs
@@ -0,0 +1,17 @@
+﻿namespace WasdiLib
+{
+    /// <summary>
+    /// Wasdi interface to be implemented by a class that is meant to run on the Wasdi platform.
+    /// </summary>
+    public interface IWasdiRunnable
+    {
+
+        /// <summary>
+        /// This is the entry point of an application deployed on the Wasdi platform.
+        /// The Wasdi platform will invoke the implementation of this Run method.
+        /// </summary>
+        /// <returns>the status of the execution (i.e. CREATED,  RUNNING,  STOPPED,  DONE,  ERROR, WAITING, READY)</returns>
+        string Run();
+
+    }
+}
