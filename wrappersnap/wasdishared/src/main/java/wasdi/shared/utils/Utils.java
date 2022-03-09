6270ec28bb6786dfecbe6d9f7f530552a505585d
ppetrescu-wasdi <petru.petrescu@wasdi.cloud>
2022-03-09 21:03:28 +0100

    #514: For the Terrascope provider, DEM & WorldCover platform, adjust the user-provided dates so that the search produces valid results

--- wrappersnap/wasdishared/src/main/java/wasdi/shared/utils/Utils.java
+++ wrappersnap/wasdishared/src/main/java/wasdi/shared/utils/Utils.java
@@ -156,7 +156,11 @@ public class Utils {
 
 		return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(oDate);
 	}
-	
+
+	public static String formatToYyyyDashMMDashdd(Date oDate) {
+		return new SimpleDateFormat("yyyy-MM-dd").format(oDate);
+	}
+
 	public static Date getWasdiDate(String sWasdiDate) {
 
 		try {
