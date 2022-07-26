d50b9a8ea7a2fd758e42ee6f3245d97f8c37b8a0
Marco Menapace <m.menapace@fadeout.it>
2022-07-26 15:35:09 +0200

    Upgrading npm version

--- docs/conf.py
+++ docs/conf.py
@@ -84,5 +84,5 @@ os.system("echo --- overwriting domain.py with the fixed version ---")
 os.system("mv domain.py /home/docs/checkouts/readthedocs.org/user_builds/wasdi/envs/latest/lib/python3.7/site-packages/javasphinx/domain.py")
 
 os.system("echo --- FIX for npm version ---")
-os.system('cd ~ ; npm install -g typedoc@0.15.0;')
+os.system('cd ~ ;npm install -g npm@8.15.0; npm install -g typedoc@0.15.0;')
 
