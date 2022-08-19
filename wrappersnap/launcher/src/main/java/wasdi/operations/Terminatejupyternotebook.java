b7328cfea46c348829b600829fd26a5fbb54f38b
ppetrescu-wasdi <petru.petrescu@wasdi.cloud>
2022-08-19 14:23:06 +0200

    #373: Add online Python (Jupyter) Editor - terminate JN

--- wrappersnap/launcher/src/main/java/wasdi/operations/Terminatejupyternotebook.java
+++ wrappersnap/launcher/src/main/java/wasdi/operations/Terminatejupyternotebook.java
@@ -4,10 +4,10 @@ import wasdi.processors.JupyterNotebookProcessorEngine;
 import wasdi.processors.WasdiProcessorEngine;
 import wasdi.shared.LauncherOperations;
 import wasdi.shared.business.ProcessWorkspace;
-import wasdi.shared.business.Processor;
+//import wasdi.shared.business.Processor;
 import wasdi.shared.business.Workspace;
 import wasdi.shared.config.WasdiConfig;
-import wasdi.shared.data.ProcessorRepository;
+//import wasdi.shared.data.ProcessorRepository;
 import wasdi.shared.data.WorkspaceRepository;
 import wasdi.shared.parameters.BaseParameter;
 import wasdi.shared.parameters.ProcessorParameter;
@@ -32,17 +32,17 @@ public class Terminatejupyternotebook extends Operation {
 		try {
 			ProcessorParameter oParameter = (ProcessorParameter) oParam;
 
-			// First Check if processor exists
-			String sProcessorId = oParameter.getProcessorID();
+//			// First Check if processor exists
+//			String sProcessorId = oParameter.getProcessorID();
 
-			ProcessorRepository oProcessorRepository = new ProcessorRepository();
-			Processor oProcessor = oProcessorRepository.getProcessor(sProcessorId);
+//			ProcessorRepository oProcessorRepository = new ProcessorRepository();
+//			Processor oProcessor = oProcessorRepository.getProcessor(sProcessorId);
 
-			// Check processor
-			if (oProcessor == null) {
-				m_oLocalLogger.error("Terminatejupyternotebook.executeOperation: oProcessor is null [" + sProcessorId + "]");
-				return false;
-			}
+//			// Check processor
+//			if (oProcessor == null) {
+//				m_oLocalLogger.error("Terminatejupyternotebook.executeOperation: oProcessor is null [" + sProcessorId + "]");
+//				return false;
+//			}
 
 			JupyterNotebookProcessorEngine oEngine = (JupyterNotebookProcessorEngine) WasdiProcessorEngine.getProcessorEngine(oParameter.getProcessorType());
 			oEngine.setSendToRabbit(m_oSendToRabbit);
