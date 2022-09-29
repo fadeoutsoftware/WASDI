14f84697e1f73990b306a07929d1895a0f249bd9
bspurgeon08 <betty.spurgeon@wasdi.cloud>
2022-09-29 18:17:57 +0200

    User Runtime and Account type display: Added runtime and re-formatted dialog

--- client/app/dialogs/edit_user/EditUserController.js
+++ client/app/dialogs/edit_user/EditUserController.js
@@ -5,18 +5,21 @@
 
 var EditUserController = (function() {
 
-    function EditUserController ($scope, oClose,oExtras,oAuthService,oConstantsService) {
+    function EditUserController ($scope, oClose,oExtras,oAuthService,oConstantsService, oProcessWorkspaceService) {
         //MEMBERS
         this.m_oScope = $scope;
         this.m_oScope.m_oController = this;
         this.m_oExtras = oExtras;
         this.m_oAuthService = oAuthService;
         this.m_oConstantsService = oConstantsService;
+        this.m_oProcessWorkspaceService = oProcessWorkspaceService;
         this.m_oUser = this.m_oExtras.user;
         this.m_bEditingPassword = false;
         this.m_bEditingUserInfo = false;
         this.m_oEditUser = {};
+        this.m_lTotalRuntime = null; 
         this.initializeEditUserInfo();
+        this.initializeUserRuntimeInfo();
 
         $scope.close = function(result) {
             oClose(result, 300); // close, but give 500ms for bootstrap to animate
@@ -163,14 +166,40 @@ var EditUserController = (function() {
         }
     };
 
+    EditUserController.prototype.initializeUserRuntimeInfo = function() {
+        let oController = this;
+        if (utilsIsStrNullOrEmpty(this.m_oUser.userId) === true) {
+            utilsVexDialogAlertTop(
+                "GURU MEDITATION<br>A VALID USER MUST BE PROVIDED"
+            );
+
+            return false;
+        }
+
+        
+        this.m_oProcessWorkspaceService.getProcessWorkspaceTotalRunningTimeByUserAndInterval(this.m_oUser.userId).then(
+            function (data) {
+                        console.log(data);
+                        if (utilsIsObjectNullOrUndefined(data.data) === false) {
+                            oController.m_lTotalRuntime = new Date(data.data).toISOString().slice(11, 19);
+                        } else {
+                            utilsVexDialogAlertTop(
+                                "GURU MEDITATION<br>ERROR IN GETTING THE TOTAL RUNNING TIME"
+                            );
+                        }
+
+                        return true;
+                    }
+        );
+    }; 
+
     EditUserController.$inject = [
         '$scope',
         'close',
         'extras',
         'AuthService',
-        'ConstantsService'
-
-
+        'ConstantsService',
+        'ProcessWorkspaceService'
     ];
     return EditUserController ;
 })();
