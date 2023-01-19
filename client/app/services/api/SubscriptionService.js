be4fa739c7f9bd169ecd57b8b9ee32c435f590b9
ppetrescu-wasdi <petru.petrescu@wasdi.cloud>
2023-01-19 14:04:09 +0100

    adding CRUD operations support for Subscriptions

---
+++ client/app/services/api/SubscriptionService.js
@@ -0,0 +1,57 @@
+/**
+ * Created by p.petrescu on 19/01/2023.
+ */
+
+'use strict';
+angular.module('wasdi.SubscriptionService', ['wasdi.SubscriptionService']).
+service('SubscriptionService', ['$http',  'ConstantsService', function ($http, oConstantsService) {
+    this.APIURL = oConstantsService.getAPIURL();
+    this.m_oHttp = $http;
+
+    this.getSubscriptionsListByUser = function () {
+        return this.m_oHttp.get(this.APIURL + '/subscriptions/byuser');
+    };
+
+    this.getSubscriptionById = function (sSubscriptionId) {
+        return this.m_oHttp.get(this.APIURL + '/subscriptions/byId?subscription=' + sSubscriptionId);
+    };
+
+    this.saveSubscription = function (oSubscription) {
+        console.log("SubscriptionService.saveSubscription | oSubscription: ", oSubscription);
+        if (utilsIsStrNullOrEmpty(oSubscription.subscriptionId)) {
+            return this.createSubscription(oSubscription);
+        } else {
+            return this.updateSubscription(oSubscription);
+        }
+    };
+
+    this.createSubscription = function (oSubscription) {
+        console.log("SubscriptionService.createSubscription | m_oEditSubscription: ", oSubscription);
+        return this.m_oHttp.post(this.APIURL + '/subscriptions/add', oSubscription);
+    };
+
+    this.updateSubscription = function (oSubscription) {
+        console.log("SubscriptionService.updateSubscription | oSubscription: ", oSubscription);
+        return this.m_oHttp.put(this.APIURL + '/subscriptions/update', oSubscription);
+    };
+
+    this.deleteSubscription = function (sSubscriptionId) {
+        return this.m_oHttp.delete(this.APIURL + '/subscriptions/delete?subscription=' + sSubscriptionId);
+    };
+
+    // // Get list of shared users by subscription id
+    // this.getUsersBySharedSubscription = function (sSubscriptionId) {
+    //     return this.m_oHttp.get(this.APIURL + '/subscriptions/share/bysubscription?subscription=' + sSubscriptionId);
+    // }
+
+    // // Add sharing
+    // this.addSubscriptionSharing = function (sSubscriptionId, sUserId) {
+    //     return this.m_oHttp.post(this.APIURL + '/subscriptions/share/add?subscription=' + sSubscriptionId + '&userId=' + sUserId);
+    // }
+
+    // // Remove sharing
+    // this.removeSubscriptionSharing = function (sSubscriptionId, sUserId) {
+    //     return this.m_oHttp.delete(this.APIURL + '/subscriptions/share/delete?subscription=' + sSubscriptionId + '&userId=' + sUserId);
+    // }
+
+}]);
