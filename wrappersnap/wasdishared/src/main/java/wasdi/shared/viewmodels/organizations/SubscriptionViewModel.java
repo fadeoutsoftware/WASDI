ffd3bae3c0634e2d3f4692c23888155dd7e8076d
ppetrescu-wasdi <petru.petrescu@wasdi.cloud>
2023-02-14 12:31:14 +0100

    trying to fix the bug related to the Date-type for POST calls

--- wrappersnap/wasdishared/src/main/java/wasdi/shared/viewmodels/organizations/SubscriptionViewModel.java
+++ wrappersnap/wasdishared/src/main/java/wasdi/shared/viewmodels/organizations/SubscriptionViewModel.java
@@ -1,10 +1,5 @@
 package wasdi.shared.viewmodels.organizations;
 
-import java.util.Date;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,19 +18,9 @@ public class SubscriptionViewModel {
 	private String description;
 	private String typeId;
 	private String typeName;
-
-	@JsonFormat(shape=JsonFormat.Shape.STRING)
-	@JsonProperty("buyDate")
-	private Date buyDate;
-
-	@JsonFormat(shape=JsonFormat.Shape.STRING)
-	@JsonProperty("startDate")
-	private Date startDate;
-
-	@JsonFormat(shape=JsonFormat.Shape.STRING)
-	@JsonProperty("endDate")
-	private Date endDate;
-
+	private String buyDate;
+	private String startDate;
+	private String endDate;
 	private int durationDays;
 	private String userId;
 	private String organizationId;
