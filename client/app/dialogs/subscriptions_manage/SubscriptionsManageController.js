33c33500509b19820abcdacb732c13b2db583f64
Betty <betty.spurgeon@wasdi.cloud>
2023-04-07 17:48:11 +0200

    manage subscriptions dialog with layout and subscriptions displayed

--- client/app/dialogs/subscriptions_manage/SubscriptionsManageController.js
+++ client/app/dialogs/subscriptions_manage/SubscriptionsManageController.js
@@ -17,9 +17,63 @@ SubscriptionsManageController = (function () {
         this.m_oSubscriptionService = oSubscriptionService;
         this.m_oOrganizationService = oOrganizationService;
 
+        this.m_bLoadingOrganizations = true;
+        this.m_bLoadingSubscriptions = true;
+        this.m_bLoadingProjects = true;
+        this.m_bIsLoading = false;
+
+
+        this.m_aoSubscriptions = [];
+        this.m_bLoadingSubscriptions = true;
+
+        this.initializeSubscriptionsInfo();
+
         $scope.close = function (result) {
             oClose(result, 500);
         }
+    }
+
+    SubscriptionsManageController.prototype.initializeSubscriptionsInfo = function () {
+        let oController = this;
+
+        this.m_oSubscriptionService.getSubscriptionsListByUser().then(function (response) {
+            if (!utilsIsObjectNullOrUndefined(response)
+                && !utilsIsObjectNullOrUndefined(response.data) && response.status === 200) {
+                oController.m_aoSubscriptions = response.data;
+            } else {
+                utilsVexDialogAlertTop(
+                    "GURU MEDITATION<br>ERROR IN GETTING THE LIST OF SUBSCRIPTIONS"
+                );
+            }
+            oController.m_bLoadingSubscriptions = false;
+            console.log(oController.m_aoSubscriptions)
+
+            return true;
+        }, function (error) {
+            let sErrorMessage = "GURU MEDITATION<br>ERROR IN GETTING THE LIST OF SUBSCRIPTIONS";
+
+            if (!utilsIsObjectNullOrUndefined(error.data) && !utilsIsStrNullOrEmpty(error.data.message)) {
+                sErrorMessage += "<br><br>" + oController.m_oTranslate.instant(error.data.message);
+            }
+
+            utilsVexDialogAlertTop(sErrorMessage);
+        });
+    }
+
+    SubscriptionsManageController.prototype.showUsersBySubscription = function (sSubscriptionId) {
+
+    }
+
+    SubscriptionsManageController.prototype.showProjectsBySubscription = function (sSubscriptionId, sSubscriptionName) {
+
+    }
+
+    SubscriptionsManageController.prototype.showSubscriptionEditForm = function (sSubscriptionId, bIsOwner) {
+
+    }
+
+    SubscriptionsManageController.prototype.deleteSubscription = function (sSubscriptionId) {
+        
     }
 
     SubscriptionsManageController.$inject = [
@@ -30,7 +84,7 @@ SubscriptionsManageController = (function () {
         'SubscriptionService',
         'OrganizationService',
         '$translate'
-    ]; 
+    ];
     return SubscriptionsManageController
-})(); 
+})();
 window.SubscriptionsManageController = SubscriptionsManageController;
