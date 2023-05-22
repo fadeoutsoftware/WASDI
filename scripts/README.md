20706288af6dc3485faf224c41a1cafb695328bb
Bertrand ROBERT <bertrand.robert@wasdi.cloud>
2023-05-22 11:43:34 +0200

    Add a new script and update the help message

--- scripts/README.md
+++ scripts/README.md
@@ -1,3 +1,13 @@
+# cleanSnapDirectories.sh
+
+## Introduction
+
+This script is used by WASDI:
+  - to clean SNAP directories
+  - recreate them if needed
+
+This script is periodically executed using a crontab.
+
 # renderJinjaTemplate.py
 
 ## Introduction
---
+++ scripts/cleanSnapDirectories.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# find and delete snap temporary files accessed last time more than 5 minutes ago
+if [[ -d "{{ sWasdiDataTemporarySnapTomcatRootDirectoryPath }}" ]]
+then
+    find {{ sWasdiDataTemporarySnapTomcatRootDirectoryPath }}/ -amin +5 -exec rm --recursive {} \; 2>/dev/null
+fi
+
+# then immediately repair dirs...
+mkdir --parents {{ sWasdiDataTemporarySnapTomcatRootDirectoryPath }}
+chown --recursive {{ sWasdiDataTemporarySnapTomcatRootDirectoryOwner }}:{{ sWasdiDataTemporarySnapTomcatRootDirectoryGroup }} {{ sWasdiDataTemporarySnapTomcatRootDirectoryPath }}
+chmod {{ sWasdiDataTemporarySnapCacheRootDirectoryMode }} {{ sWasdiDataTemporarySnapTomcatRootDirectoryPath }}
+
+# check link from /tmp exists and recreate if necessary
+if [[ ! -h "/tmp/snap-tomcat" ]]
+then
+    ln --symbolic {{ sWasdiDataTemporarySnapTomcatRootDirectoryPath }} /tmp/snap-tomcat
+fi
+
+# change owner and group of the symbolic link
+chown --no-dereference {{ sWasdiDataTemporarySnapTomcatRootDirectoryOwner }}:{{ sWasdiDataTemporarySnapTomcatRootDirectoryGroup }} /tmp/snap-tomcat
+
+# find and delete snap cache files accessed last time more than 5 minutes ago
+if [[ -d "{{ sWasdiDataTemporarySnapCacheRootDirectoryPath }}" ]]
+then
+    find {{ sWasdiDataTemporarySnapCacheRootDirectoryPath }}/ -amin +5 -exec rm --recursive {} \; 2>/dev/null
+fi
+
+# then immediately repair dirs...
+mkdir --parents {{ sWasdiDataTemporarySnapCacheRootDirectoryPath }}
+chown --recursive {{ sWasdiDataTemporarySnapCacheRootDirectoryOwner }}:{{ sWasdiDataTemporarySnapCacheRootDirectoryGroup }} {{ sWasdiDataTemporarySnapCacheRootDirectoryPath }}
+chmod {{ sWasdiDataTemporarySnapCacheRootDirectoryMode }} {{ sWasdiDataTemporarySnapCacheRootDirectoryPath }}
+
+# check link from /tmp/snap-cache exists and recreate if necessary
+if [[ ! -h "/tmp/snap-cache" ]]
+then
+    ln --symbolic {{ sWasdiDataTemporarySnapCacheRootDirectoryPath }} /tmp/snap-cache
+fi
+
+# change owner and group of the symbolic link
+chown --no-dereference {{ sWasdiDataTemporarySnapCacheRootDirectoryOwner }}:{{ sWasdiDataTemporarySnapCacheRootDirectoryGroup }} /tmp/snap-cache
+
+# check link from {{ sWasdiSystemUserHome }}/.snap/var/cache exists and recreate if necessary
+if [[ ! -h "{{ sWasdiSystemUserHome }}/.snap/var/cache" ]]
+then
+    ln --symbolic {{ sWasdiDataTemporarySnapCacheRootDirectoryPath }} {{ sWasdiSystemUserHome }}/.snap/var/cache
+fi
+
+# change owner and group of the symbolic link
+chown --no-dereference {{ sWasdiDataTemporarySnapCacheRootDirectoryOwner }}:{{ sWasdiDataTemporarySnapCacheRootDirectoryGroup }} {{ sWasdiSystemUserHome }}/.snap/var/cache
