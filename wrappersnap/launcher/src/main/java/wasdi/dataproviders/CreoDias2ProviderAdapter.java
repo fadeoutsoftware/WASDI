0b13476e2882fa0a08ee1f1b10980ce13d28312b
leonevalentina <valentina.leone@wasdi.cloud>
2023-07-13 15:02:26 +0200

    implement auth token retrieval for downloading the file

--- wrappersnap/launcher/src/main/java/wasdi/dataproviders/CreoDias2ProviderAdapter.java
+++ wrappersnap/launcher/src/main/java/wasdi/dataproviders/CreoDias2ProviderAdapter.java
@@ -4,21 +4,31 @@ import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.security.SecureRandom;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.net.io.Util;
+import org.checkerframework.checker.units.qual.A;
 import org.json.JSONObject;
 
 import wasdi.shared.business.ProcessWorkspace;
+import wasdi.shared.utils.HttpUtils;
+import wasdi.shared.utils.Utils;
+import wasdi.shared.utils.log.LoggerWrapper;
 import wasdi.shared.utils.log.WasdiLog;
+import wasdi.shared.viewmodels.HttpCallResponse;
 
 public class CreoDias2ProviderAdapter extends ProviderAdapter {
 	
 	private static final String SAUTHENTICATION_URL = "https://identity.cloudferro.com/auth/realms/wekeo-elasticity/protocol/openid-connect/token";
+	private static final String SDOWNLOAD_URL = "https://zipper.dataspace.copernicus.eu/odata/v1/Products(702b4faf-16d5-4450-9f61-4d0a13f96794)/$value?token=";
 	
 	public CreoDias2ProviderAdapter() {
 		super();
 		m_sDataProviderCode = "CREODIAS2";
 	}
+	
 
 	@Override
 	protected void internalReadConfig() {
@@ -28,8 +38,8 @@ public class CreoDias2ProviderAdapter extends ProviderAdapter {
 
 	@Override
 	public long getDownloadFileSize(String sFileURL) throws Exception {
-		// TODO Auto-generated method stub
 		// receives the file URI and must return the size of the file. Useful to give progress to the user
+		// TODO: are we are talking about the overall file size? or just the size has been downloaded until a certain point?
 		return 0;
 	}
 
@@ -37,12 +47,63 @@ public class CreoDias2ProviderAdapter extends ProviderAdapter {
 	public String executeDownloadFile(String sFileURL, String sDownloadUser, String sDownloadPassword,
 			String sSaveDirOnServer, ProcessWorkspace oProcessWorkspace, int iMaxRetry) throws Exception {
 		// TODO Auto-generated method stub
-		// main method. sFileUrl is the url received by the lined data provider. sSaveDirOnSerer is the local folder. 
+		// main method. sFileUrl is the url received by the lined data provider. sSaveDirOnServer is the local folder. 
 		// must return the valid file full path or "" if the download was not possible. 
 		
+		String sAccessToken = getAuthenticationToken(sDownloadUser, sDownloadPassword);
 		
+		if (Utils.isNullOrEmpty(sAccessToken)) {
+			WasdiLog.debugLog("CreoDias2ProviderAdaper.executeDownloadFile. Error retrieving the access token. Impossible to continue.");
+			return "";
+		}
+		
+		// with the auth token, we can send the download request
+		String sDownloadUrl = SDOWNLOAD_URL + sAccessToken;
+		
+		// TODO: understand if I can should also pass the name of the file 
+		String sDownloadedFilePath = downloadViaHttp(sFileURL, null, sSaveDirOnServer);
+		if(Utils.isNullOrEmpty(sDownloadedFilePath)) {
+			//try again
+//			++iAttempt;
+//			long lRandomWaitSeconds = new SecureRandom().longs(lLo, lUp).findFirst().getAsLong();
+			//prepare to wait longer next time
+//			lLo = lRandomWaitSeconds;
+//			lUp += lWaitStep;
+//			WasdiLog.warnLog("CreoDias2ProviderAdapter.executeDownloadFile. Download failed. Trying again after waoting  " + lRandomWaitSeconds +" seconds...");
+//			TimeUnit.SECONDS.sleep(lRandomWaitSeconds);
+		} else {
+			//we're done
+			WasdiLog.debugLog("CreoDias2ProviderAdapter.executeDownloadFile. Download completed: " + sDownloadedFilePath);
+//			break;
+		}
 		
 		return null;
+	}
+	
+	private String getAuthenticationToken(String sUsername, String sPassword) {
+		String sPayload = "client_id=CLOUDFERRO_PUBLIC&password=" + sPassword + "&username=" + sUsername + "&grant_type=password";
+		HttpCallResponse oResponse = null;
+		try {
+			oResponse = HttpUtils.httpPost(SAUTHENTICATION_URL, sPayload);
+			int iResponseCode = oResponse.getResponseCode();
+			if (oResponse == null ||  oResponse.getResponseCode() < 200 || oResponse.getResponseCode() > 299) {
+				WasdiLog.debugLog("CreoDias2ProviderAdaper.getAuthenticationToken. Error code " + iResponseCode + " while trying to retrieve the auth token.");
+				return "";
+			} 
+		} catch (Exception oEx) {
+			WasdiLog.debugLog("CreoDias2ProviderAdaper.getAuthenticationToken. Error while trying to retrieve the authentication token." + oEx.getMessage());
+			return "";
+		}
+		
+		JSONObject oResponseBody = new JSONObject(oResponse.getResponseBody());
+		String sAccessToken = oResponseBody.optString("access_token", "");
+		
+		if (Utils.isNullOrEmpty(sAccessToken)) {
+			WasdiLog.debugLog("CreoDias2ProviderAdaper.executeDownloadFile. No access token found.");
+			return "";
+		}
+		
+		return sAccessToken;
 	}
 
 	@Override
@@ -60,17 +121,18 @@ public class CreoDias2ProviderAdapter extends ProviderAdapter {
 	}
 	
 	public static void main(String[]args) throws Exception {
-		URL oURL = new URL("https://identity.cloudferro.com/auth/realms/wekeo-elasticity/protocol/openid-connect/token");
+		String sUrl = "https://identity.cloudferro.com/auth/realms/wekeo-elasticity/protocol/openid-connect/token";
+		URL oURL = new URL(sUrl);
 
 		HttpURLConnection oConnection = (HttpURLConnection) oURL.openConnection();
 		oConnection.setRequestMethod("POST");
 		oConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
 		oConnection.setDoOutput(true);
-		String sDownloadPassword = "**********";
-		String sDownloadUser = "**********";
-		String totp = "140043";
+		String sDownloadPassword = "******";
+		String sDownloadUser = "******";
+		String sPayload = "client_id=CLOUDFERRO_PUBLIC&password=" + sDownloadPassword + "&username=" + sDownloadUser + "&grant_type=password";
 		oConnection.getOutputStream().write(("client_id=CLOUDFERRO_PUBLIC&password=" + sDownloadPassword + "&username=" + sDownloadUser + "&grant_type=password").getBytes());
-		int iStatus = oConnection.getResponseCode();
+/*		int iStatus = oConnection.getResponseCode();
 		WasdiLog.debugLog("CREODIASProviderAdapter.obtainKeycloakToken: Response status: " + iStatus);
 		if( iStatus == 200) {
 			InputStream oInputStream = oConnection.getInputStream();
@@ -91,6 +153,20 @@ public class CreoDias2ProviderAdapter extends ProviderAdapter {
 			WasdiLog.debugLog("CREODIASProviderAdapter.obtainKeycloakToken:" + sMessage);
 
 		}
+		*/
+		HttpCallResponse oResponse = HttpUtils.httpPost(sUrl, sPayload);
+		System.out.println(oResponse.getResponseBody());
+		JSONObject oResponseBody = new JSONObject(oResponse.getResponseBody());
+		String sAccessToken = oResponseBody.optString("access_token");
+		
+		String otherUrl = "https://zipper.dataspace.copernicus.eu/odata/v1/Products(702b4faf-16d5-4450-9f61-4d0a13f96794)/$value?token=" + sAccessToken;
+		
+		HttpCallResponse oResponse2 = HttpUtils.httpGet(otherUrl);
+		
+		System.out.println(oResponse2);
+		
+		
+		
 
 
 	}
