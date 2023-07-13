4068a1ddea5c3d873dc0b1999eeae4a04f2042af
leonevalentina <valentina.leone@wasdi.cloud>
2023-07-13 16:44:44 +0200

    add code to actually perform the download operation and implement method to get the file size

--- wrappersnap/launcher/src/main/java/wasdi/dataproviders/CreoDias2ProviderAdapter.java
+++ wrappersnap/launcher/src/main/java/wasdi/dataproviders/CreoDias2ProviderAdapter.java
@@ -1,28 +1,33 @@
 package wasdi.dataproviders;
 
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.security.SecureRandom;
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.commons.net.io.Util;
-import org.checkerframework.checker.units.qual.A;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import wasdi.shared.business.ProcessWorkspace;
 import wasdi.shared.utils.HttpUtils;
 import wasdi.shared.utils.Utils;
-import wasdi.shared.utils.log.LoggerWrapper;
 import wasdi.shared.utils.log.WasdiLog;
 import wasdi.shared.viewmodels.HttpCallResponse;
 
 public class CreoDias2ProviderAdapter extends ProviderAdapter {
 	
+	private static final String SODATA_ATTRIBUTES = "Attributes";
+	private static final String SODATA_NAME = "Name";
+	private static final String SODATA_VALUE = "Value";
+	private static final String SODATA_SIZE = "ContentLength";
+	
 	private static final String SAUTHENTICATION_URL = "https://identity.cloudferro.com/auth/realms/wekeo-elasticity/protocol/openid-connect/token";
 	private static final String SDOWNLOAD_URL = "https://zipper.dataspace.copernicus.eu/odata/v1/Products(702b4faf-16d5-4450-9f61-4d0a13f96794)/$value?token=";
+	
+	private static final String SFILE_URL_START = "https://datahub.creodias.eu/odata/v1/Products(";
+	private static final String SFILE_URL_END = ")/$value";
+	
+	private static final String SCREODIAS_BASE_URL = "https://datahub.creodias.eu/odata/v1/Products?";
 	
 	public CreoDias2ProviderAdapter() {
 		super();
@@ -40,44 +45,101 @@ public class CreoDias2ProviderAdapter extends ProviderAdapter {
 	public long getDownloadFileSize(String sFileURL) throws Exception {
 		// receives the file URI and must return the size of the file. Useful to give progress to the user
 		// TODO: are we are talking about the overall file size? or just the size has been downloaded until a certain point?
-		return 0;
+		
+		WasdiLog.debugLog("CreoDias2ProviderAdaper.getDownloadFileSize. Retrieving information for product: " + sFileURL);
+		
+		// create a creodias query like: https://datahub.creodias.eu/odata/v1/Products?$filter=Id eq '79d7807b-66e5-41d2-a586-1628eb82c3e0'&$expand=Attributes
+		String sProductId = getProductIdFromURL(sFileURL);
+		String sUrl = SCREODIAS_BASE_URL + "$filter=Id eq '" + sProductId + "'&$expand=Attributes";
+		HttpCallResponse sResponse = null;
+		
+		try {
+			sResponse = HttpUtils.httpGet(sUrl);
+			if (sResponse == null || sResponse.getResponseCode() < 200 || sResponse.getResponseCode() > 299 || Utils.isNullOrEmpty(sResponse.getResponseBody())) {
+				WasdiLog.debugLog("CreoDias2ProviderAdaper.getDownloadFileSize. Error retrieving the information about the product from the provider. " + sUrl);
+				return -1L;
+			}
+		} catch (Exception oEx) {
+			WasdiLog.debugLog("CreoDias2ProviderAdaper.getDownloadFileSize. An exception occurred while retrieving the product information from the provider. " + oEx.getMessage());
+			return -1L;
+		}
+		
+		JSONObject oJsonBody = new JSONObject(sResponse.getResponseBody());
+		JSONArray aoJsonAttributes = oJsonBody.optJSONArray(SODATA_ATTRIBUTES);
+		
+		if (aoJsonAttributes == null) 
+			WasdiLog.debugLog("CreoDias2ProviderAdaper.getDownloadFileSize. Array of attributes not found for key " + SODATA_ATTRIBUTES);
+		
+		String sFileSize = getAttribute(aoJsonAttributes, SODATA_SIZE);
+		
+		if (Utils.isNullOrEmpty(sFileSize)) {
+			WasdiLog.debugLog("CreoDias2ProviderAdaper.getDownloadFileSize. Value not found for attribute name: " + SODATA_SIZE);
+			return -1L;
+		}
+		
+		return Long.parseLong(sFileSize);
 	}
+	
+	
+	private String getAttribute(JSONArray aoAttributes, String sAttributeName) {
+		for (Object oAtt : aoAttributes) {
+			JSONObject oJsonAtt = (JSONObject) oAtt;
+			if (oJsonAtt.get(SODATA_NAME).equals(sAttributeName))
+				return oJsonAtt.optString(SODATA_VALUE);
+		}
+		return "";
+	}
+	
+	
+	private String getProductIdFromURL(String sFileUrl) {
+		// url in input is something like: https://datahub.creodias.eu/odata/v1/Products(a6212de3-f2e4-58c2-840b-7f42c3c8c612)/$value
+		return sFileUrl.replace(SFILE_URL_START, "").replace(SFILE_URL_END, "");
+	}
+	
 
 	@Override
 	public String executeDownloadFile(String sFileURL, String sDownloadUser, String sDownloadPassword,
 			String sSaveDirOnServer, ProcessWorkspace oProcessWorkspace, int iMaxRetry) throws Exception {
-		// TODO Auto-generated method stub
 		// main method. sFileUrl is the url received by the lined data provider. sSaveDirOnServer is the local folder. 
 		// must return the valid file full path or "" if the download was not possible. 
 		
-		String sAccessToken = getAuthenticationToken(sDownloadUser, sDownloadPassword);
+		int iAttemptCount = 0;
+		int iWaitDelta = 100;
+		long lLowerWatingTime = 1L;
+		long lUpperWatingTime = 10L;
 		
-		if (Utils.isNullOrEmpty(sAccessToken)) {
-			WasdiLog.debugLog("CreoDias2ProviderAdaper.executeDownloadFile. Error retrieving the access token. Impossible to continue.");
-			return "";
+		while (iAttemptCount < iMaxRetry) {
+			String sAccessToken = getAuthenticationToken(sDownloadUser, sDownloadPassword);
+			
+			if (Utils.isNullOrEmpty(sAccessToken)) {
+				WasdiLog.debugLog("CreoDias2ProviderAdaper.executeDownloadFile. Error retrieving the access token. Impossible to continue.");
+				// TODO: better to return an empty string or null?
+				return "";
+			}
+			
+			// with the auth token, we can send the download request
+			String sDownloadUrl = SDOWNLOAD_URL + sAccessToken;
+			
+			// TODO: understand if I should also pass the name of the file 
+			String sDownloadedFilePath = downloadViaHttp(sDownloadUrl, null, sSaveDirOnServer);
+			
+			if(Utils.isNullOrEmpty(sDownloadedFilePath)) {
+				// we will try again
+				iAttemptCount++;
+				long lRandomWaitSeconds = new SecureRandom().longs(lLowerWatingTime, lUpperWatingTime).findFirst().getAsLong();
+				//prepare to wait longer next time
+				lLowerWatingTime = lRandomWaitSeconds;
+				lUpperWatingTime += iWaitDelta;
+				WasdiLog.warnLog("CreoDias2ProviderAdapter.executeDownloadFile. Download failed. Trying again after waiting  " + lRandomWaitSeconds +" seconds.");
+				TimeUnit.SECONDS.sleep(lRandomWaitSeconds);
+			} else {
+				// download completed
+				WasdiLog.debugLog("CreoDias2ProviderAdapter.executeDownloadFile. Download completed: " + sDownloadedFilePath);
+				return sDownloadedFilePath;
+			}
 		}
-		
-		// with the auth token, we can send the download request
-		String sDownloadUrl = SDOWNLOAD_URL + sAccessToken;
-		
-		// TODO: understand if I can should also pass the name of the file 
-		String sDownloadedFilePath = downloadViaHttp(sFileURL, null, sSaveDirOnServer);
-		if(Utils.isNullOrEmpty(sDownloadedFilePath)) {
-			//try again
-//			++iAttempt;
-//			long lRandomWaitSeconds = new SecureRandom().longs(lLo, lUp).findFirst().getAsLong();
-			//prepare to wait longer next time
-//			lLo = lRandomWaitSeconds;
-//			lUp += lWaitStep;
-//			WasdiLog.warnLog("CreoDias2ProviderAdapter.executeDownloadFile. Download failed. Trying again after waoting  " + lRandomWaitSeconds +" seconds...");
-//			TimeUnit.SECONDS.sleep(lRandomWaitSeconds);
-		} else {
-			//we're done
-			WasdiLog.debugLog("CreoDias2ProviderAdapter.executeDownloadFile. Download completed: " + sDownloadedFilePath);
-//			break;
-		}
-		
-		return null;
+		// TODO: better to return an empty string or null?
+		return "";
 	}
 	
 	private String getAuthenticationToken(String sUsername, String sPassword) {
@@ -87,7 +149,7 @@ public class CreoDias2ProviderAdapter extends ProviderAdapter {
 			oResponse = HttpUtils.httpPost(SAUTHENTICATION_URL, sPayload);
 			int iResponseCode = oResponse.getResponseCode();
 			if (oResponse == null ||  oResponse.getResponseCode() < 200 || oResponse.getResponseCode() > 299) {
-				WasdiLog.debugLog("CreoDias2ProviderAdaper.getAuthenticationToken. Error code " + iResponseCode + " while trying to retrieve the auth token.");
+				WasdiLog.debugLog("CreoDias2ProviderAdaper.getAuthenticationToken. Error code while trying to retrieve the auth token.");
 				return "";
 			} 
 		} catch (Exception oEx) {
@@ -131,29 +193,7 @@ public class CreoDias2ProviderAdapter extends ProviderAdapter {
 		String sDownloadPassword = "******";
 		String sDownloadUser = "******";
 		String sPayload = "client_id=CLOUDFERRO_PUBLIC&password=" + sDownloadPassword + "&username=" + sDownloadUser + "&grant_type=password";
-		oConnection.getOutputStream().write(("client_id=CLOUDFERRO_PUBLIC&password=" + sDownloadPassword + "&username=" + sDownloadUser + "&grant_type=password").getBytes());
-/*		int iStatus = oConnection.getResponseCode();
-		WasdiLog.debugLog("CREODIASProviderAdapter.obtainKeycloakToken: Response status: " + iStatus);
-		if( iStatus == 200) {
-			InputStream oInputStream = oConnection.getInputStream();
-			ByteArrayOutputStream oBytearrayOutputStream = new ByteArrayOutputStream();
-			if(null!=oInputStream) {
-				Util.copyStream(oInputStream, oBytearrayOutputStream);
-				String sResult = oBytearrayOutputStream.toString();
-				//WasdiLog.debugLog("CREODIASProviderAdapter.obtainKeycloakToken: json: " + sResult);
-				JSONObject oJson = new JSONObject(sResult);
-				String sToken = oJson.optString("access_token", null);
-				System.out.println(sToken);
-			}
-		} else {
-			ByteArrayOutputStream oBytearrayOutputStream = new ByteArrayOutputStream();
-			InputStream oErrorStream = oConnection.getErrorStream();
-			Util.copyStream(oErrorStream, oBytearrayOutputStream);
-			String sMessage = oBytearrayOutputStream.toString();
-			WasdiLog.debugLog("CREODIASProviderAdapter.obtainKeycloakToken:" + sMessage);
 
-		}
-		*/
 		HttpCallResponse oResponse = HttpUtils.httpPost(sUrl, sPayload);
 		System.out.println(oResponse.getResponseBody());
 		JSONObject oResponseBody = new JSONObject(oResponse.getResponseBody());
@@ -164,9 +204,6 @@ public class CreoDias2ProviderAdapter extends ProviderAdapter {
 		HttpCallResponse oResponse2 = HttpUtils.httpGet(otherUrl);
 		
 		System.out.println(oResponse2);
-		
-		
-		
 
 
 	}
