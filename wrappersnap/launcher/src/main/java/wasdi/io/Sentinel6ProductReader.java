62187c083e08464845918f59ebc331415f2b4169
leonevalentina <valentina.leone@wasdi.cloud>
2023-08-22 17:53:52 +0200

    Adjust product reader for Sentinel-6 products for make it read NetCDF files using the proper library

--- wrappersnap/launcher/src/main/java/wasdi/io/Sentinel6ProductReader.java
+++ wrappersnap/launcher/src/main/java/wasdi/io/Sentinel6ProductReader.java
@@ -1,30 +1,40 @@
 package wasdi.io;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.esa.snap.core.datamodel.Product;
 
+import com.google.common.io.Files;
+
+import ucar.nc2.Group;
+import ucar.nc2.NetcdfFile;
+import ucar.nc2.NetcdfFiles;
+import ucar.nc2.Variable;
 import wasdi.shared.utils.Utils;
 import wasdi.shared.utils.ZipFileUtils;
 import wasdi.shared.utils.log.WasdiLog;
+import wasdi.shared.viewmodels.products.BandViewModel;
+import wasdi.shared.viewmodels.products.GeorefProductViewModel;
+import wasdi.shared.viewmodels.products.NodeGroupViewModel;
+import wasdi.shared.viewmodels.products.ProductViewModel;
 
 public class Sentinel6ProductReader extends CmNcProductReader {
 
 	/**
-	 * @param oProductFile the envisat (zip) file to be read
+	 * @param oProductFile the Seninel 6 (zip) file to be read
 	 */
 	public Sentinel6ProductReader(File oProductFile) {
 		super(oProductFile);
-		if (oProductFile.getName().endsWith(".zip")) {
-			String sProductFilePath = adjustFileAfterDownload(oProductFile.getAbsolutePath(), oProductFile.getName());
-			m_oProductFile = new File(sProductFilePath); 
-		}
 	}
 
 	@Override
 	public String adjustFileAfterDownload(String sDownloadedFileFullPath, String sFileNameFromProvider) {
+		
 		try {
 			if (Utils.isNullOrEmpty(sDownloadedFileFullPath)) {
 				WasdiLog.errorLog(
@@ -32,162 +42,145 @@ public class Sentinel6ProductReader extends CmNcProductReader {
 				return null;
 			}
 			if (Utils.isNullOrEmpty(sFileNameFromProvider)) {
-				WasdiLog.errorLog(
-						"Sentinel6ProductReader.adjustFileAfterDownload. sFileNameFromProvider null or empty, aborting");
+				WasdiLog.errorLog("Sentinel6ProductReader.adjustFileAfterDownload. sFileNameFromProvider null or empty, aborting");
 				return null;
 			}
 			if (!sFileNameFromProvider.toUpperCase().startsWith("S6")
 					|| !sFileNameFromProvider.toLowerCase().endsWith(".zip")) {
-				WasdiLog.errorLog("Sentinel6ProductReader.adjustFileAfterDownload: " + sFileNameFromProvider
-						+ " does not look like a Sentinel-6 file name");
+				WasdiLog.errorLog("Sentinel6ProductReader.adjustFileAfterDownload: " + sFileNameFromProvider + " does not look like a Sentinel-6 file name");
 				return null;
 			}
 		} catch (Exception oE) {
-			WasdiLog.errorLog("Sentinel6ProductReader.adjustFileAfterDownload. arguments checking failed due to: "
-					+ oE.getMessage() + ", aborting");
+			WasdiLog.errorLog("Sentinel6ProductReader.adjustFileAfterDownload. arguments checking failed due to: " + oE.getMessage() + ", aborting");
 			return null;
 		}
 		
 
 		try {
+			// path and object of the workspace directory
 			String sDownloadPath = new File(sDownloadedFileFullPath).getParentFile().getPath();
+			File oWorkspaceDir = new File(sDownloadPath);
 			
-			String sNewFolderName = sFileNameFromProvider.substring(0,
-					sFileNameFromProvider.toLowerCase().lastIndexOf(".zip"));
-			
+			// name, path and object of the unzipped folder
+			String sNewFolderName = sFileNameFromProvider.substring(0, sFileNameFromProvider.toLowerCase().lastIndexOf(".zip"));
 			String sNewFolderPath = sDownloadPath + File.separator + sNewFolderName;
+			File oTargetDirectory = new File(sNewFolderPath);
 			
-			List<File> asFilesInDownloadFolder = Arrays.asList(new File(sDownloadPath).listFiles());
-			if (asFilesInDownloadFolder.stream().anyMatch(oFile -> oFile.isDirectory() && oFile.getName().equals(sNewFolderName))) {
-				WasdiLog.debugLog("Sentinel6ProductReader.adjustFileAfterDownload. File already unzipped in: " + sNewFolderPath);
-				return m_oProductFile.getAbsolutePath();
+			WasdiLog.debugLog("Sentinel6ProductReader.adjustFileAfterDownload. File is a Sentinel-6 image, start unzip of file: " + sDownloadedFileFullPath);
+			File oSourceFile = new File(sDownloadedFileFullPath);
+			ZipFileUtils.cleanUnzipFile(oSourceFile, oTargetDirectory);
+			
+			
+			boolean bIsUnzippedDirPresent = Arrays.asList(oWorkspaceDir.listFiles()).stream().anyMatch(oFile -> oFile.isDirectory() && oFile.getName().equals(sNewFolderName));
+			
+			if (!bIsUnzippedDirPresent) {
+				WasdiLog.errorLog("Sentinel6ProductReader.adjustFileAfterDownload: the unzipped folder is not present");
+				return null;
+			}
 				
-			} else {
-				WasdiLog.debugLog(
-						"Sentinel6ProductReader.adjustFileAfterDownload. File is a Sentinel-6 image, start unzip of file: "
-								+ sDownloadedFileFullPath);
-				ZipFileUtils oZipExtractor = new ZipFileUtils();
-
-				oZipExtractor.unzip(sDownloadPath + File.separator + sFileNameFromProvider, sDownloadPath);
-				// remove .zip
-				deleteDownloadedZipFile(sDownloadedFileFullPath);
+			File oNcFile = null;
+			for (File oFile : oTargetDirectory.listFiles()) {
+				if (oFile.isFile() && oFile.getName().toLowerCase().endsWith(".nc")) {
+					oNcFile = oFile;
+					break;		// TODO: so far we get the fist NetCDF file found in the folder. Later, we will need to understand how to select the correct one
+				}
+			}
+			
+			if (oNcFile == null) {
+				WasdiLog.errorLog("Sentinel6ProductReader.adjustFileAfterDownload. No NetCDF file found in the unzipped folder");
+				return null;
+			}
 				
-				File oNewFolder = new File(sNewFolderPath);
-				for (File oFile : oNewFolder.listFiles()) {
-					if (oFile.isFile() && oFile.getName().endsWith(".nc")) {
-						return oFile.getAbsolutePath();
-					}
-				}		
+			WasdiLog.debugLog("Sentinel6ProductReader.adjustFileAfterDownload. Found NetCDF file: " + oNcFile.getAbsolutePath() );
+			File oNcTargetFile = new File(sDownloadPath + File.separator + oNcFile.getName());
+			Files.move(oNcFile, oNcTargetFile);
+			final String sNcFileName = oNcFile.getName();
+			boolean bIsFileMoved = Arrays.asList(oWorkspaceDir.listFiles()).stream().anyMatch(oFile -> oFile.isFile() && oFile.getName().equals(sNcFileName));
+			if (bIsFileMoved) {
+				WasdiLog.debugLog("Sentinel6ProductReader.adjustFileAfterDownload. File correctly moved in workspace folder: " + oNcTargetFile.getName());
+				m_oProductFile = oNcTargetFile;
+				return oNcTargetFile.getAbsolutePath();
+			} else  {
+				WasdiLog.errorLog("Sentinel6ProductReader.adjustFileAfterDownload. File not moved to the workspace folder.");
+				return null;
 			}
 
 		} catch (Exception oEx) {
 			WasdiLog.errorLog("Sentinel6ProductReader.adjustFileAfterDownload. error ", oEx);
 		}
 
-		return sDownloadedFileFullPath;
+		return null;
 	}
+	
+	@Override
+	public ProductViewModel getProductViewModel() {
 
-	/**
-	 * @param sFileNameFromProvider
-	 * @param sDownloadPath
-	 */
-	private void deleteDownloadedZipFile(String sDownloadedFileFullPath) {
+		if (m_oProductFile == null) return null;
+
+    	// Create the return value
+    	GeorefProductViewModel oRetViewModel = null;
+
 		try {
-			File oZipFile = new File(sDownloadedFileFullPath);
-			if (!oZipFile.delete()) {
-				WasdiLog.errorLog("Sentinel6ProductReader.deleteZipFile. cannot delete zip file");
-			} else {
-				WasdiLog.debugLog("Sentinel6ProductReader.deleteZipFile. file zip successfully deleted");
-			}
-		} catch (Exception oE) {
-			WasdiLog.errorLog("Sentinel6ProductReader.deleteZipFile. exception while trying to delete zip file: " + oE);
+			NetcdfFile oFile = NetcdfFiles.open(m_oProductFile.getAbsolutePath());
+
+	    	// Create the Product View Model
+	    	oRetViewModel = new GeorefProductViewModel();
+
+        	// Set name values
+        	oRetViewModel.setFileName(m_oProductFile.getName());
+        	oRetViewModel.setName(Utils.getFileNameWithoutLastExtension(m_oProductFile.getName()));
+        	oRetViewModel.setProductFriendlyName(oRetViewModel.getName());
+
+	    	NodeGroupViewModel oNodeGroupViewModel = new NodeGroupViewModel();
+        	oNodeGroupViewModel.setNodeName("Bands");
+
+    		Group oRootGroup = oFile.getRootGroup();
+    		List<Group> aoRootGroupGroups = oRootGroup.getGroups();
+
+        	List<BandViewModel> aoBands = new ArrayList<>();
+
+    		for (Group oGroup : aoRootGroupGroups) {
+
+    			List<Variable> aoVariableList = oGroup.getVariables();
+    			for (Variable v : aoVariableList) {
+    				String variableShortName = v.getShortName();
+    			        	
+		        	// Create the single band representing the shape
+		        	BandViewModel oBandViewModel = new BandViewModel();
+		        	oBandViewModel.setPublished(false);
+		        	oBandViewModel.setGeoserverBoundingBox("");
+		        	oBandViewModel.setPublished(false);
+		        	oBandViewModel.setName(variableShortName);
+			        aoBands.add(oBandViewModel);
+    			}
+    		}	
+
+        	oNodeGroupViewModel.setBands(aoBands);
+	    	oRetViewModel.setBandsGroups(oNodeGroupViewModel);
+		} catch (Exception e) {
+    		WasdiLog.debugLog("Sentinel5ProductReader.getProductViewModel: exception reading the shape file: " + e.toString());
 		}
+		
+    	return oRetViewModel;
 	}
 
-	/**
-	 * Get the SNAP product or null if this is not a product readable by Snap
-	 * 
-	 * @return
-	 */
+	
+
+
 	@Override
 	public Product getSnapProduct() {
-		
-		
-		if (m_bSnapReadAlreadyDone) {
-			return m_oProduct;
-		}
-
-		// prepare path
-		String sBase = null;
-		File oOriginalProductFile = m_oProductFile;
-		try {
-			sBase = m_oProductFile.getAbsolutePath();
-
-		} catch (Exception oE) {
-			WasdiLog.errorLog("Sentinel6ProductReader.getSnapProduct. setting paths failed due to: " + oE);
-			return null;
-		}
-
-		if (m_oProductFile.isDirectory()) {
-			File[] aoFiles = m_oProductFile.listFiles();
-			for (int i = 0; i < aoFiles.length; i++) {
-				File oCurrentFile = aoFiles[i];
-				if (oCurrentFile.isFile() && oCurrentFile.getName().endsWith(".nc")) {
-					m_oProductFile = oCurrentFile;
-					super.getSnapProduct();
-					break;
-				}
-			}
-		} else {
-			WasdiLog.debugLog("Sentinel6ProductReader.getSnapProduct. " + sBase + "is not a folder");
-			return null;
-		}
-
-		// reset the File pointer
-		m_oProductFile = oOriginalProductFile;
-
-		return m_oProduct;
+    	WasdiLog.debugLog("Sentinel6ProductReader.readSnapProduct. We do not want SNAP to read S6, return null ");
+    	return null;   
+	
 	}
+	
 
-	/**
-	 * @param sBase     base folder
-	 * @param sFileName name of the file in the base folder from which the band
-	 *                  should be read
-	 */
-	private void readProductBandFromFile(String sBase, String sFileName) {
-		WasdiLog.debugLog(
-				"Sentinel6ProductReader.readProductBandFromFile. base path " + sBase + ", file name: " + sFileName);
-		try {
-			m_oProductFile = new File(sBase + File.separator + sFileName);
-			if (!m_oProductFile.exists()) {
-				WasdiLog.warnLog("Sentinel6ProductReader.readProductBandFromFile. file " + sBase + File.separator
-						+ sFileName + " does not exist");
-				return;
-			}
-			if (m_bSnapReadAlreadyDone == false) {
-				m_oProduct = readSnapProduct();
-				WasdiLog.debugLog("Sentinel6ProductReader.readProductBandFromFile. snap product has been read: "
-						+ m_oProduct.getName());
-			}
-			if (m_oProduct != null) {
-				m_bSnapReadAlreadyDone = true;
-				WasdiLog.debugLog("Sentinel6ProductReader.readProductBandFromFile. snap product has been already read");
-			} else {
-				WasdiLog.debugLog("Sentinel6ProductReader.readProductBandFromFile. snap product not yet read");
-				m_bSnapReadAlreadyDone = false;
-			}
-		} catch (Exception oE) {
-			WasdiLog.errorLog("Sentinel6ProductReader.readProductBandFromFile. tried to read " + sFileName
-					+ " but failed: " + oE);
-			m_bSnapReadAlreadyDone = false;
-		}
-	}
 
 	public static void main(String[] args) throws Exception {
-		String sFilePath = "C:/Users/valentina.leone/.wasdi/S6A_P4_1B_LR______20211025T003221_20211025T012834_20211025T090135_3373_035_109_054_EUM__OPE_ST_F03.SEN6.zip";
+		String sFilePath = "C:/Users/valentina.leone/.wasdi/S6A_P4_2__LR______20220814T223051_20220815T001047_20220815T002526_5996_065_017_009_EUM__OPE_NR_F06.SEN6.zip";
 		Sentinel6ProductReader pr = new Sentinel6ProductReader(new File(sFilePath));
 		String res = pr.adjustFileAfterDownload(sFilePath,
-				"S6A_P4_1B_LR______20211025T003221_20211025T012834_20211025T090135_3373_035_109_054_EUM__OPE_ST_F03.SEN6.zip");
+				"S6A_P4_2__LR______20220814T223051_20220815T001047_20220815T002526_5996_065_017_009_EUM__OPE_NR_F06.SEN6.zip");
 		System.out.println(res);
 
 	}
