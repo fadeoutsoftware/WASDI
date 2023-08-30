d66d956b07cc7779effc536b3570a4eb6f5a5859
leonevalentina <valentina.leone@wasdi.cloud>
2023-08-30 12:22:26 +0200

    Remove hardcoding of spatial resolution in CDS datasets

--- wrappersnap/launcher/src/main/java/wasdi/dataproviders/CDSProviderAdapter.java
+++ wrappersnap/launcher/src/main/java/wasdi/dataproviders/CDSProviderAdapter.java
@@ -269,15 +269,12 @@ public class CDSProviderAdapter extends ProviderAdapter {
 
 		List<String> oaTimeHours = Arrays.asList("00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00");
 
-		List<Double> adGrid = Arrays.asList(0.25, 0.25);
-
 		Map<String, Object> aoHashMap = new HashMap<>();
 		if (sProductType != null) aoHashMap.put("product_type", sProductType);
 		aoHashMap.put("variable", asVariables);
 		aoHashMap.put("year", sYear);
 		aoHashMap.put("month", sMonth);
 		aoHashMap.put("time", oaTimeHours);
-		aoHashMap.put("grid", adGrid);
 		aoHashMap.put("format", sFormat);
 		if (sMonthlyAggregation.equalsIgnoreCase("true")) {
 			aoHashMap.put("day", asDays);
