208f6dee6774bf70d8594698b43d8f5c0da0cf09
leonevalentina <valentina.leone@wasdi.cloud>
2023-08-31 10:06:59 +0200

    Remove hardcoding of the grid spatial resolution following the hotfix did for support channel

--- wrappersnap/launcher/src/main/java/wasdi/dataproviders/CDSProviderAdapter.java
+++ wrappersnap/launcher/src/main/java/wasdi/dataproviders/CDSProviderAdapter.java
@@ -366,10 +366,8 @@ public class CDSProviderAdapter extends ProviderAdapter {
 			String sProductType,  List<String> asVariables, String sFormat, String sBoundingBox, List<String> asPressureLevels) {
 		
 		List<String> oaTimeHours = Arrays.asList("00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00");
-		List<Double> adGrid = Arrays.asList(0.25, 0.25);
 
 		aoParametersMap.put("time", oaTimeHours);
-		aoParametersMap.put("grid", adGrid);
 		
 		if (!Utils.isNullOrEmpty(sProductType))
 			aoParametersMap.put("product_type", sProductType);
