6ed64f7c76b766e9ef7417bfb656b8825fdef957
wasdi-sysadmin-br <bertrand.robert@wasdi.cloud>
2023-11-09 19:00:35 +0100

    Bug fix

--- processorTypes/wasdiIDLDocker/docker-entrypoint.sh
+++ processorTypes/wasdiIDLDocker/docker-entrypoint.sh
@@ -5,4 +5,4 @@ then
     echo "${WASDI_IDL_LICENSE_SERVER_URL}" > /usr/local/harris/license/o_licenseserverurl.txt
 fi
 
-sudo -u appwasdi python3 /usr/bin/gunicorn --workers 1 --max-requests 1 --timeout 3600 --bind 0.0.0.0:5000 wasdiProcessorServer:app
+python3 /usr/bin/gunicorn --workers 1 --max-requests 1 --timeout 3600 --bind 0.0.0.0:5000 wasdiProcessorServer:app
