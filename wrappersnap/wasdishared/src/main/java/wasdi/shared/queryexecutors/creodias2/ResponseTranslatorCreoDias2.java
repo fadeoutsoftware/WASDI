2281547bdb311432545ed762a06bd6d2115459b2
pcampanella <p.campanella@fadeout.it>
2024-02-07 16:55:37 +0100

    added parse of multy polygons footprints for creo2

--- wrappersnap/wasdishared/src/main/java/wasdi/shared/queryexecutors/creodias2/ResponseTranslatorCreoDias2.java
+++ wrappersnap/wasdishared/src/main/java/wasdi/shared/queryexecutors/creodias2/ResponseTranslatorCreoDias2.java
@@ -49,7 +49,7 @@ public class ResponseTranslatorCreoDias2 extends ResponseTranslator {
 	// WASDI  keywords
 	private static final String sDATE = "date";
 	private static final String SINSTRUMENT = "instrumentshortname";
-	private static final String SMULTI_POLYGON = "MULTYPOLYGON";
+	private static final String SMULTI_POLYGON = "MultiPolygon";
 	private static final String SRELATIVE_ORBIT = "relativeorbitnumber";
 	private static final String SPLATFORM_NAME = "platformname";
 	private static final String SPOLYGON = "POLYGON";
@@ -255,16 +255,27 @@ public class ResponseTranslatorCreoDias2 extends ResponseTranslator {
 	
 	
 	private String parseFootPrint(JSONObject oJsonObject) {
-		JSONObject oJsonFootprint = new JSONObject(oJsonObject.optString(SODATA_FOOTPRINT));
-		
-		String sType = oJsonFootprint.optString(SODATA_TYPE);
-		
-		if (sType.equals(SODATA_POLYGON)) {
-			JSONArray aoCoordinates = (JSONArray) oJsonFootprint.optJSONArray(SODATA_COORDINATES);
-			String sCoordinates = parseCoordinates(aoCoordinates);
-			return SPOLYGON + " ((" + sCoordinates + "))";
+		try {
+			JSONObject oJsonFootprint = new JSONObject(oJsonObject.optString(SODATA_FOOTPRINT));
+			
+			String sType = oJsonFootprint.optString(SODATA_TYPE);
+			
+			if (sType.equals(SODATA_POLYGON)) {
+				JSONArray aoCoordinates = (JSONArray) oJsonFootprint.optJSONArray(SODATA_COORDINATES);
+				String sCoordinates = parseCoordinates(aoCoordinates);
+				return SPOLYGON + " ((" + sCoordinates + "))";
+			}
+			else if (sType.equals(SMULTI_POLYGON)) {
+				// NOTE: like this we are taking only the first polygon of the multi-polygon. May be to improved in future
+				JSONArray aoCoordinates = (JSONArray) oJsonFootprint.optJSONArray(SODATA_COORDINATES);
+				String sCoordinates = parseCoordinates(aoCoordinates.getJSONArray(0));
+				return SPOLYGON + " ((" + sCoordinates + "))";			
+			}			
 		}
-		// TODO: how to manage the multi-polygon? How I can simulate and example in Creodias?
+		catch (Exception oEx) {
+			WasdiLog.errorLog("ResponseTranslatorCreoDias2.parseFootPrint: error " + oEx.toString());
+		}
+		
 		return "";	
 	}
 	
