8bfd7ecc988f7dce855e785c56dfe74157cee454
leonevalentina <valentina.leone@wasdi.cloud>
2024-10-10 18:02:16 +0200

    add response translator - first draft

--- wrappersnap/wasdishared/src/main/java/wasdi/shared/queryexecutors/esa/QueryExecutorESA.java
+++ wrappersnap/wasdishared/src/main/java/wasdi/shared/queryexecutors/esa/QueryExecutorESA.java
@@ -1,6 +1,7 @@
 package wasdi.shared.queryexecutors.esa;
 
 import java.util.List;
+import java.util.ArrayList;
 
 import org.json.JSONObject;
 
@@ -10,6 +11,7 @@ import wasdi.shared.queryexecutors.PaginatedQuery;
 import wasdi.shared.queryexecutors.Platforms;
 import wasdi.shared.queryexecutors.QueryExecutor;
 import wasdi.shared.utils.HttpUtils;
+import wasdi.shared.utils.Utils;
 import wasdi.shared.utils.log.WasdiLog;
 import wasdi.shared.viewmodels.HttpCallResponse;
 import wasdi.shared.viewmodels.search.QueryResultViewModel;
@@ -40,57 +42,27 @@ public class QueryExecutorESA extends QueryExecutor {
 		QueryViewModel oQueryViewModel = m_oQueryTranslator.parseWasdiClientQuery(sQuery);
 		
 		if (!m_asSupportedPlatforms.contains(oQueryViewModel.platformName)) {
-			WasdiLog.debugLog("QueryExecutorESA.executeCount. Unsupported platform: " + oQueryViewModel.platformName);
+			WasdiLog.warnLog("QueryExecutorESA.executeCount. Unsupported platform: " + oQueryViewModel.platformName);
 			return iCount;
 		}
 		
-		// spatial filter
-		Double dWest = oQueryViewModel.west;
-		Double dNorth = oQueryViewModel.north;
-		Double dEast = oQueryViewModel.east;
-		Double dSouth = oQueryViewModel.south;
-
-		// temporal filter
-		String sDateFrom = oQueryViewModel.startFromDate;
-		String sDateTo = oQueryViewModel.endToDate;
-		
-		// product type filter
-		String sProductType = oQueryViewModel.productType;
-		
-		// name filter
-		String sFileName = oQueryViewModel.productName; // TODO
-		
-		
-		String sCountURL = m_ESABaseUrl;
-		
-		if (!sCountURL.endsWith("/")) 
-			sCountURL += "/";
-			
-		sCountURL += "collections/" + sProductType + "/items?";
-		sCountURL += "bbox=" + dWest + "," + dSouth + "," + dEast + "," + dNorth; 
-		
-		sCountURL += "&datetime=" + sDateFrom + "/" + sDateTo;
-		
-		WasdiLog.debugLog("QueryExecutorESA.executeCount. Sending count query to data provider: " + sCountURL);
-		
 		try {
-			HttpCallResponse oResponse = HttpUtils.httpGet(sCountURL);
+			String sResult =  getResultsFromEOCAT(oQueryViewModel);
 			
-			int iResponseCode = oResponse.getResponseCode();
-			
-			if (iResponseCode >= 200 && iResponseCode <= 299) {
-				String sResponseBody = oResponse.getResponseBody();
-				JSONObject oJsonBody = new JSONObject(sResponseBody);
-				Integer iMatches = oJsonBody.optInt("numberMatched");
-				
-				if (iMatches != null) {
-					iCount = iMatches;
-				}
-				
-				WasdiLog.debugLog("QueryExecutorESA.executeCount. Number of matches found: " + iMatches);
-			} else {
-				WasdiLog.warnLog("QueryExeutorESA.executeCount. Error code from HTTP request: " + iResponseCode);
+			if (Utils.isNullOrEmpty(sResult)) {
+				WasdiLog.debugLog("QueryExecutorESA.executeCount. Error getting the results from EOCAt");
+				return iCount;
 			}
+			
+			JSONObject oJsonBody = new JSONObject(sResult);
+			
+			Integer iMatches = oJsonBody.optInt("numberMatched");
+				
+			if (iMatches != null) {
+				iCount = iMatches;
+			}
+				
+			WasdiLog.debugLog("QueryExecutorESA.executeCount. Number of matches found: " + iMatches);
 			
 		} catch (Exception oEx) {
 			WasdiLog.errorLog("QueryExecutorESA.executeCount. Error retrieving number of results: ", oEx);
@@ -101,7 +73,108 @@ public class QueryExecutorESA extends QueryExecutor {
 
 	@Override
 	public List<QueryResultViewModel> executeAndRetrieve(PaginatedQuery oQuery, boolean bFullViewModel) {
-		// TODO Auto-generated method stub
+		
+		
+		QueryViewModel oQueryViewModel = m_oQueryTranslator.parseWasdiClientQuery(oQuery.getQuery());
+		
+		if (!m_asSupportedPlatforms.contains(oQueryViewModel.platformName)) {
+			WasdiLog.warnLog("QueryExecutorESA.executeAndRetrieve. Unsupported platform: " + oQueryViewModel.platformName);
+			return null;
+		}
+		
+		List<QueryResultViewModel> oResults = new ArrayList<>(); 
+		try {
+			
+			String sResult =  getResultsFromEOCAT(oQueryViewModel);
+			
+			if (Utils.isNullOrEmpty(sResult)) {
+				WasdiLog.debugLog("QueryExecutorESA.executeAndRetrieve. Error getting the results from EOCAt");
+				return null;
+			}
+			
+			
+			WasdiLog.debugLog("QueryExecutorESA.executeAndRetrieve. Returning results: " + oResults.size());
+			
+			return oResults;
+			
+		} catch (Exception oEx) {
+			WasdiLog.errorLog("QueryExecutorESA.executeAndRetrieve. Error retrieving number of results: ", oEx);
+		}
+		
+		return null;
+		
+	}
+	
+	
+	public String getResultsFromEOCAT(QueryViewModel oQueryViewModel) {
+		
+		// product type filter
+		String sProductType = oQueryViewModel.productType;
+		
+		if (Utils.isNullOrEmpty(sProductType)) {
+			WasdiLog.warnLog("QueryExecutorESA.getResultsFromEOCAT. Product type not specified");
+			return null;
+		}
+		
+		// name filter
+		String sFileName = oQueryViewModel.productName;
+		
+		String sURL = m_ESABaseUrl;
+		
+		if (!sURL.endsWith("/")) 
+			sURL += "/";
+			
+		sURL += "collections/" + sProductType + "/items";
+		
+		if (!Utils.isNullOrEmpty(sFileName)) {
+			// example: https://eocat.esa.int/eo-catalogue/collections/SAR_IMP_1P/items/SAR_IMP_1PNESA20110212_100804_00000015A165_00165_82684_0000
+			// search by name
+			sURL += sFileName;
+		}
+		else {
+			// spatial filter
+			Double dWest = oQueryViewModel.west;
+			Double dNorth = oQueryViewModel.north;
+			Double dEast = oQueryViewModel.east;
+			Double dSouth = oQueryViewModel.south;
+
+			// temporal filter
+			String sDateFrom = oQueryViewModel.startFromDate;
+			String sDateTo = oQueryViewModel.endToDate;
+			
+			if (dWest == null || dNorth == null || dEast == null || dSouth == null) {
+				WasdiLog.warnLog("QueryExecutorESA.getResultsFromEOCAT. Some coordinates were not specified");
+				return null;
+			}
+			
+			if (Utils.isNullOrEmpty(sDateFrom) || Utils.isNullOrEmpty(sDateFrom)) {
+				WasdiLog.warnLog("QueryExecutorESA.getResultsFromEOCAT. Missing start or end date");
+				return null;
+			}
+			
+			sURL += "?bbox=" + dWest + "," + dSouth + "," + dEast + "," + dNorth; 
+			
+			sURL += "&datetime=" + sDateFrom + "/" + sDateTo;			
+		}
+		
+		WasdiLog.debugLog("QueryExecutorESA.getResultsFromEOCAT. Sending  query to data provider: " + sURL);
+		
+		try {
+			HttpCallResponse oResponse = HttpUtils.httpGet(sURL);
+			
+			int iResponseCode = oResponse.getResponseCode();
+			
+			if (iResponseCode >= 200 && iResponseCode <= 299) {
+				String sResponseBody = oResponse.getResponseBody();
+				return sResponseBody;		
+			} else {
+				WasdiLog.warnLog("QueryExeutorESA.getResultsFromEOCAT. Error code from HTTP request: " + iResponseCode);
+			}
+			
+		} catch (Exception oEx) {
+			WasdiLog.errorLog("QueryExecutorESA.getResultsFromEOCAT. Error retrieving number of results: ", oEx);
+		}
+		
 		return null;
 	}
 	
--- wrappersnap/wasdishared/src/main/java/wasdi/shared/queryexecutors/esa/ResponseTranslatorESA.java
+++ wrappersnap/wasdishared/src/main/java/wasdi/shared/queryexecutors/esa/ResponseTranslatorESA.java
@@ -1,8 +1,14 @@
 package wasdi.shared.queryexecutors.esa;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.LinkedList;
 import java.util.List;
 
 import wasdi.shared.queryexecutors.ResponseTranslator;
+import wasdi.shared.utils.Utils;
+import wasdi.shared.utils.log.WasdiLog;
 import wasdi.shared.viewmodels.search.QueryResultViewModel;
 
 public class ResponseTranslatorESA extends ResponseTranslator {
@@ -13,7 +19,39 @@ public class ResponseTranslatorESA extends ResponseTranslator {
 
 	@Override
 	public List<QueryResultViewModel> translateBatch(String sResponse, boolean bFullViewModel) {
-		// TODO Auto-generated method stub
+		
+		if (Utils.isNullOrEmpty(sResponse)) {
+			WasdiLog.debugLog("ResponseTranslatorESA.translateBatch. Response is null or empty");
+			return null;
+		}
+		
+		JSONObject oJSONResponse = new JSONObject(sResponse);
+		
+		JSONArray aoFeatureArray = oJSONResponse.optJSONArray("features");
+		
+		if (aoFeatureArray == null) {
+			WasdiLog.debugLog("ResponseTranslatorESA.translateBatch. No 'features' entry found in the response");
+			return null;
+		}
+		
+		List<QueryResultViewModel> aoResults = new LinkedList<>();
+		
+		try {
+			for (Object oItem : aoFeatureArray) {
+				if (oItem != null) {
+					JSONObject oJsonItem = (JSONObject) (oItem);
+					aoResults.add(processProduct(oJsonItem, bFullViewModel));
+	
+				}
+			}
+		} catch (Exception oEx) {
+			WasdiLog.errorLog("ResponseTranslatorESA.translateBatch. Exception when translating the results in the Wasdi view model ", oEx);
+		}
+		
+		return null;
+	}
+	
+	private QueryResultViewModel processProduct(JSONObject oJsonItem, boolean bFullViewModel) {
 		return null;
 	}
 
