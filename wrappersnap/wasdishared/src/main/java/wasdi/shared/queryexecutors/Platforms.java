b17e9b50adf504b31a2e20d4176dcc9a651bb2ed
leonevalentina <valentina.leone@wasdi.cloud>
2024-10-21 09:34:53 +0200

    add filters in the client app config

--- wrappersnap/wasdishared/src/main/java/wasdi/shared/queryexecutors/Platforms.java
+++ wrappersnap/wasdishared/src/main/java/wasdi/shared/queryexecutors/Platforms.java
@@ -55,9 +55,10 @@ public class Platforms {
 	public static String COP_DEM = "COP_DEM";
 	public static String S2GLC = "S2GLC";
 	
-	public static String BIGBANG = "BIGBANG";
 	
 	// Return Plaforms
+	public static String BIGBANG = "BIGBANG";
 	public static String RETURN_RASTER = "ReturnRaster";
+	public static String METEOCEAN = "MeteOcean"; 
 
 }
