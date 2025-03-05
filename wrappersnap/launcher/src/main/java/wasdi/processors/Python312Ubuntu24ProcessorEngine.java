dbf1a4b37c374631d01d690d01552bfa2e604b9b
FADEOUT\p.campanella <p.campanella@fadeout.it>
2025-03-05 17:06:11 +0100

    wrong base path

--- wrappersnap/launcher/src/main/java/wasdi/processors/Python312Ubuntu24ProcessorEngine.java
+++ wrappersnap/launcher/src/main/java/wasdi/processors/Python312Ubuntu24ProcessorEngine.java
@@ -1,21 +1,22 @@
 package wasdi.processors;
 
 import wasdi.shared.business.processors.ProcessorTypes;
+import wasdi.shared.config.WasdiConfig;
 
 public class Python312Ubuntu24ProcessorEngine extends PipOneShotProcessorEngine {
 	
 	public Python312Ubuntu24ProcessorEngine() {
-		super();
-		 if (!m_sDockerTemplatePath.endsWith("/")) m_sDockerTemplatePath += "/";
-         m_sDockerTemplatePath += ProcessorTypes.getTemplateFolder(ProcessorTypes.PYTHON312_UBUNTU24);
+		m_sDockerTemplatePath = WasdiConfig.Current.paths.dockerTemplatePath;
+		if (!m_sDockerTemplatePath.endsWith("/")) m_sDockerTemplatePath += "/";
+        m_sDockerTemplatePath += ProcessorTypes.getTemplateFolder(ProcessorTypes.PYTHON312_UBUNTU24);
          
-         m_asDockerTemplatePackages = new String[8];
-         m_asDockerTemplatePackages[0] = "flask";
-         m_asDockerTemplatePackages[1] = "gunicorn";
-         m_asDockerTemplatePackages[2] = "requests";
-         m_asDockerTemplatePackages[4] = "wheel";
-         m_asDockerTemplatePackages[5] = "wasdi";
-         m_asDockerTemplatePackages[6] = "time";
-         m_asDockerTemplatePackages[7] = "datetime";         
-	}
+        m_asDockerTemplatePackages = new String[7];
+        m_asDockerTemplatePackages[0] = "flask";
+        m_asDockerTemplatePackages[1] = "gunicorn";
+        m_asDockerTemplatePackages[2] = "requests";
+        m_asDockerTemplatePackages[3] = "wheel";
+        m_asDockerTemplatePackages[4] = "wasdi";
+        m_asDockerTemplatePackages[5] = "time";
+        m_asDockerTemplatePackages[6] = "datetime";
+    }
 }
