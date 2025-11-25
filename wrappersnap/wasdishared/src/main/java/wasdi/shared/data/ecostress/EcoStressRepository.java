0451573abef155ac9e082e951609ff6a5a3ead76
leonevalentina <valentina.leone@wasdi.cloud>
2025-11-25 09:59:38 +0100

    remove unused fields

--- wrappersnap/wasdishared/src/main/java/wasdi/shared/data/ecostress/EcoStressRepository.java
+++ wrappersnap/wasdishared/src/main/java/wasdi/shared/data/ecostress/EcoStressRepository.java
@@ -25,17 +25,11 @@ public class EcoStressRepository extends MongoRepository {
 			try {
 				String sJSON = "{ \"fileName\": \""+oNewDocument.getFileName()+"\",";
 
-				sJSON += "\"startOrbitNumber\": "+oNewDocument.getStartOrbitNumber()+",";
-				sJSON += "\"stopOrbitNumber\": "+oNewDocument.getStopOrbitNumber()+",";
 				sJSON += "\"dayNightFlag\": \""+oNewDocument.getDayNightFlag()+"\",";
 
 				sJSON += "\"beginningDate\": "+oNewDocument.getBeginningDate()+",";
 				sJSON += "\"endingDate\": "+oNewDocument.getEndingDate()+",";
 
-				sJSON += "\"platform\": \""+oNewDocument.getPlatform()+"\",";
-				sJSON += "\"instrument\": \""+oNewDocument.getInstrument()+"\",";
-				sJSON += "\"sensor\": \""+oNewDocument.getSensor()+"\",";
-				sJSON += "\"parameterName\": \""+oNewDocument.getParameterName()+"\",";
 				sJSON += "\"s3Path\": \""+oNewDocument.getS3Path()+"\",";
 				sJSON += "\"url\": \""+oNewDocument.getUrl()+"\",";
 				sJSON += "\"location\":" + oNewDocument.getLocation();
