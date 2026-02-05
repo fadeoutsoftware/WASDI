c10b95376b91804f2ca843cb23e3f2d576184c94
leonevalentina <valentina.leone@wasdi.cloud>
2026-02-05 18:02:42 +0100

    start drafting the method

--- wrappersnap/launcher/src/main/java/wasdi/io/HDFProductReader.java
+++ wrappersnap/launcher/src/main/java/wasdi/io/HDFProductReader.java
@@ -1,10 +1,14 @@
 package wasdi.io;
 
 import java.io.File;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.esa.snap.core.datamodel.MetadataElement;
 import org.esa.snap.core.datamodel.Product;
 
+import wasdi.shared.business.ecostress.EcoStressItemForReading;
+import wasdi.shared.data.ecostress.EcoStressRepository;
 import wasdi.shared.utils.log.WasdiLog;
 
 public class HDFProductReader extends SnapProductReader {
@@ -75,7 +79,44 @@ public class HDFProductReader extends SnapProductReader {
 		
 		WasdiLog.infoLog("HDFProductReader.getFileForPublishBand. Absolute file of the product" + m_oProductFile.getAbsolutePath());
 		
+		String sProductName = m_oProduct.getName();
+		
+		if (!m_oProduct.getName().toUpperCase().startsWith("EEH2TES_L2_LSTE")) {
+			return super.getFileForPublishBand(sBand, sLayerId, sPlatform);
+		}
+		
+		// we need to publish the LST bands of LSTE ECOSTRESS products
+		
+		// first of all, we need to find the dedicated L1_GEO file
+		String sProductInfo = extractProductInfo(sProductName);
+		
+		String sGEOProductNamePrefix = "ECOv002_L1B_GEO_" + sProductInfo;
+		
+		EcoStressRepository oRepo = new EcoStressRepository();
+		
+		EcoStressItemForReading oEcostressItem = oRepo.getEcoStressByFileNamePrefix(sGEOProductNamePrefix);
+		
+		if (oEcostressItem == null) {
+			WasdiLog.errorLog("HDFProductReader.getFileForPublishBand. No GEO product found for file " + sProductName);
+		}
+		
+		// from here I can download the GEO product in  the temp folder
+		
+		
+		
+		
 		return null;
+	}
+	
+	private String extractProductInfo(String sFileName) {
+        String sRegex = "(\\d{5}_\\d{3}_\\d{8}T\\d{6})";
+        Pattern oPattern = Pattern.compile(sRegex);
+        Matcher oMatcher = oPattern.matcher(sFileName);
+
+        if (oMatcher.find()) 
+            return oMatcher.group(1);
+        
+        return null;
 	}
 	
 	
