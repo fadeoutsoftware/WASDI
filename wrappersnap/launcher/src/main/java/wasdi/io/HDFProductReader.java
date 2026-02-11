cf16bef4584e7de6541f34e66ab59b0de564a67a
leonevalentina <valentina.leone@wasdi.cloud>
2026-02-11 15:48:30 +0100

    separate args

--- wrappersnap/launcher/src/main/java/wasdi/io/HDFProductReader.java
+++ wrappersnap/launcher/src/main/java/wasdi/io/HDFProductReader.java
@@ -206,7 +206,8 @@ public class HDFProductReader extends SnapProductReader {
 			sGdalCommand = "gdalwarp";
 			sGdalCommand = GdalUtils.adjustGdalFolder(sGdalCommand);
 			asWarpArgs.add(sGdalCommand);
-			asWarpArgs.add("-dstnodata 0");
+			asWarpArgs.add("-dstnodata");
+			asWarpArgs.add("0");
 			asWarpArgs.add("-geoloc");
 			asWarpArgs.add("-t_srs");
 			asWarpArgs.add("EPSG:4326");
